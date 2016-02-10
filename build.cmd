d6dff5f899e20893b7a8d88865b7455ba736788e
David Podhola <david@podhola.net>
2016-02-10 10:09:33 +0100

    add build.cmd to be buildable on windows
    try to add Seq compatible List funcs (singleton not working)

---
+++ build.cmd
@@ -0,0 +1,15 @@
+@echo off
+cls
+
+.paket\paket.bootstrapper.exe
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+.paket\paket.exe restore
+if errorlevel 1 (
+  exit /b %errorlevel%
+)
+
+packages\FAKE\tools\FAKE.exe build.fsx %*
+
