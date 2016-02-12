7018365ae50f593020d482e1f9bd9e93ff594a1a
David Podhola <david@podhola.net>
2016-02-12 15:49:54 +0100

    fnr.exe to "fix" https://github.com/fsharp/FSharp.Compiler.Service/issues/529
    run fabel2babel.js before tests (this was why the tests were not running, not the node version)

--- before_build.cmd
+++ before_build.cmd
@@ -1,2 +1,3 @@
 call setup.cmd
+utils\fnr\fnr.exe --cl --find "FSharp.Compiler.Service.ProjectCrackerToolCopy" --replace "FSharpCompilerServiceProjectCrackerToolCopy" --dir "%cd%" --fileMask "*.targets" --includeSubDirectories
 set PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
