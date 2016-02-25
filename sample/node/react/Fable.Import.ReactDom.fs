3f5f853b21caa05341c592190241615d9811ee66
David Teasdale <david@teasdale.me>
2016-02-25 15:34:29 +0000

    Add react node server render sample

---
+++ sample/node/react/Fable.Import.ReactDom.fs
@@ -0,0 +1,46 @@
+namespace Fable.Import
+open System
+
+open Fable.Import.React
+
+module ReactDom =
+    type U2<'a, 'b> = Case1 of 'a | Case2 of 'b
+    type U3<'a, 'b, 'c> = Case1 of 'a | Case2 of 'b | Case3 of 'c
+
+    module DOM =
+        type U2<'a, 'b> = Case1 of 'a | Case2 of 'b
+        type U3<'a, 'b, 'c> = Case1 of 'a | Case2 of 'b | Case3 of 'c
+
+        type Globals =
+            abstract version: string with get, set
+            abstract findDOMNode: instance: ReactInstance -> 'E
+            abstract findDOMNode: instance: ReactInstance -> Element
+            abstract render: element: DOMElement<'P> * container: Element * ?callback: Func<Element, obj> -> Element
+            abstract render: element: ClassicElement<'P> * container: Element * ?callback: Func<ClassicComponent<'P, 'S>, obj> -> ClassicComponent<'P, 'S>
+            abstract render: element: ReactElement<'P> * container: Element * ?callback: Func<Component<'P, 'S>, obj> -> Component<'P, 'S>
+            abstract unmountComponentAtNode: container: Element -> bool
+            abstract unstable_batchedUpdates: callback: Func<'A, 'B, obj> * a: 'A * b: 'B -> unit
+            abstract unstable_batchedUpdates: callback: Func<'A, obj> * a: 'A -> unit
+            abstract unstable_batchedUpdates: callback: Func<obj> -> unit
+            abstract unstable_renderSubtreeIntoContainer: parentComponent: Component<obj, obj> * nextElement: DOMElement<'P> * container: Element * ?callback: Func<Element, obj> -> Element
+            abstract unstable_renderSubtreeIntoContainer: parentComponent: Component<obj, obj> * nextElement: ClassicElement<'P> * container: Element * ?callback: Func<ClassicComponent<'P, 'S>, obj> -> ClassicComponent<'P, 'S>
+            abstract unstable_renderSubtreeIntoContainer: parentComponent: Component<obj, obj> * nextElement: ReactElement<'P> * container: Element * ?callback: Func<Component<'P, 'S>, obj> -> Component<'P, 'S>
+
+        let [<Import("react-dom")>] Globals: Globals = failwith "JS only"
+
+
+    module DOMServer =
+        type U2<'a, 'b> = Case1 of 'a | Case2 of 'b
+        type U3<'a, 'b, 'c> = Case1 of 'a | Case2 of 'b | Case3 of 'c
+
+
+        type Globals =
+            abstract version: string with get, set
+            abstract renderToString: element: ReactElement<obj> -> string
+            abstract renderToStaticMarkup: element: ReactElement<obj> -> string
+
+        let [<Import("react-dom/server")>] Globals: Globals = failwith "JS only"
+
+
+
+
