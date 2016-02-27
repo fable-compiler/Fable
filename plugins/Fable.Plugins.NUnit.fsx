96dee5d24134136d564fe18796edb5a812a689c4
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-02-27 11:20:12 +0100

    Plugin docs

--- plugins/Fable.Plugins.NUnit.fsx
+++ plugins/Fable.Plugins.NUnit.fsx
@@ -1,4 +1,4 @@
-module Fable.Plugins
+namespace Fable.Plugins
 
 #r "../build/main/Fable.exe"
 
@@ -6,57 +6,60 @@ open Fable.AST
 open Fable.FSharp2Fable
 open Fable.Fable2Babel
 
-let private (|TestFixture|_|) (decl: Fable.Declaration) =
-    match decl with
-    | Fable.EntityDeclaration (ent, entDecls, entRange) ->
-        match ent.TryGetDecorator "TestFixture" with
-        | Some _ -> Some (ent, entDecls, entRange)
-        | None -> None
-    | _ -> None
-
-let private (|Test|_|) (decl: Fable.Declaration) =
-    match decl with
-    | Fable.MemberDeclaration m ->
-        match m.Kind, m.TryGetDecorator "Test" with
-        | Fable.Method name, Some _ -> Some (m, name)
+module Util =
+    let (|TestFixture|_|) (decl: Fable.Declaration) =
+        match decl with
+        | Fable.EntityDeclaration (ent, entDecls, entRange) ->
+            match ent.TryGetDecorator "TestFixture" with
+            | Some _ -> Some (ent, entDecls, entRange)
+            | None -> None
         | _ -> None
-    | _ -> None
 
-// Compile tests using Mocha.js BDD interface
-let private transformTest com ctx (test: Fable.Member) name =
-    let testName =
-        Babel.StringLiteral name :> Babel.Expression
-    let testBody =
-        Util.funcExpression com ctx test.Arguments test.Body :> Babel.Expression
-    let testRange =
-        match testBody.loc with
-        | Some loc -> test.Range + loc | None -> test.Range
-    // it('Test name', function() { /* Tests */ });
-    Babel.ExpressionStatement(
-        Babel.CallExpression(Babel.Identifier "it",
-            [U2.Case1 testName; U2.Case1 testBody], testRange), testRange)
-    :> Babel.Statement
+    let (|Test|_|) (decl: Fable.Declaration) =
+        match decl with
+        | Fable.MemberDeclaration m ->
+            match m.Kind, m.TryGetDecorator "Test" with
+            | Fable.Method name, Some _ -> Some (m, name)
+            | _ -> None
+        | _ -> None
 
-let private transformTestFixture com ctx (fixture: Fable.Entity) testDecls testRange =
-    let testDesc =
-        Babel.StringLiteral fixture.Name :> Babel.Expression
-    let testBody =
-        Babel.FunctionExpression([],
-            Babel.BlockStatement (testDecls, ?loc=Some testRange), ?loc=Some testRange)
-        :> Babel.Expression
-    Babel.ExpressionStatement(
-        Babel.CallExpression(Babel.Identifier "describe",
-            [U2.Case1 testDesc; U2.Case1 testBody],
-            testRange)) :> Babel.Statement
+    // Compile tests using Mocha.js BDD interface
+    let transformTest com ctx (test: Fable.Member) name =
+        let testName =
+            Babel.StringLiteral name :> Babel.Expression
+        let testBody =
+            Util.funcExpression com ctx test.Arguments test.Body :> Babel.Expression
+        let testRange =
+            match testBody.loc with
+            | Some loc -> test.Range + loc | None -> test.Range
+        // it('Test name', function() { /* Tests */ });
+        Babel.ExpressionStatement(
+            Babel.CallExpression(Babel.Identifier "it",
+                [U2.Case1 testName; U2.Case1 testBody], testRange), testRange)
+        :> Babel.Statement
 
-let asserts com (i: Fable.ApplyInfo) =
-    match i.methodName with
-    | "areEqual" ->
-        Fable.Util.ImportCall("assert", true, None, Some "equal", false, i.args)
-        |> Fable.Util.makeCall com i.range i.returnType |> Some
-    | _ -> None
+    let transformTestFixture com ctx (fixture: Fable.Entity) testDecls testRange =
+        let testDesc =
+            Babel.StringLiteral fixture.Name :> Babel.Expression
+        let testBody =
+            Babel.FunctionExpression([],
+                Babel.BlockStatement (testDecls, ?loc=Some testRange), ?loc=Some testRange)
+            :> Babel.Expression
+        Babel.ExpressionStatement(
+            Babel.CallExpression(Babel.Identifier "describe",
+                [U2.Case1 testDesc; U2.Case1 testBody],
+                testRange)) :> Babel.Statement
 
-type TestPlugin() =
+    let asserts com (i: Fable.ApplyInfo) =
+        match i.methodName with
+        | "areEqual" ->
+            Fable.Util.ImportCall("assert", true, None, Some "equal", false, i.args)
+            |> Fable.Util.makeCall com i.range i.returnType |> Some
+        | _ -> None
+
+open Util
+
+type NUnitPlugin() =
     interface IDeclarePlugin with
         member x.TryDeclareRoot com ctx file =
             if file.Root.TryGetDecorator "TestFixture" |> Option.isNone then None else
@@ -64,7 +67,6 @@ type TestPlugin() =
             let rootRange = Util.foldRanges SourceLocation.Empty rootDecls
             (rootRange, transformTestFixture com ctx file.Root rootDecls rootRange |> U2.Case1)
             |> Some
-
         member x.TryDeclare com ctx decl =
             match decl with
             | Test (test, name) ->
