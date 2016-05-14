321fa08e92686d1fc1111a5b12f65e8f079c5378
Phillip Trelford <phil@trelford.com>
2016-05-14 13:02:39 +0100

    Removing commented out code from Mario sample

--- samples/browser/mario/mario.fsx
+++ samples/browser/mario/mario.fsx
@@ -1,23 +1,13 @@
 // ---
-// header: Canvas
-// tagline: Using HTML5 canvas (adapted from FunScript)
+// header: Mario
+// tagline: Playable Mario with HTML5 Canvas
+// popup-style: width:580px
 // ---
 
 #r "node_modules/fable-core/Fable.Core.dll"
 
 open Fable.Core
 open Fable.Import.Browser
-
-(*
-let canvas =  document.getElementsByTagName_canvas().[0.]
-canvas.width <- 1000.
-canvas.height <- 800.
-let ctx = canvas.getContext_2d()
-ctx.fillStyle <- U3.Case1 "rgb(200,0,0)"
-ctx.fillRect (10., 10., 55., 50.);
-ctx.fillStyle <- U3.Case1 "rgba(0, 0, 200, 0.5)"
-ctx.fillRect (30., 30., 55., 50.)
-*)
 
 /// Handles keydown and keyup events of the window and exposes them
 /// using the 'arrows' property (which is a tuple int*int with -1 if the
