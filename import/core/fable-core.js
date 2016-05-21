ae4d3d77a50a49e600540491a6a6691625fccb9a
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-05-21 23:49:21 +0200

    Add Event functions to fable-core.js

--- import/core/fable-core.js
+++ import/core/fable-core.js
@@ -1987,4 +1987,217 @@
       }, w),
     ];
   };
+  
+  var Event = exports.Event = function(sbscrb, delegates) {
+    var _this = this;
+    this.delegates = delegates || new Array()
+    
+    this.publish = function () {
+       return _this 
+    };
+    
+    this.trigger = function (value) { 
+      Seq.iter(function(f) { f(value) }, _this.delegates ) 
+    };
+    
+    var addHandler = function (f) {
+      _this.delegates.push(f)
+    };
+    
+    var removeHandler = function (f) {
+      var index = _this.delegates.indexOf(f);
+      if (index > -1) {
+        _this.delegates.splice(index, 1);
+      }
+    }
+    
+    this.subscribe = sbscrb || function (observer) {
+      var f = observer.onNext; 
+      addHandler(f); 
+      
+       var disp = {
+        dispose: function () {
+          removeHandler(f);
+        }
+      };
+      disp[FSymbol.interfaces] = ["System.IDisposable"];
+      return disp;
+    }
+
+  };
+  
+  Event.add = function (f, w) {
+    w.subscribe(new Observer(f));
+  };
+  
+  Event.map = function(f, w) {
+    var s = function(observer) {
+      w.subscribe(new Observer(
+        function (v) {
+          Obs.__protect(
+            function () { 
+              return f(v)
+            },
+            observer.onNext,
+            observer.onError
+          );
+        },
+        observer.onError,
+        observer.onCompleted));
+    }
+    return new Event(s, w.delegates) 
+  }
+  
+  Event.choose = function (f, w) {
+    var s = function (observer) {
+      return w.subscribe(new Observer(
+        function (v) {
+          Obs.__protect(
+            function () { return f(v) },
+            function (v) {
+              if (v != null) {
+                observer.onNext(v);
+              }
+            },
+            observer.onError
+          );
+        },
+        observer.onError,
+        observer.onCompleted
+      ));
+    };
+    return new Event(s, w.delegates)
+    
+  };
+  
+  Event.filter = function (f, w) {
+    return Event.choose(function (x) {
+      return f(x) ? x : null;
+    }, w);
+  };
+  
+  Event.partition = function (f, w) {
+    return [
+      Event.filter(f, w),
+      Event.filter(function (x) {
+        return !f(x);
+      }, w)
+    ];
+  };
+  
+  Event.scan = function (f, state, w) {
+   var s = function (observer) {
+      return w.subscribe(new Observer(
+        function (v) {
+          Obs.__protect(
+            function () { return f(state, v) },
+            function (z) {
+              state = z;
+              observer.onNext(z);
+            },
+            observer.onError
+          );
+        },
+        observer.onError,
+        observer.onCompleted
+      ));
+    };
+    return new Event(s, w.delegates)
+    
+  };
+  
+  Event.pairwise = function (w) {
+    var s = function (observer) {
+      var lastArgs = null;
+      return w.subscribe(new Observer(
+        function (args2) {
+          if (lastArgs != null) {
+            observer.onNext([lastArgs, args2]);
+          }
+          lastArgs = args2;
+        },
+        observer.onError,
+        observer.onCompleted
+      ));
+    };
+    return new Event(s, w.delegates)
+    
+  };
+  
+  Event.merge = function (w1, w2) {
+    var s = function (observer) {
+      var stopped = false,
+          completed1 = false,
+          completed2 = false;
+      var h1 = w1.subscribe(new Observer(
+        function (v) {
+          if (!stopped) {
+            observer.onNext(v);
+          }
+        },
+        function (e) {
+          if (!stopped) {
+            stopped = true;
+            observer.onError(e);
+          }
+        },
+        function () {
+          if (!stopped) {
+            completed1 = true;
+            if (completed2) {
+              stopped = true;
+              observer.onCompleted();
+            }
+          }
+        }
+      ));
+      var h2 = w2.subscribe(new Observer(
+        function (v) {
+          if (!stopped) {
+            observer.onNext(v);
+          }
+        },
+        function (e) {
+          if (!stopped) {
+            stopped = true;
+            observer.onError(e);
+          }
+        },
+        function () {
+          if (!stopped) {
+            completed2 = true;
+            if (completed1) {
+              stopped = true;
+              observer.onCompleted();
+            }
+          }
+        }
+      ));
+      var disp = {
+        dispose: function () {
+          h1.dispose();
+          h2.dispose();
+        }
+      };
+      disp[FSymbol.interfaces] = ["System.IDisposable"];
+      return disp;
+    };
+    
+    return new Event(s, w1.delegates.concat(w2.delegates))
+  };
+  
+  Event.split = function (f, w) {
+    return [
+      Event.choose(function (v) {
+        var res = f(v);
+        return res.Case == "Choice1Of2"
+          ? res.Fields[0] : null;
+      }, w),
+      Event.choose(function (v) {
+        var res = f(v);
+        return res.Case == "Choice2Of2"
+          ? res.Fields[0] : null;
+      }, w),
+    ];
+  };  
 });
