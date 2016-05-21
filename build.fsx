db4004001c2e09bc4ef3075f32272713a6574cde
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2016-05-22 00:07:56 +0200

    Temp change to build script

--- build.fsx
+++ build.fsx
@@ -160,8 +160,8 @@ Target "MochaTest" (fun _ ->
     FileUtils.cp "src/tests/package.json" testsBuildDir
     Npm.install testsBuildDir []
     // Copy the development version of fable-core.js
-    if environVar "DEV_MACHINE" = "1" then
-        FileUtils.cp "import/core/fable-core.js" "build/tests/node_modules/fable-core/"
+    //if environVar "DEV_MACHINE" = "1" then
+    FileUtils.cp "import/core/fable-core.js" "build/tests/node_modules/fable-core/"
     Npm.script testsBuildDir "test" []
 )
 
