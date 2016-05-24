f4b0e1726390818f4aefeb2e2ace0f0c86d53b9d
7sharp9 <kukulcanenator@googlemail.com>
2016-05-24 22:03:07 +0100

    Revert "Add an extension to allow let! bindings of promises in async"
    This reverts commit b4e9db4f32353e8d2815bc5189da79beb5282a35.

--- import/core/Fable.Core.Extensions.fs
+++ import/core/Fable.Core.Extensions.fs
@@ -8,7 +8,3 @@ module Extensions =
     type Async with
         static member AwaitPromise(promise: Promise<'T>): Async<'T> = failwith "JS only"
         static member StartAsPromise(workflow: Async<'T>, ?token: CancellationToken): Promise<'T> = failwith "JS only"
-
-    type Microsoft.FSharp.Control.AsyncBuilder with
-        member __.Bind(promise, f) = 
-            async.Bind (Async.AwaitPromise(promise), f)
