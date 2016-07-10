7d083898605ef238912ccf4c2042b7c6a9e99679
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-07-10 11:06:29 +0200

    Remove unused file

--- src/fable/Fable.Core/src/AssemblyInfo.fs
+++
@@ -1,9 +0,0 @@
-﻿namespace System
-open System.Reflection
-
-[<assembly: AssemblyVersionAttribute("0.2.0")>]
-do ()
-
-module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.2.0"
-    let [<Literal>] InformationalVersion = "0.2.0"
