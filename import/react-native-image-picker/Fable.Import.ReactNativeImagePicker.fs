e9de746c70619428d544d93bdb2d36ff2b373c5a
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-07-28 10:08:23 +0200

    Add ReactNativeImagePicker bindings

---
+++ import/react-native-image-picker/Fable.Import.ReactNativeImagePicker.fs
@@ -0,0 +1,67 @@
+namespace Fable.Import
+
+open System
+open Fable.Core
+open Fable.Import
+open Fable.Import.JS
+open Fable.Import.Browser
+
+module ReactImagePicker =
+
+    [<StringEnum>]
+    type CameraType =
+    | [<CompiledName("front")>] Front
+    | [<CompiledName("back")>] Back
+
+    [<StringEnum>]
+    type MediaType =
+    | [<CompiledName("photo")>] Photo
+    | [<CompiledName("video")>] Video
+    | [<CompiledName("mixed")>] Mixed
+
+    [<StringEnum>]
+    type VideoQuality =
+    | [<CompiledName("low")>] Low
+    | [<CompiledName("medium")>] Medium
+    | [<CompiledName("high")>] High    
+
+    type ImagePicker =
+        abstract member showImagePicker: ImagePickerOptions * (ImagePickerResult -> unit) -> unit
+        abstract member launchCamera: ImagePickerOptions * (ImagePickerResult -> unit) -> unit
+        abstract member launchImageLibrary: ImagePickerOptions * (ImagePickerResult -> unit) -> unit
+
+    and StorageOptions =
+        abstract skipBackup: bool option with get, set
+        abstract path: string option with get, set
+
+    and ImagePickerOptions =
+        abstract title: string option with get, set
+        abstract cancelButtonTitle: string option with get, set
+        abstract takePhotoButtonTitle: string option with get, set
+        abstract chooseFromLibraryButtonTitle: string option with get, set
+        abstract cameraType: CameraType option with get, set
+        abstract mediaType: MediaType option with get, set
+        abstract maxWidth: int option with get, set
+        abstract maxHeight: int option with get, set
+        abstract quality: float option with get, set
+        abstract videoQuality: VideoQuality option with get, set
+        abstract durationLimit: int option with get, set
+        abstract rotation: int option with get, set
+        abstract allowsEditing: bool option with get, set
+        abstract noData: bool option with get, set
+        abstract storageOptions: StorageOptions option with get, set
+
+    and ImagePickerResult =     
+        abstract didCancel: bool with get, set
+        abstract error: string with get, set
+        abstract data: string with get, set
+        abstract uri: string with get, set
+        abstract origURL: string with get, set
+        abstract isVertical: bool with get, set
+        abstract width: int with get, set
+        abstract height: int with get, set
+        abstract fileSize: int with get, set
+
+    type Globals =
+        [<Import("default", from="react-native-image-picker")>]
+        static member ImagePicker with get(): ImagePicker = failwith "JS only" and set(v: ImagePicker): unit = failwith "JS only"
---
+++ import/react-native-image-picker/README.md
@@ -0,0 +1,42 @@
+# fable-import-react-native-image-picker
+
+Fable bindings for React Native Image Picker
+
+## Installation
+
+```sh
+$ npm install --save react-native react-dom fable-core
+$ npm install --save-dev fable-import-react fable-import-react-native fable-import-react-native-image-picker
+```
+
+## Usage
+
+Follow instructions for [react-native-image-picker](https://github.com/marcshilling/react-native-image-picker).
+
+### In a F# project (.fsproj)
+
+```xml
+  <ItemGroup>
+    <Reference Include="node_modules/fable-core/Fable.Core.dll" />
+    <Compile Include="node_modules/fable-import-react/Fable.Import.React.fs" />    
+    <Compile Include="node_modules/fable-import-react/Fable.Helpers.React.fs" />
+    <Compile Include="node_modules/fable-import-react/Fable.Import.ReactNative.fs" />
+    <Compile Include="node_modules/fable-import-react/Fable.Import.ReactNativeImagePicker.fs" />        
+  </ItemGroup>
+```
+
+### In a F# script (.fsx)
+
+```fsharp
+#r "node_modules/fable-core/Fable.Core.dll"
+#load "node_modules/fable-import-react/Fable.Import.React.fs"
+#load "node_modules/fable-import-react/Fable.Helpers.React.fs"
+#load "node_modules/fable-import-react/Fable.Import.ReactNative.fs"
+#load "node_modules/fable-import-react/Fable.Import.ReactNativeImagePicker.fs"
+
+open Fable.Core
+open Fable.Import
+module R = Fable.Helpers.React
+module RN = Fable.Helpers.ReactNative
+open R.Props
+```
