39ff5a5011bdfe2af5f167cef0d7f5f4754317c1
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-08-04 16:15:16 +0200

    Starting with simple store

--- import/react-native-simple-store/Fable.Helpers.ReactNative.SimpleStore.fs
+++ import/react-native-simple-store/Fable.Helpers.ReactNative.SimpleStore.fs
@@ -1,42 +1,38 @@
 [<Fable.Core.Erase>]
-module internal Fable.Helpers.ReactNative
+module internal Fable.Helpers.SimpleStore
 
 open System
 open Fable.Import.ReactNative
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import
+open Fable.Core.JsInterop
 
-type RN = ReactNative.Globals
+/// Loads a value as string with the given key from the local device storage. Returns None if the key is not found.
+let inline getItem (key:string) = async {
+    let! v = Globals.AsyncStorage.getItem key |> Async.AwaitPromise
+    match v with
+    | null -> return None
+    | _ -> return Some v
+}
 
-module SimpleStore =
-    open Fable.Core.JsInterop
+/// Loads a value with the given key from the local device storage. Returns None if the key is not found.
+let inline load<'a> (key:string) : Async<'a option> = async {
+    let! v = Globals.AsyncStorage.getItem key |> Async.AwaitPromise
+    match v with
+    | null -> return None
+    | _ -> return Some (ofJson v)
+}
 
-    /// Loads a value as string with the given key from the local device storage. Returns None if the key is not found.
-    let inline getItem (key:string) = async {
-        let! v = Globals.AsyncStorage.getItem key |> Async.AwaitPromise
-        match v with
-        | null -> return None
-        | _ -> return Some v
-    }
+/// Saves a value with the given key to the local device storage.
+let inline setItem (k:string) (v:string) = async {
+    let! v = Globals.AsyncStorage.setItem(k,v) |> Async.AwaitPromise
+    ()
+}
 
-    /// Loads a value with the given key from the local device storage. Returns None if the key is not found.
-    let inline load<'a> (key:string) : Async<'a option> = async {
-        let! v = Globals.AsyncStorage.getItem key |> Async.AwaitPromise
-        match v with
-        | null -> return None
-        | _ -> return Some (ofJson v)
-    }
-
-    /// Saves a value with the given key to the local device storage.
-    let inline setItem (k:string) (v:string) = async {
-        let! v = Globals.AsyncStorage.setItem(k,v) |> Async.AwaitPromise
-        ()
-    }
-
-    /// Saves a value with the given key to the local device storage.
-    let inline save<'a> (k:string) (v:'a) = async {
-        let s:string = toJson v
-        let! v = Globals.AsyncStorage.setItem(k,s) |> Async.AwaitPromise
-        ()
-    }    
+/// Saves a value with the given key to the local device storage.
+let inline save<'a> (k:string) (v:'a) = async {
+    let s:string = toJson v
+    let! v = Globals.AsyncStorage.setItem(k,s) |> Async.AwaitPromise
+    ()
+}    
