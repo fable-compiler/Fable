3b44a40bd13c936b84d4289d772090f75c515168
Steffen Forkmann <steffen.forkmann@msu-solutions.de>
2016-08-09 19:57:19 +0200

    Try to make headers a obj

--- import/fetch/Fable.Import.Fetch.fs
+++ import/fetch/Fable.Import.Fetch.fs
@@ -69,7 +69,7 @@ module Fetch =
         /// Returns the HTTP status code
         abstract status: int
         /// Returns the response headers
-        abstract headers: seq<(string*string)>
+        abstract headers: obj
 
     and [<StringEnum; RequireQualifiedAccess>] ResponseType =
         | Basic | Cors | Default | Error | Opaque
