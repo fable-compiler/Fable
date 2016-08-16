193f90aa2c1a7b920dbbc7402dfc739b04bf03d9
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-08-16 13:36:51 +0200

    Don't open System.Text.RegularExpressions in unneeded bindings

--- import/d3/Fable.Import.D3.fs
+++ import/d3/Fable.Import.D3.fs
@@ -6,7 +6,6 @@
 namespace Fable.Import
 
 open System
-open System.Text.RegularExpressions
 open Fable.Core
 open Fable.Import.JS
 open Fable.Import.Browser
--- import/webgl-ext/Fable.Import.WebGLExt.fs
+++ import/webgl-ext/Fable.Import.WebGLExt.fs
@@ -9,7 +9,6 @@
 
 namespace Fable.Import
 open System
-open System.Text.RegularExpressions
 open Fable.Core
 open Fable.Import.JS
 
