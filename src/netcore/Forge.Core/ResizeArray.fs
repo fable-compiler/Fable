2ca3c792e090faf5df559967afd853c2a7caed51
ncave <test2@hotmail.com>
2016-08-26 10:44:32 -0700

    Added Forge.Core

---
+++ src/netcore/Forge.Core/ResizeArray.fs
@@ -0,0 +1,489 @@
+﻿[<RequireQualifiedAccess>]
+module Forge.ResizeArray
+
+/// <summary>
+/// Functional operators related to the System.Collections.Generic.List&lt;T&gt; type (called ResizeArray in F#).
+/// </summary>
+
+open System.Collections.Generic
+open LanguagePrimitives
+open OptimizedClosures
+
+
+/// Determines if a reference is a null reference, and if it is, throws an <see cref="System.ArgumentNullException"/>.
+let inline checkNonNull paramName arg =
+    if isNull arg then
+        if System.String.IsNullOrWhiteSpace paramName then
+            raise ^ System.ArgumentNullException ()
+        else
+            raise ^ System.ArgumentNullException paramName
+
+let keyNotFound (message : string) : 'T =
+    if System.String.IsNullOrEmpty message then
+        raise <| System.Collections.Generic.KeyNotFoundException ()
+    else
+        raise <| System.Collections.Generic.KeyNotFoundException message
+
+    
+let argOutOfRange (paramName : string) (message : string) : 'T =
+    match System.String.IsNullOrEmpty paramName, System.String.IsNullOrEmpty message with
+    | false, false ->
+        raise <| System.ArgumentOutOfRangeException (paramName, message)
+    | false, true ->
+        raise <| System.ArgumentOutOfRangeException (paramName)
+    | true, true ->
+        raise <| System.ArgumentOutOfRangeException ()
+    | true, false ->
+        raise <| System.ArgumentOutOfRangeException ("(Unspecified parameter)", message)
+
+/// Return the length of the collection.
+let inline length (resizeArray : ResizeArray<'T>) : int =
+    resizeArray.Count
+
+/// Return true if the given array is empty, otherwise false.
+let inline isEmpty (resizeArray : ResizeArray<'T>) : bool =
+    resizeArray.Count = 0
+
+/// Fetch an element from the collection.
+let inline get index (resizeArray : ResizeArray<'T>) : 'T =
+    resizeArray.[index]
+
+/// Set the value of an element in the collection.
+let inline set index value (resizeArray : ResizeArray<'T>)  : unit =
+    resizeArray.[index] <- value
+
+/// Create a ResizeArray whose elements are all initially the given value.
+let create count value : ResizeArray<'T> =
+    // Preconditions
+    if count < 0 then
+        invalidArg "count" "The number of elements may not be negative."
+
+    let resizeArray = ResizeArray (count)
+    for i = 0 to count - 1 do
+        resizeArray.Add value
+    resizeArray
+
+/// Create a ResizeArray by calling the given generator on each index.
+let init count initializer : ResizeArray<'T> =
+    // Preconditions
+    if count < 0 then
+        invalidArg "count" "The number of elements may not be negative."
+
+    let resizeArray = ResizeArray (count)
+    for i = 0 to count - 1 do
+        resizeArray.Add <| initializer count
+    resizeArray
+
+/// Adds an object to the end of the ResizeArray.
+let inline add item (resizeArray : ResizeArray<'T>) =
+    resizeArray.Add item
+    resizeArray
+
+
+let inline insert index item (resizeArray : ResizeArray<'T>) =
+    resizeArray.Insert(index, item)
+    resizeArray
+
+
+let inline remove item (resizeArray : ResizeArray<'T>) =
+    resizeArray.Remove item |> ignore
+    resizeArray
+
+
+/// Determines whether an element is in the ResizeArray.
+let inline contains (value : 'T) (resizeArray : ResizeArray<'T>) : bool =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+
+    resizeArray.Contains value
+
+/// Build a ResizeArray from the given sequence.
+let inline ofSeq (sequence : seq<'T>) : ResizeArray<'T> =
+    ResizeArray (sequence)
+
+/// Build a ResizeArray from the given list.
+let ofList (list : 'T list) : ResizeArray<'T> =
+
+    let len = list.Length
+    let res = ResizeArray<_>(len)
+    let rec add = function
+        | [] -> ()
+        | e::l -> res.Add(e); add l
+    add list
+    res
+
+/// Build a ResizeArray from the given array.
+let inline ofArray (arr : 'T[]) : ResizeArray<'T> =
+    ResizeArray (arr)
+
+
+
+/// Return a view of the ResizeArray as an enumerable object.
+let toSeq (resizeArray : ResizeArray<'T>) : seq<'T> =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+
+    Seq.readonly resizeArray
+
+
+/// Build a list from the given ResizeArray.
+let toList (resizeArray : ResizeArray<'T>) : 'T list =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+
+    let mutable res = []
+    for i = length resizeArray - 1 downto 0 do
+        res <- resizeArray.[i] :: res
+    res
+
+
+/// Return a fixed-length array containing the elements of the input ResizeArray.
+let inline toArray (resizeArray : ResizeArray<'T>) : 'T[] =
+    resizeArray.ToArray ()
+    
+
+/// Sorts the elements of the ResizeArray by mutating the ResizeArray in-place.
+/// Elements are compared using Operators.compare.
+let inline sortInPlace<'T when 'T : comparison> (resizeArray : ResizeArray<'T>) : unit =
+    resizeArray.Sort ()
+        
+
+/// Sort the elements using the key extractor and generic comparison on the keys.
+let inline sortInPlaceBy<'T, 'Key when 'Key : comparison>
+        (projection : 'T -> 'Key) (resizeArray : ResizeArray<'T>) =
+    resizeArray.Sort (fun x y ->
+        compare (projection x) (projection y))
+
+
+/// Sort the elements using the given comparison function.
+let inline sortInPlaceWith (comparer : 'T -> 'T -> int) (resizeArray : ResizeArray<'T>) : unit =
+    resizeArray.Sort (comparer)
+
+
+/// Build a new ResizeArray that contains the elements of the given ResizeArray.
+let inline copy (resizeArray : ResizeArray<'T>) : ResizeArray<'T> =
+    ResizeArray (resizeArray)
+
+
+/// Return an array containing the given element.
+let singleton value : ResizeArray<'T> =
+    let resizeArray = ResizeArray ()
+    resizeArray.Add value
+    resizeArray
+
+
+/// Build a new ResizeArray that contains the elements of each of the given sequence of ResizeArrays.
+let concat (resizeArrays : seq<ResizeArray<'T>>) : ResizeArray<'T> =
+    // Preconditions
+    checkNonNull "resizeArrays" resizeArrays
+
+    let flattened = ResizeArray ()
+    for resizeArray in resizeArrays do
+        flattened.AddRange resizeArray
+    flattened
+    
+
+/// Build a new ResizeArray that contains the elements of the first ResizeArray followed by
+/// the elements of the second ResizeArray.
+let append (resizeArray1 : ResizeArray<'T>) (resizeArray2 : ResizeArray<'T>) : ResizeArray<'T> =
+    // Preconditions
+    checkNonNull "resizeArray1" resizeArray1
+    checkNonNull "resizeArray2" resizeArray2
+
+    let combined = ResizeArray (resizeArray1.Count + resizeArray2.Count)
+    combined.AddRange resizeArray1
+    combined.AddRange resizeArray2
+    combined
+
+
+/// Return a new ResizeArray with the elements in reverse order.
+let rev (resizeArray : ResizeArray<'T>) : ResizeArray<'T> =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+
+    let len = length resizeArray
+    let result = ResizeArray (len)
+    for i = len - 1 downto 0 do
+        result.Add resizeArray.[i]
+    result
+
+
+
+/// Test if any element of the array satisfies the given predicate.
+/// If the input function is <c>f</c> and the elements are <c>i0...iN</c> 
+/// then computes <c>p i0 or ... or p iN</c>.
+let inline exists (predicate : 'T -> bool) (resizeArray : ResizeArray<'T>) : bool =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+
+    resizeArray.Exists (System.Predicate predicate)
+
+
+/// Test if all elements of the array satisfy the given predicate.
+/// If the input function is <c>f</c> and the elements are <c>i0...iN</c> and "j0...jN"
+/// then computes <c>p i0 && ... && p iN</c>.
+let inline forall (predicate : 'T -> bool) (resizeArray : ResizeArray<'T>) : bool =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+    resizeArray.TrueForAll (System.Predicate predicate)
+
+
+
+/// Return a new collection containing only the elements of the collection
+/// for which the given predicate returns <c>true</c>.
+let inline filter (predicate : 'T -> bool) (resizeArray : ResizeArray<'T>) : ResizeArray<'T> =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+    resizeArray.FindAll (System.Predicate predicate)
+
+
+/// <summary>
+/// Apply the given function to each element of the array. Return
+/// the array comprised of the results "x" for each element where
+/// the function returns <c>Some(x)</c>.
+/// </summary>
+let choose (chooser : 'T -> 'U option) (resizeArray : ResizeArray<'T>) : ResizeArray<'U> =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+
+    // OPTIMIZATION : If the input list is empty return immediately.
+    if isEmpty resizeArray then
+        ResizeArray ()
+    else
+        let result = ResizeArray ()
+        let count = resizeArray.Count
+
+        for i = 0 to count - 1 do
+            match chooser resizeArray.[i] with
+            | None -> ()
+            | Some value ->
+                result.Add value
+
+        result
+
+
+/// <summary>
+/// Return the first element for which the given function returns <c>true</c>.
+/// Return <c>None</c> if no such element exists.
+/// </summary>
+let tryFind (predicate : 'T -> bool) (resizeArray : ResizeArray<'T>) : 'T option =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+    match resizeArray.FindIndex ^ System.Predicate predicate with
+    | -1    -> None
+    | index -> Some resizeArray.[index]
+
+
+/// <summary>
+/// Return the first element for which the given function returns <c>true</c>.
+/// Raise <c>KeyNotFoundException</c> if no such element exists.
+/// </summary>
+let find (predicate : 'T -> bool) (resizeArray : ResizeArray<'T>) : 'T =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+    match resizeArray.FindIndex (System.Predicate predicate) with
+    | -1    -> raise <| System.Collections.Generic.KeyNotFoundException ()
+    | index -> resizeArray.[index]
+
+
+/// Return the index of the first element in the array
+/// that satisfies the given predicate.
+let tryFindIndex (predicate : 'T -> bool) (resizeArray : ResizeArray<'T>) : int option =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+    match resizeArray.FindIndex ^ System.Predicate predicate with
+    | -1    ->  None
+    | index -> Some index
+
+        
+/// <summary>
+/// Return the index of the first element in the array
+/// that satisfies the given predicate. Raise <c>KeyNotFoundException</c> if 
+/// none of the elements satisfy the predicate.
+/// </summary>
+let findIndex (predicate : 'T -> bool) (resizeArray : ResizeArray<'T>) : int =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+    match resizeArray.FindIndex ^ System.Predicate predicate with
+    | -1    ->  raise <| System.Collections.Generic.KeyNotFoundException ()
+    | index -> index
+
+
+let indexOf elem (resizeArray : ResizeArray<'T>) =
+    resizeArray.FindIndex ^ System.Predicate ((=) elem)
+
+
+/// Return the index of the first element in the array
+/// that satisfies the given predicate.
+let tryFindIndexi predicate (resizeArray : ResizeArray<'T>) : int option =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+
+    let predicate = FSharpFunc<_,_,_>.Adapt predicate
+
+    let lastIndex = length resizeArray - 1
+    let mutable index = -1
+    let mutable foundMatch = false
+    while index < lastIndex && not foundMatch do
+        let i = index + 1
+        index <- i
+        foundMatch <- predicate.Invoke (i, resizeArray.[i])
+
+    if foundMatch then
+        Some index
+    else None
+
+/// <summary>
+/// Return the index of the first element in the array
+/// that satisfies the given predicate. Raise <c>KeyNotFoundException</c> if 
+/// none of the elements satisfy the predicate.
+/// </summary>
+let findIndexi predicate (resizeArray : ResizeArray<'T>) : int =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+
+    match tryFindIndexi predicate resizeArray with
+    | Some index ->
+        index
+    | None ->
+        keyNotFound "An element satisfying the predicate was not found in the collection."
+
+/// <summary>
+/// Applies the given function to successive elements, returning the first
+/// result where function returns <c>Some(x)</c> for some x. If the function
+/// never returns <c>Some(x)</c>, returns <c>None</c>.
+/// </summary>
+let tryPick (picker : 'T -> 'U option) (resizeArray : ResizeArray<'T>) : 'U option =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+
+    let count = resizeArray.Count
+    let mutable result = None
+    let mutable index = 0
+
+    while index < count && Option.isNone result do
+        result <- picker resizeArray.[index]
+        index <- index + 1
+    result
+
+
+let swap (index1:int) (index2:int) (resizeArray : ResizeArray<'T>) =
+    checkNonNull "resizeArray" resizeArray
+    if index1 < 0 || index1 > resizeArray.Count then
+        argOutOfRange "index1" (sprintf "'%i' - is outside the range of the resizeArray" index1)
+    if index2 < 0 || index2 > resizeArray.Count then
+        argOutOfRange "index1" (sprintf "'%i' - is outside the range of the resizeArray" index2)
+
+    let t1,t2 = resizeArray.[index1], resizeArray.[index2]
+    resizeArray.[index1] <- t2
+    resizeArray.[index2] <- t1
+    resizeArray
+
+/// <summary>
+/// Applies the given function to successive elements, returning the first
+/// result where function returns <c>Some(x)</c> for some x. If the function
+/// never returns <c>Some(x)</c>, raises KeyNotFoundException.
+/// </summary>
+let pick (picker : 'T -> 'U option) (resizeArray : ResizeArray<'T>) : 'U =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+
+    let count = resizeArray.Count
+    let mutable result = None
+    let mutable index = 0
+
+    while index < count && Option.isNone result do
+        result <- picker resizeArray.[index]
+        index <- index + 1
+
+    match result with
+    | Some result ->
+        result
+    | None ->
+        // TODO : Return a better error message
+        //keyNotFound ""
+        raise <| System.Collections.Generic.KeyNotFoundException ()
+
+/// Apply the given function to each element of the array.
+let iter (action : 'T -> unit) (resizeArray : ResizeArray<'T>) : unit =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+
+    let count = resizeArray.Count
+    for i = 0 to count - 1 do
+        action resizeArray.[i]
+
+/// Apply the given function to each element of the array. The integer passed to the
+/// function indicates the index of element.
+let iteri (action : int -> 'T -> unit) (resizeArray : ResizeArray<'T>) : unit =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+
+    let action = FSharpFunc<_,_,_>.Adapt action
+
+    let count = resizeArray.Count
+    for i = 0 to count - 1 do
+        action.Invoke (i, resizeArray.[i])
+
+
+
+/// <summary>
+/// Build a new array whose elements are the results of applying the given function
+/// to each of the elements of the array.
+/// </summary>
+/// <param name="mapping"></param>
+/// <param name="resizeArray"></param>
+/// <returns></returns>
+let inline map (mapping : 'T -> 'U) (resizeArray : ResizeArray<'T>) : ResizeArray<'U> =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+
+    let len = length resizeArray
+    let res = ResizeArray<_>(len)
+    for i = 0 to len - 1 do
+        res.Add(mapping resizeArray.[i])
+    res    
+
+
+
+/// <summary>
+/// Split the collection into two collections, containing the elements for which
+/// the given predicate returns <c>true</c> and <c>false</c> respectively.
+/// </summary>
+/// <param name="predicate"></param>
+/// <param name="resizeArray"></param>
+/// <returns></returns>
+let partition predicate (resizeArray : ResizeArray<'T>) : ResizeArray<'T> * ResizeArray<'T> =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+
+    let trueResults = ResizeArray ()
+    let falseResults = ResizeArray ()
+
+    let len = length resizeArray
+    for i = 0 to len - 1 do
+        let el = resizeArray.[i]
+        if predicate el then
+            trueResults.Add el
+        else
+            falseResults.Add el
+
+    trueResults, falseResults
+
+
+/// <summary>Returns the greatest of all elements of the ResizeArray, compared via Operators.max on the function result.</summary>
+/// <param name="resizeArray">The input ResizeArray.</param>
+/// <returns>The maximum element.</returns>
+/// <exception cref="System.ArgumentException">Thrown when <paramref name="resizeArray"/> is empty.</exception>
+let inline max (resizeArray : ResizeArray<'T>) =
+    // Preconditions
+    checkNonNull "resizeArray" resizeArray
+    if resizeArray.Count = 0 then
+        invalidArg "resizeArray" "The input collection is empty."
+
+    let mutable acc = resizeArray.[0]
+    for i = 1 to resizeArray.Count - 1 do
+        let curr = resizeArray.[i]
+        if curr > acc then
+            acc <- curr
+    acc
---
+++ src/netcore/Forge.Core/XLinq.fs
@@ -0,0 +1,244 @@
+﻿[<AutoOpen>]
+module Forge.XLinq
+open System.Runtime.CompilerServices
+open System.Xml.Linq
+
+
+
+let xattr (name:string) value    = XAttribute (XName.Get name, value)
+
+
+let inline localName x = (^a:(member Name:XName) x).LocalName
+
+let inline private matchName (name:string) x = name = (localName x)
+
+/// Helper function to filter a seq of XElements by matching their local name against the provided string
+let inline private nameFilter name sqs = sqs |> Seq.filter ^ matchName name
+
+let inline private hasNamed name sqs = sqs |> Seq.exists ^ matchName name
+
+let inline private getNamed name sqs = sqs |> Seq.find ^ matchName name
+
+let  inline private tryGetNamed name sqs = 
+    (None, sqs) ||> Seq.fold (fun acc elm ->
+        match acc with
+        | Some _ -> acc
+        | None   -> if matchName name elm then Some elm else None
+    )
+
+
+[<RequireQualifiedAccess>]
+module XDoc =
+
+    let elements (xdoc:#XDocument) = xdoc.Elements()
+
+    let hasElement name (xdoc:#XDocument) =
+        elements xdoc |> hasNamed name
+
+    let getElement name (xdoc:#XDocument)  =
+        elements xdoc |> getNamed name
+
+    let tryGetElement name (xdoc:#XDocument)  =
+        elements xdoc |> tryGetNamed name
+
+    let getElements name (xdoc:#XDocument)  =
+        elements xdoc |> nameFilter name
+
+
+[<RequireQualifiedAccess>]
+/// Functions for operating on XNodes
+module XNode =
+
+    /// Returns a seq of XElements that precede the XNode 
+    let elementsBefore  (node:#XNode) = node.ElementsBeforeSelf()
+
+    /// Returns a seq of XElements that follow the XNode
+    let elementsAfter   (node:#XNode) = node.ElementsAfterSelf()
+  
+    /// Returns a seq of XElements that follow the XNode with a local name matching `name`
+    let elementsAfterNamed (node:#XNode) name =
+        elementsAfter node |> nameFilter name
+
+    /// Returns a seq of XElements that precede the XNode with a local name matching `name`
+    let elementsBeforeNamed (node:#XNode) name =
+        elementsBefore node |> nameFilter name
+
+    /// Returns seq of the XNode's ancestor XElements
+    let ancestors (node:#XNode) = node.Ancestors()
+
+    /// Returns the first ancestor of the XNode with a local name matching `name`
+    let ancestorNamed (name:string) (node:#XNode) =
+        ancestors node |> tryGetNamed name
+
+    /// Returns all ancestors of the XNode with a local name matching `name`
+    let ancestorsNamed (name:string) (node:#XNode) =
+        ancestors node |> nameFilter name 
+
+    /// Insert a sibling XNode before `node`
+    let addBefore (insert:#XNode) (node:#XNode) =
+        node.AddBeforeSelf insert
+        node
+    
+    /// Insert a sibling XNode after `node`
+    let addAfter (insert:#XNode) (node:#XNode) =
+        node.AddAfterSelf insert
+        node
+
+        
+    let next (node:#XNode) = node.NextNode        
+    let previous (node:#XNode) = node.PreviousNode
+    let parent (node:#XNode) = node.Parent
+
+    let isBefore (target:#XNode) (node:#XNode) = node.IsBefore target
+    let isAfter (target:#XNode)  (node:#XNode) = node.IsAfter target
+
+
+[<RequireQualifiedAccess>]
+/// Functions for operating on XContainers
+module XCont =
+
+    let descendants (cont:#XContainer) = cont.Descendants()
+    
+    let descendantNamed name (cont:#XContainer) =
+        descendants cont |> tryGetNamed name
+
+    let descendantsNamed name (cont:#XContainer) = 
+        descendants cont |> nameFilter name
+    
+    let elements (cont:#XContainer) = cont.Elements()
+
+    let hasElement name (cont:#XContainer) =
+        elements cont |> hasNamed name
+
+    let getElement name (cont:#XContainer) =
+        elements cont |> getNamed name
+
+    let tryGetElement name (cont:#XContainer) =
+        elements cont |> tryGetNamed name
+
+    let getElements name (cont:#XContainer) =
+        elements cont |> nameFilter name
+
+    let nodes (cont:#XContainer) = cont.Nodes()
+
+
+[<RequireQualifiedAccess>]
+module XAttr =
+    let value (xattr:XAttribute) = xattr.Value
+    let parent (xattr:XAttribute) = xattr.Parent
+    let previous (xattr:XAttribute) = xattr.PreviousAttribute
+    let next (xattr:XAttribute) = xattr.NextAttribute
+
+[<RequireQualifiedAccess>]
+/// Functions for operating on XElements
+module XElem =
+
+    let inline isNamed name (xelem:#XElement) = 
+        matchName name xelem
+
+    let inline notNamed name (xelem:#XElement) = 
+        not ^ matchName name xelem
+
+    let create (name:string) (content:seq<'a>) = 
+        XElement (XName.Get name, Seq.toArray content)
+
+    let value (xelem:#XElement) = xelem.Value
+
+    let nodes (xelem:#XElement) = xelem.Nodes()
+    
+    let descendants (xelem:#XElement) = xelem.Descendants()
+    
+    let descendantNamed name (xelem:#XElement) =
+        descendants xelem |> tryGetNamed name
+
+    let descendantsNamed name (xelem:#XElement) = 
+        descendants xelem |> nameFilter name
+    
+    let elements (xelem:#XElement) = xelem.Elements()
+
+    let hasElement name (xelem:#XElement) =
+        elements xelem |> hasNamed name
+
+    let getElement name (xelem:#XElement) =
+        elements xelem |> getNamed name
+
+    let getElementValue name (xelem:#XElement) =
+        elements xelem |> getNamed name |> value
+
+    let tryGetElement name (xelem:#XElement) =
+        elements xelem |> tryGetNamed name
+
+    let tryGetElementValue name (xelem:#XElement) =
+        elements xelem |> tryGetNamed name |> Option.map value
+
+    let getElements name (xelem:#XElement) =
+        elements xelem |> nameFilter name
+
+    let attributes (xelem:#XElement) =
+        xelem.Attributes()
+    
+    let hasAttribute name (xelem:#XElement) =
+        attributes xelem |> hasNamed name
+
+    let getAttribute name (xelem:#XElement) =
+        xelem.Attribute ^ XName.Get name
+
+    let getAttributeValue name (xelem:#XElement) =
+        xelem.Attribute ^ XName.Get name |> XAttr.value
+
+    let tryGetAttribute name (xelem:#XElement) =
+        attributes xelem |> tryGetNamed name
+
+    let tryGetAttributeValue name (xelem:#XElement) =
+        tryGetAttribute name xelem |> Option.map XAttr.value
+
+    let setAttribute name value (xelem:#XElement) =
+        xelem.SetAttributeValue(XName.Get name, value)
+        xelem
+    
+    let addAttribute (xattr:#XAttribute) (xelem:#XElement) =
+        xelem.Add xattr
+        xelem
+
+    let setElement name value (xelem:#XElement) =
+        xelem.SetElementValue(XName.Get name, value)
+        xelem
+
+    let addElement (child:XElement) (parent:XElement) =
+        parent.Add child
+        parent
+
+    let addElements (children:#seq<XElement>) (parent:XElement) =
+        parent.Add children
+        parent
+
+    /// Creates a new XElement and adds it as a child
+    let inline addElem elmName value xelem =
+        addElement (create elmName [value]) xelem
+
+
+
+    
+[<Extension>]
+type XLinqSeqExtensions =
+    [<Extension>] 
+    static member Ancestors (source:seq<#XNode>) name = source.Ancestors ^ XName.Get name
+
+    [<Extension>] 
+    static member AncestorsAndSelf (source:seq<XElement>)  name =  source.AncestorsAndSelf ^ XName.Get name
+
+    [<Extension>] 
+    static member Attributes (source:seq<XElement>)  name =  source.Attributes ^ XName.Get name
+
+    [<Extension>] 
+    static member Descendants (source:seq<#XContainer>) name = source.Descendants ^ XName.Get name
+
+    [<Extension>] 
+    static member DescendantsAndSelf (source:seq<XElement>) name =  source.DescendantsAndSelf ^ XName.Get name
+
+    [<Extension>] 
+    static member Elements (source:seq<#XContainer>) name =  source.Elements ^ XName.Get name
+
+
+
+
