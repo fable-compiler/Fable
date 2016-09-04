bc7066b613ae7fcdf531a91249687aa86a5491c6
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-09-05 01:17:51 +0200

    Sample completed (but not working)

---
+++ samples/browser/redux-todomvc/redux-todomvc.fsx
@@ -0,0 +1,353 @@
+(**
+ - title: React-Redux TodoMVC with Fable
+ - tagline: Use Redux development tools with Fable
+ - app-style: width:800px; margin:20px auto 50px auto;
+ - require-paths: `'react': 'lib/react/react-with-addons', 'react-dom': 'lib/react/react-dom', 'redux': 'lib/redux/redux', 'react-redux': 'lib/redux/react-redux'`
+ - intro: This is a port of [React-Redux TodoMVC](https://github.com/reactjs/redux/tree/master/examples/todomvc) to show how easy
+   is to take advantage of the full power of [React](https://facebook.github.io/react/) and [Redux](http://redux.js.org/) in Fable apps.
+   You can also compare the [F# source code](https://github.com/fable-compiler/Fable/blob/master/samples/browser/react-redux/react-redux.fsx)
+   with the [original JS implementation](https://github.com/reactjs/redux/tree/master/examples/todomvc)
+   to see the advantages of Fable programming. And you can install the [Redux DevTools](http://zalmoxisus.github.io/redux-devtools-extension/)
+   to get the full debugging experience of modern JS development with Fable!
+*)
+
+(**
+## JavaScript bindings and helpers
+
+Fable includes [React bindings and helpers](https://www.npmjs.com/package/fable-import-react)
+to make interaction with the tool more idiomatic in F#.
+*)
+
+#r "node_modules/fable-core/Fable.Core.dll"
+#load "node_modules/fable-import-react/Fable.Import.React.fs"
+#load "node_modules/fable-import-react/Fable.Helpers.React.fs"
+
+module Redux =
+    open Fable.Import
+    open Fable.Core
+    open Fable.Core.JsInterop
+
+    type IStore<'TState, 'TAction> = interface end
+
+    let [<Import("createStore","redux")>] private createStore' = obj()
+
+    let inline createStore (reducer: 'TState->'TAction->'TState) (initState: 'TState) =
+        createStore'$((fun state action ->
+            match box action?Case with
+            | :? string -> reducer state action
+            | _ -> state), initState, unbox Browser.window?devToolsExtension
+                                      && unbox (Browser.window?devToolsExtension()))
+        |> unbox<IStore<'TState, 'TAction>>
+
+    let dispatch (store: IStore<'TState, 'TAction>) (x: 'TAction) =
+        let x = toPlainJsObj x
+        x?``type`` <- x?Case
+        store?dispatch(x) |> ignore
+
+    let inline subscribe (store: IStore<'TState, 'TAction>) (f: unit->unit) =
+        store?subscribe(f)
+
+    let inline getState (store: IStore<'TState, 'TAction>) =
+        store?getState() |> unbox<'TState>
+
+module ReactRedux =
+    open Redux
+    open Fable.Core
+    open Fable.Core.JsInterop
+    open Fable.Import
+
+    let [<Import("connect","react-redux")>] private connect' = obj()
+
+    let inline connect<'T,'S,'A,'P,'a,'b when 'T :> React.Component<'P,obj>>
+            (mapState:'S->'a) (mapDispatch:('A->unit)->'b) (merge:'a->'b->'P) =
+        connect'$(mapState, mapDispatch, merge)$(typeof<'T>)
+        |> unbox<React.ReactElement<obj>>
+
+    [<Import("Provider","react-redux")>]
+    type private ProviderType() =
+        inherit React.Component<obj,obj>()
+
+    let inline provider (store: IStore<'TState, 'TAction>) app =
+        Fable.Helpers.React.com<ProviderType,_,_> (createObj ["store"==>store]) [app]
+
+open System
+open Fable.Core
+open Fable.Core.JsInterop
+open Fable.Import
+
+module R = Fable.Helpers.React
+open R.Props
+
+type Todo = { Text: string; Completed: bool; Id: int } 
+
+type TodoAction =
+    | AddTodo of text:string
+    | DeleteTodo of id:int
+    | EditTodo of id:int * text:string
+    | CompleteTodo of id:int
+    | CompleteAll
+    | ClearCompleted
+
+type TodoFilter =
+    | ShowAll = 0
+    | ShowActive = 1
+    | ShowCompleted = 2
+
+let [<Literal>] ESCAPE_KEY = 27.
+let [<Literal>] ENTER_KEY = 13.
+
+let classNames =
+    List.choose (fun (txt,add) -> if add then Some txt else None)
+    >> String.concat " "
+
+type TodoTextInputProps =
+    abstract OnSave: string->unit
+    abstract Text: string option
+    abstract Placeholder: string
+    abstract Editing: bool
+    abstract NewTodo: bool
+
+type TodoTextInputState = { Text: string }
+
+type TodoTextInput(props, ctx) as this =
+    inherit React.Component<TodoTextInputProps, TodoTextInputState>(props, ctx)
+    do this.state <- { Text = defaultArg props.Text "" } 
+
+    member this.HandleSubmit(e: React.KeyboardEvent) =
+        if e.which = ENTER_KEY then
+            let text = (unbox<string> e.target?value).Trim()
+            this.props.OnSave(text)
+            if this.props.NewTodo then
+                this.setState({ Text = "" })
+
+    member this.HandleChange(e: React.SyntheticEvent) =
+        this.setState({ Text = unbox e.target?value })
+
+    member this.HandleBlur(e: React.SyntheticEvent) =
+        if not this.props.NewTodo then
+            this.props.OnSave(unbox e.target?value)
+
+    member this.render() =
+        R.input [
+            ClassName(
+                classNames [
+                    "edit", this.props.Editing
+                    "new-todo", this.props.NewTodo
+                ])
+            Type "text"
+            OnBlur this.HandleBlur
+            OnChange this.HandleChange
+            OnKeyDown this.HandleSubmit
+            AutoFocus (unbox this.state.Text)
+            unbox("placeHolder", this.props.Placeholder)
+        ] []
+
+type TodoItemProps =
+    abstract Todo: Todo
+    abstract EditTodo: int * string -> unit
+    abstract DeleteTodo: int -> unit
+    abstract CompleteTodo: int -> unit
+
+type TodoItemState = { Editing: bool }
+
+type TodoItem(props, ctx) as this =
+    inherit React.Component<TodoItemProps, TodoItemState>(props, ctx)
+    do this.state <- { Editing = false } 
+
+    member this.HandleDoubleClick(_) =
+        this.setState({ Editing = true })
+
+    member this.HandleSave(id, text: string) =
+        if text.Length = 0
+        then this.props.DeleteTodo(id)
+        else this.props.EditTodo(id, text)
+        this.setState({ Editing = false })
+
+    member this.render() =
+        let element =
+            if this.state.Editing
+            then R.com<TodoTextInput,_,_>
+                    { new TodoTextInputProps with
+                        member __.OnSave(text: string) =
+                            this.HandleSave(this.props.Todo.Id, text)
+                        member __.Editing = this.state.Editing
+                        member __.Text = Some this.props.Todo.Text
+                        member __.Placeholder = ""
+                        member __.NewTodo = false } []
+            else R.div [ClassName "view"] [
+                    R.input [
+                        ClassName "toggle"
+                        Type "checkbox"
+                        Checked this.props.Todo.Completed
+                        OnChange (fun _ -> this.props.CompleteTodo(this.props.Todo.Id))
+                    ] []
+                    R.label [OnDoubleClick this.HandleDoubleClick]
+                            [unbox this.props.Todo.Text]
+                    R.div [
+                        ClassName "destroy"
+                        OnClick (fun _ -> this.props.DeleteTodo(this.props.Todo.Id))
+                    ] []
+                ]
+        R.li [ClassName(
+                classNames [
+                    "completed", this.props.Todo.Completed
+                    "editing", this.state.Editing])]
+             [element]
+
+type HeaderProps = { AddTodo: string->unit }
+
+let Header (props: HeaderProps) =
+    R.header [ClassName "header"] [
+        R.h1 [] [unbox "todos"]
+        R.com<TodoTextInput,_,_>
+            { new TodoTextInputProps with
+                member __.OnSave(text: string) =
+                    if text.Length <> 0 then
+                        props.AddTodo text
+                member __.Placeholder = "What needs to be done?"
+                member __.NewTodo = true
+                member __.Text = None
+                member __.Editing = false } []
+    ]
+
+type FooterProps =
+    abstract ActiveCount: int
+    abstract CompletedCount: int
+    abstract Filter: TodoFilter
+    abstract OnShow: TodoFilter->unit
+    abstract OnClearCompleted: React.SyntheticEvent->unit
+
+let Footer =
+    let filterTitles =
+        dict [
+            TodoFilter.ShowAll, "All"
+            TodoFilter.ShowActive, "Active"
+            TodoFilter.ShowCompleted, "Completed"
+        ]
+    let renderTodoCount activeCount =
+        [R.span [ClassName "todo-count"] [
+            unbox(sprintf "%s item%s left"
+                (if activeCount > 0 then string activeCount else "No")
+                (if activeCount <> 1 then "s" else ""))
+        ]]
+    let renderFilterLink filter selectedFilter onShow =
+        [R.a [
+            ClassName (classNames ["selected", filter = selectedFilter])
+            Style [unbox("cursor", "pointer")]
+            OnClick (fun _ -> onShow filter)
+        ] [unbox filterTitles.[filter]]]
+    let renderClearButton completedCount onClearCompleted =
+        if completedCount > 0
+        then [R.button [ClassName "clear-completed"
+                        OnClick onClearCompleted]
+                       [unbox "Clear completed"]]
+        else []
+    fun (props: FooterProps) ->
+        let listItems =
+            [TodoFilter.ShowAll; TodoFilter.ShowActive; TodoFilter.ShowCompleted]
+            |> List.map (fun filter ->
+                R.li [Key (string filter)] (renderFilterLink filter props.Filter props.OnShow))
+        R.footer [ClassName "footer"] <|
+            renderTodoCount props.ActiveCount
+            @ [R.ul [ClassName "filters"] listItems]
+            @ renderClearButton props.CompletedCount props.OnClearCompleted
+
+type MainSectionProps = { Todos: Todo[]; Dispatch: TodoAction->unit }
+type MainSectionState = { Filter: TodoFilter }
+
+type MainSection(props, ctx) as this =
+    inherit React.Component<MainSectionProps, MainSectionState>(props, ctx)
+    let todoFilters =
+        dict [
+            TodoFilter.ShowAll, fun _ -> true
+            TodoFilter.ShowActive, fun (todo: Todo) -> not todo.Completed
+            TodoFilter.ShowCompleted, fun todo -> todo.Completed
+        ]
+    do this.state <- { Filter = TodoFilter.ShowAll } 
+
+    member this.HandleClearCompleted() =
+        this.props.Dispatch(ClearCompleted)
+
+    member this.HandleShow(filter) =
+        this.setState({ Filter = filter })
+
+    member this.renderToggleAll(completedCount) =
+        if this.props.Todos.Length > 0
+        then [R.input [
+                ClassName "toggle-all"
+                Type "checkbox"
+                Checked (completedCount = this.props.Todos.Length)
+                OnChange (fun _ -> this.props.Dispatch(CompleteAll))
+            ] []]
+        else []
+
+    member this.renderFooter(completedCount) =
+        if this.props.Todos.Length > 0
+        then [R.fn Header (failwith "") []]
+        else []
+
+    member this.render() =
+        let filteredTodos =
+            this.props.Todos
+            |> Array.filter todoFilters.[this.state.Filter]
+            |> Array.toList
+        let completedCount =
+            (0, this.props.Todos) ||> Array.fold (fun count todo ->
+                if todo.Completed then count + 1 else count)
+        R.section [ClassName "main"] <|
+            this.renderToggleAll completedCount
+            @ [R.ul [ClassName "todo-list"]
+                (filteredTodos
+                |> List.map (fun todo ->
+                    R.com<TodoItem,_,_>
+                        { new TodoItemProps with
+                            member __.Todo = todo
+                            member __.EditTodo(id, text) = this.props.Dispatch(EditTodo(id, text))
+                            member __.DeleteTodo(id) = this.props.Dispatch(DeleteTodo id)
+                            member __.CompleteTodo(id) = this.props.Dispatch(CompleteTodo id) } []))]
+            @ this.renderFooter completedCount
+
+type App() =
+    inherit React.Component<MainSectionProps, obj>()
+    member this.render() =
+        R.div [] [
+            unbox "Hello"
+            // R.fn Header { AddTodo = fun text -> this.props.Dispatch(AddTodo text) } []
+            // R.com<MainSection,_,_> this.props []
+        ]
+
+let app =
+    ReactRedux.connect<App,Todo[],TodoAction,_,_,_>
+        id id (fun todos dispatch -> { Todos=todos; Dispatch=dispatch })
+
+let reducer (state: Todo[]) = function
+    | AddTodo text ->
+        let id = (-1, state) ||> Array.fold (fun id todo -> max id todo.Id)
+        Array.append [|{Id=id; Completed=false; Text=text}|] state
+    | DeleteTodo id ->
+        state |> Array.filter (fun todo -> todo.Id <> id) 
+    | EditTodo(id, text) ->
+        state |> Array.map(fun todo ->
+            if todo.Id = id
+            then { todo with Text=text }
+            else todo)
+    | CompleteTodo id ->
+        state |> Array.map(fun todo ->
+            if todo.Id = id
+            then { todo with Completed=not todo.Completed }
+            else todo)
+    | CompleteAll ->
+        let areAllMarked = state |> Array.forall (fun todo -> todo.Completed)
+        state |> Array.map(fun todo -> { todo with Completed=not areAllMarked})
+    | ClearCompleted ->
+        state |> Array.filter (fun todo -> not todo.Completed) 
+
+let store =
+    { Text="Use Fable + React + Redux"; Completed=false; Id=0}
+    |> Array.singleton
+    |> Redux.createStore reducer
+
+ReactDom.render(
+    ReactRedux.provider store app,
+    Browser.document.getElementsByClassName("todoapp").[0]
+) |> ignore
