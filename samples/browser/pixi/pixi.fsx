ad0cb642a50a274e15649f2841ffa62d64db8de4
Max Wilson <maxw@microsoft.com>
2016-09-10 15:03:13 -0700

    Fix compile error in Pixi.fs

--- samples/browser/pixi/pixi.fsx
+++ samples/browser/pixi/pixi.fsx
@@ -5,6 +5,7 @@
 open Fable.Core
 open Fable.Import
 open Fable.Import.Browser
+open Fable.Core.JsInterop
 
 // You can use either `new PIXI.WebGLRenderer`, `new PIXI.CanvasRenderer`, or `PIXI.autoDetectRenderer`
 // which will try to choose the best renderer for the environment you are in.
