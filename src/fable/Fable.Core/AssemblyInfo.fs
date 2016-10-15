3ed28172d90342fbfe5c19db9663eda10d0469f2
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2016-10-15 02:36:15 +0200

    Update FCS

--- src/fable/Fable.Core/AssemblyInfo.fs
+++ src/fable/Fable.Core/AssemblyInfo.fs
@@ -2,9 +2,8 @@
 namespace System
 open System.Reflection
 
-[<assembly: AssemblyVersionAttribute("0.7.2")>]
+[<assembly: AssemblyVersionAttribute("0.7.3")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.7.2"
-    let [<Literal>] InformationalVersion = "0.7.2"
+    let [<Literal>] AssemblyVersion = "0.7.3"
