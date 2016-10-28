aeb94e5e09c5acac12e3d238f724963c83e3b4ad
Pauan <pcxunlimited@gmail.com>
2016-10-28 02:05:09 -1000

    Optimizing equal and compare when their arguments are referencially equal (#512)

--- src/fable/Fable.Core/ts/List.ts
+++ src/fable/Fable.Core/ts/List.ts
@@ -121,30 +121,42 @@ export default class List<T> implements IEquatable<List<T>>, IComparable<List<T>
   }
 
   Equals(x: List<T>) {
-    const iter1 = this[Symbol.iterator](), iter2 = x[Symbol.iterator]();
-    for (let i = 0; ; i++) {
-      let cur1 = iter1.next(), cur2 = iter2.next();
-      if (cur1.done)
-        return cur2.done ? true : false;
-      else if (cur2.done)
-        return false;
-      else if (!equals(cur1.value, cur2.value))
-        return false
+    // Optimization if they are referencially equal
+    if (this === x) {
+      return true;
+
+    } else {
+      const iter1 = this[Symbol.iterator](), iter2 = x[Symbol.iterator]();
+      for (;;) {
+        let cur1 = iter1.next(), cur2 = iter2.next();
+        if (cur1.done)
+          return cur2.done ? true : false;
+        else if (cur2.done)
+          return false;
+        else if (!equals(cur1.value, cur2.value))
+          return false
+      }
     }
   }
 
   CompareTo(x: List<T>) {
-    let acc = 0;
-    const iter1 = this[Symbol.iterator](), iter2 = x[Symbol.iterator]();
-    for (let i = 0; ; i++) {
-      let cur1 = iter1.next(), cur2 = iter2.next();
-      if (cur1.done)
-        return cur2.done ? acc : -1;
-      else if (cur2.done)
-        return 1;
-      else {
-        acc = compare(cur1.value, cur2.value);
-        if (acc != 0) return acc;
+    // Optimization if they are referencially equal
+    if (this === x) {
+      return 0;
+
+    } else {
+      let acc = 0;
+      const iter1 = this[Symbol.iterator](), iter2 = x[Symbol.iterator]();
+      for (;;) {
+        let cur1 = iter1.next(), cur2 = iter2.next();
+        if (cur1.done)
+          return cur2.done ? acc : -1;
+        else if (cur2.done)
+          return 1;
+        else {
+          acc = compare(cur1.value, cur2.value);
+          if (acc != 0) return acc;
+        }
       }
     }
   }
