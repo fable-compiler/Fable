9405fd32da1880a98dca801bcd070ee0d98bb7b6
David Teasdale <david@teasdale.me>
2016-11-21 15:23:31 +0000

    Add a round function Add some tests

--- src/fable/Fable.Core/ts/Util.ts
+++ src/fable/Fable.Core/ts/Util.ts
@@ -304,3 +304,12 @@ export function toPlainJsObj(source: any) {
     return source;
   }
 }
+
+export function round(value : number, digits: number = 0) {
+    const m = Math.pow(10, digits);
+    const n = +(digits ? value * m : value).toFixed(8);
+    const i = Math.floor(n), f = n - i;
+    const e = 1e-8;
+    const r = (f > 0.5 - e && f < 0.5 + e) ? ((i % 2 == 0) ? i : i + 1) : Math.round(n);
+    return digits ? r / m : r;
+}
