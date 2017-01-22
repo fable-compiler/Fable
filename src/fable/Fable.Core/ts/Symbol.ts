4329af6544f8db2a5696d40758e93dac762ca57a
Alessandro Rizzotto <easysoft2k15@gmail.com>
2017-01-22 11:37:09 +0100

    Use {} when Window, Global and Self are undefined

--- src/fable/Fable.Core/ts/Symbol.ts
+++ src/fable/Fable.Core/ts/Symbol.ts
@@ -10,7 +10,7 @@ const fableGlobal: {
   const globalObj =
     typeof window !== "undefined" ? window
     : (typeof global !== "undefined" ? global
-    : (typeof self !== "undefined" ? self : null));
+    : (typeof self !== "undefined" ? self : {}));
   if (typeof globalObj.__FABLE_CORE__ === "undefined") {
     globalObj.__FABLE_CORE__ = {
       types: new Map<string, FunctionConstructor>(),
