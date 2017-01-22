26e81af6a532b531712108a948ac02f3c900e7cb
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-01-22 15:48:08 +0100

    Remove __FABLE_CORE__ global object

--- src/fable/Fable.Core/ts/Symbol.ts
+++ src/fable/Fable.Core/ts/Symbol.ts
@@ -1,33 +1,13 @@
-declare var global: any;
-
-const fableGlobal: {
-  types: Map<string, FunctionConstructor>,
-  symbols: {
-    reflection: symbol,
-    generics: symbol,
-  }
-} = function () {
-  const globalObj =
-    typeof window !== "undefined" ? window
-    : (typeof global !== "undefined" ? global
-    : (typeof self !== "undefined" ? self : {}));
-  if (typeof globalObj.__FABLE_CORE__ === "undefined") {
-    globalObj.__FABLE_CORE__ = {
-      types: new Map<string, FunctionConstructor>(),
-      symbols: {
-        reflection: Symbol("reflection"),
-      }
-    };
-  }
-  return globalObj.__FABLE_CORE__;
-}();
+const types = new Map<string, FunctionConstructor>();
 
 export function setType(fullName: string, cons: FunctionConstructor) {
-  fableGlobal.types.set(fullName, cons);
+  types.set(fullName, cons);
 }
 
 export function getType(fullName: string) {
-  return fableGlobal.types.get(fullName);
+  return types.get(fullName);
 }
 
-export default (fableGlobal.symbols);
+export default {
+  reflection: Symbol("reflection")
+}
