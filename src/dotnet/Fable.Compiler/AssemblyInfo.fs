237371d066cf2ccfdd4693c90a7e8da95ec8b349
ncave <test2@hotmail.com>
2017-02-28 09:01:23 -0800

    Fixed test

--- src/dotnet/Fable.Compiler/AssemblyInfo.fs
+++ src/dotnet/Fable.Compiler/AssemblyInfo.fs
@@ -2,8 +2,8 @@
 namespace System
 open System.Reflection
 
-[<assembly: AssemblyVersionAttribute("0.7.43")>]
+[<assembly: AssemblyVersionAttribute("0.7.47")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] AssemblyVersion = "0.7.43"
+    let [<Literal>] AssemblyVersion = "0.7.47"
