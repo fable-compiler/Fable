42886945bec0483e274805def5e3e054b9993ff1
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-03-09 23:02:41 +0100

    Parallel compilation

--- src/dotnet/Fable.Server/State.fs
+++ src/dotnet/Fable.Server/State.fs
@@ -129,6 +129,32 @@ let createState checker projectOptions (com: ICompiler) (msg: Message) projFile 
     // TODO: Add warnings to compiler
     com, State(projectOptions, checkedProject)
 
+let toJsonAndReply =
+    let jsonSettings =
+        JsonSerializerSettings(
+            Converters=[|Json.ErasedUnionConverter()|],
+            NullValueHandling=NullValueHandling.Ignore,
+            StringEscapeHandling=StringEscapeHandling.EscapeNonAscii)
+    fun (replyChannel: string->unit) fileName (value: obj) ->
+        fileName |> Option.iter (printfn "Fable server sent: %s")
+        JsonConvert.SerializeObject(value, jsonSettings) |> replyChannel
+
+let sendError replyChannel (ex: Exception) =
+    let rec innerStack (ex: Exception) =
+        if isNull ex.InnerException then ex.StackTrace else innerStack ex.InnerException
+    match ex with
+    | :? FableError as err ->
+        let errMessage = err.FormattedMessage
+        printfn "%s" errMessage
+        // Don't print stack trace for known Fable errors
+        ["error", dict ["message", errMessage]]
+        |> dict |> toJsonAndReply replyChannel None
+    | ex ->
+        let stack = innerStack ex
+        printfn "ERROR: %s\n%s" ex.Message stack
+        ["error", dict ["message", ex.Message; "stack", stack]]
+        |> dict |> toJsonAndReply replyChannel None
+
 let startAgent () = MailboxProcessor<Command>.Start(fun agent ->
     let addLogs (com: Compiler) (file: Babel.Program) =
         let loc = defaultArg file.loc SourceLocation.Empty
@@ -140,15 +166,6 @@ let startAgent () = MailboxProcessor<Command>.Start(fun agent ->
                Array.map Log.message infos,
                Array.map Log.message warnings
         Babel.Program(file.fileName, loc, file.body, file.directives, infos, warnings)
-    let toJsonAndReply =
-        let jsonSettings =
-            JsonSerializerSettings(
-                Converters=[|Json.ErasedUnionConverter()|],
-                NullValueHandling=NullValueHandling.Ignore,
-                StringEscapeHandling=StringEscapeHandling.EscapeNonAscii)
-        fun (replyChannel: string->unit) fileName (value: obj) ->
-            fileName |> Option.iter (printfn "Fable server sent: %s")
-            JsonConvert.SerializeObject(value, jsonSettings) |> replyChannel
     let rec loop (checker: FSharpChecker) (com: Compiler) (state: State option) = async {
         let! (Parse msg), replyChannel = agent.Receive()
         try
@@ -181,26 +198,18 @@ let startAgent () = MailboxProcessor<Command>.Start(fun agent ->
                 else msg.path
             // Set file as already compiled
             state.CompiledFiles.[fileName] <- true
-            FSharp2Fable.Compiler.transformFile com state state.CheckedProject fileName
-            |> Fable2Babel.Compiler.transformFile com state
-            |> addLogs com
-            |> toJsonAndReply replyChannel (Some fileName)
+            Async.Start(async {
+                try
+                    FSharp2Fable.Compiler.transformFile com state state.CheckedProject fileName
+                    |> Fable2Babel.Compiler.transformFile com state
+                    |> addLogs com
+                    |> toJsonAndReply replyChannel (Some fileName)
+                with ex ->
+                    sendError replyChannel ex
+            })
             return! loop checker com (Some state)
-        with
-        | :? FableError as err ->
-            let errMessage = err.FormattedMessage
-            printfn "%s" errMessage
-            // Don't print stack trace for known Fable errors
-            ["error", dict ["message", errMessage]]
-            |> dict |> toJsonAndReply replyChannel None
-            return! loop checker com state
-        | ex ->
-            let rec innerStack (ex: Exception) =
-                if isNull ex.InnerException then ex.StackTrace else innerStack ex.InnerException
-            let stack = innerStack ex
-            printfn "ERROR: %s\n%s" ex.Message stack
-            ["error", dict ["message", ex.Message; "stack", stack]]
-            |> dict |> toJsonAndReply replyChannel None
+        with ex ->
+            sendError replyChannel ex
             return! loop checker com state
     }
     let compiler = Compiler(getDefaultOptions(), [])
