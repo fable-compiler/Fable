d9b7a923e48d13cbd6587142507c19e9a196205d
Joe Grund <grundjoseph@gmail.com>
2017-03-15 15:49:16 -0400

    Socket should inherit from EventEmitter
    Socket is an extension of EventEmitter. We should inherit from it.

--- src/fable/Fable.Core/Import/Fable.Import.Node.fs
+++ src/fable/Fable.Core/Import/Fable.Import.Node.fs
@@ -224,6 +224,7 @@ open NodeJS
 
 module net_types =
     type [<AllowNullLiteral>] Socket =
+        inherit EventEmitter
         abstract writable: bool with get, set
         abstract _write: chunk: obj * encoding: string * callback: Function -> unit
         abstract write: chunk: obj * ?cb: Function -> bool
