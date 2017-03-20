e66cb7fb073b07d9231c9e315943507f8ac1c5a7
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2017-03-20 12:32:25 +0100

    Add template

---
+++ src/templates/simple/Content/src/App.fs
@@ -0,0 +1,19 @@
+module FableTemplate
+
+open Fable.Core
+open Fable.Core.JsInterop
+open Fable.Import
+
+let init() =
+    let canvas = Browser.document.getElementsByTagName_canvas().[0]
+    canvas.width <- 1000.
+    canvas.height <- 800.
+    let ctx = canvas.getContext_2d()
+    // The (!^) operator checks and casts a value to an Erased Union type
+    // See http://fable.io/docs/interacting.html#Erase-attribute
+    ctx.fillStyle <- !^"rgb(200,0,0)"
+    ctx.fillRect (10., 10., 55., 50.)
+    ctx.fillStyle <- !^"rgba(0, 0, 200, 0.5)"
+    ctx.fillRect (30., 30., 55., 50.)
+
+init()
