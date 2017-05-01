7c012a0179a02338b8cf552c373241521966b612
Joe Grund <grundjoseph@gmail.com>
2017-05-01 15:18:33 -0400

    add undefined
    Signed-off-by: Joe Grund <grundjoseph@gmail.com>

--- src/dotnet/Fable.Core/Import/Node/Globals.fs
+++ src/dotnet/Fable.Core/Import/Node/Globals.fs
@@ -11,7 +11,10 @@ let [<Global>] __filename: string = jsNative
 let [<Global>] __dirname: string = jsNative
 let [<Global>] require: NodeJS.NodeRequire = jsNative
 let [<Global>] ``module``: NodeJS.NodeModule = jsNative
+
 let [<Global>] exports: obj = jsNative
+
+let [<Global>] undefined: obj = jsNative
 let [<Global>] SlowBuffer: buffer_types.SlowBufferStatic = jsNative
 let [<Global>] Buffer: buffer_types.BufferStatic = jsNative
 let [<Global>] ``process``:Process = jsNative
