e91dc0601e8daf13d42c3f6309a6d995a099ff3e
Marcel Schwark <forlanms@gmail.com>
2017-05-02 22:06:05 +0200

    Add documentation for the pong-sample (website).

--- samples/browser/pong/pong.fsx
+++ samples/browser/pong/pong.fsx
@@ -1,8 +1,13 @@
 (**
  - title: Fable Pong
- - tagline: Pong clone using HTML5 canvas
+ - tagline: Two player Pong clone using HTML5 canvas
+ - intro: This demo shows a Fable implementation of Pong, which is also part of the [F# Advent Calendar in English 2016](https://sergeytihon.wordpress.com/2016/10/23/f-advent-calendar-in-english-2016/).
+
+ You can find the [full source code on GitHub](https://github.com/fsprojects/Fable/blob/master/samples/browser/pong/pong.fsx).
+
 *)
 
+(*** hide ***)
 #r "../../node_modules/fable-core/Fable.Core.dll"
 #load "keyboard.fsx"
 #load "win.fsx"
@@ -15,6 +20,18 @@ open Keyboard
 open Win
 
 let w, h = Win.dimensions()
+
+(**
+   Defining the model
+   -----
+ 
+   When thinking about the Pong game, there are basically three types of models:
+
+   1. **Paddles:** The paddles have got a position and a specific size.
+   2. **Ball:** The ball has got a position and a size as well. But it has also got speed and an angle.
+   3. **Game status:** Some kind of storage containing information about the current score.
+
+*)
 
 type PongElement = { 
     x : float; 
@@ -35,6 +52,14 @@ type GameStatus = {
     active : bool;
 }
 
+(**
+   Controlling the paddles
+   -----
+ 
+   To control a paddle there are only two functions necessary. A canMove-function, to indicate whether a paddle can move in a certain direction, and an actual move-function to move the paddle.
+   The parameter direction is a tuple (int * int). When the first parameter of the tuple is set to 1, we want the paddle to move up. When the second parameter of the tuple is set to 1, we want the paddle to move down. By using pattern matching we can check which value of the tuple is set to 1. Since every object is immutable, we either return a copy of the current paddle with its new Y-position (line 10 and 11) or simply return the input-paddle if no movement is allowed.
+*)
+
 let canMove direction paddle =
     match direction with
     | (1, _) -> paddle.y > 0.
@@ -50,6 +75,14 @@ let move direction paddle =
     else
         paddle
 
+(**
+   Collision detection
+   -----
+ 
+   This discriminated union describes the whole collision system of the game: There can be no collision (None), the Top or Bottom of the canvas may be hit, the Left or Right part of the canvas may be hit (so a player scored) or finally a paddle was hit (LeftPaddle & RightPaddle). The following function takes the paddles and the ball as input parameters and returns the found type of collision.
+*)
+
+
 type Collision = 
     | None
     | Top
@@ -58,6 +91,12 @@ type Collision =
     | Right
     | LeftPaddle
     | RightPaddle
+
+
+(**
+   With this function, implementing a final collision-function to determine the new angle of the ball is straight forward again. (Thanks to pattern matching)
+*)
+
 
 let checkCollision leftPaddle rightPaddle ball =
     let hitTop = ball.element.y <= 0.
@@ -75,6 +114,10 @@ let checkCollision leftPaddle rightPaddle ball =
     | (_, _, _, _, _, true) -> RightPaddle
     | _ -> None
 
+(**
+   When hitting either the top or the bottom of the canvas, we negate the value of the angle (angle of incidence is equal to the angle of reflection). When hitting the left or right part of the canvas, we simply keep the input angle, since evaluating the score isn’t done here. To actually calculate the angle when a paddle is hit, we use yet another function.
+*)
+
 let calculateAngle paddle hitRightPaddle determineAngle ball =
     let relativeIntersectY = (paddle.y + (paddle.height / 2.)) - ball.element.y
     let normalizedRelativeIntersectionY = (relativeIntersectY / (paddle.height / 2.))
@@ -82,6 +125,10 @@ let calculateAngle paddle hitRightPaddle determineAngle ball =
         Math.PI
     else
         normalizedRelativeIntersectionY |> determineAngle
+
+(**
+  For the calculation, we determine the relative intersection where the ball hit the paddle. Afterwards we normalize that value. So for example, if the paddle is 20 pixels high, that value will be between -10 and 10. Therefore we can dynamically calculate the angle depending on the impact. As seen in the collision function, the determineAngle parameter of this calculation-function is a function itself. Depending on which paddle is hit, we have to use a slightly modified calculation of the final angle. As you can see in line 5, we’ve also got a special case we have to deal with. If the right paddle got hit in the exact center, so normalizedRelativeIntersectionY = 0. && hitRightPaddle, we will have to return Pi as the new angle, since the radiant value of Pi is equal to 180°.
+*)
 
 let collision leftPaddle rightPaddle ball =
     match ball |> checkCollision leftPaddle rightPaddle with
