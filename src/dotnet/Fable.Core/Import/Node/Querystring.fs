1e65fd26f385d6e5dc0f65f2b1d5a14a6a1b537d
Joe Grund <grundjoseph@gmail.com>
2017-05-16 19:29:57 -0400

    Add Url and Querystring modules
    Signed-off-by: Joe Grund <grundjoseph@gmail.com>

---
+++ src/dotnet/Fable.Core/Import/Node/Querystring.fs
@@ -0,0 +1,19 @@
+module rec Fable.Import.Node.Querystring
+
+open Fable.Core
+
+type [<AllowNullLiteral>] StringifyOptions =
+    abstract encodeURIComponent: (string -> string) option with get, set
+
+type [<AllowNullLiteral>] ParseOptions =
+    abstract maxKeys: float option with get, set
+    abstract decodeURIComponent: (string -> string) option with get, set
+
+type Globals =
+    abstract stringify: obj: 'T * ?sep: string * ?eq: string * ?options: StringifyOptions -> string
+    abstract parse: str: string * ?sep: string * ?eq: string * ?options: ParseOptions -> 'T
+    abstract escape: str: string -> string
+    abstract unescape: str: string -> string
+
+[<Import("*", "querystring")>] 
+let exports: Globals = jsNative
