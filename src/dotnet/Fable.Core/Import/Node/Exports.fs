f1b55ed88db5683db560c35d44921efe3a9c74e8
Joe Grund <grundjoseph@gmail.com>
2017-05-18 11:08:40 -0400

    Update node binding exports as per discussion.

--- src/dotnet/Fable.Core/Import/Node/Exports.fs
+++ src/dotnet/Fable.Core/Import/Node/Exports.fs
@@ -4,8 +4,26 @@ module Fable.Import.Node.Exports
 open Fable.Import.Node
 open Fable.Core
 
+[<Import("*", "buffer")>] 
+let buffer: Buffer.IExports = jsNative
+
+[<Import("*", "child_process")>]
+let child_process: ChildProcess.IExports = jsNative
+
+[<Import("*", "events")>]
+let events: Events.IExports = jsNative
+
+[<Import("*", "fs")>]
+let fs: Fs.IExports = jsNative
+
+[<Import("*","net")>]
+let net: Net.IExports = jsNative
+
 [<Import("*", "querystring")>]
 let querystring: Querystring.IExports = jsNative
+
+[<Import("*", "stream")>]
+let stream: Stream.IExports = jsNative
 
 [<Import("*", "url")>]
 let url: Url.IExports = jsNative
