34e6f8ac92865db863cff40e18b257dca7b26187
ncave <test2@hotmail.com>
2017-06-20 12:05:38 -0700

    Version check as a warning

--- build.fsx
+++ build.fsx
@@ -429,12 +429,12 @@ let runFableServer args f =
 
 let runTestsJs () =
     Npm.install __SOURCE_DIRECTORY__ []
-    runFableServer ["--no-version-check"; "--verbose"] <| fun () ->
+    runFableServer ["--verbose"] <| fun () ->
         Npm.script __SOURCE_DIRECTORY__ "webpack" ["--config src/tests/webpack.config.js"]
     Npm.script __SOURCE_DIRECTORY__ "mocha" ["./build/tests/bundle.js"]
 
 let quickTest() =
-    Util.run "src/tools" dotnetExePath "../../build/fable/dotnet-fable.dll npm-run rollup --no-version-check"
+    Util.run "src/tools" dotnetExePath "../../build/fable/dotnet-fable.dll npm-run rollup"
     Node.run "." "src/tools/temp/QuickTest.js" []
 
 Target "QuickTest" quickTest
