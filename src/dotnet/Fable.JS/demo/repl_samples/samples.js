64a1ef78fcc25a7e24b8c5b17a365d3720507d2a
ncave <test2@hotmail.com>
2017-07-28 17:09:19 -0700

    Fixed REPL build

--- src/dotnet/Fable.JS/demo/repl_samples/samples.js
+++ src/dotnet/Fable.JS/demo/repl_samples/samples.js
@@ -3,5 +3,6 @@ var SAMPLES = {
     sudoku: "Sudoku solver",
     fibonacci: "Fibonacci as a sequence",
     fibonacci_memoize: "Memoized Fibonacci",
+    raytracer: "Ray Tracer",
     safe_wrappers: "Safe Wrappers"
 }
