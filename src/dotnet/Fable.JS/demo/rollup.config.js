0dcb550ef9756d5d33fe94b8dcd858071801d39b
ncave <test2@hotmail.com>
2017-09-13 15:23:59 -0700

    updated repl configs

--- src/dotnet/Fable.JS/demo/rollup.config.js
+++ src/dotnet/Fable.JS/demo/rollup.config.js
@@ -1,36 +1,39 @@
-import fable from 'rollup-plugin-fable';
-var path = require('path');
+// import babel from 'rollup-plugin-babel';
+// import fable from 'rollup-plugin-fable';
+// const fableUtils = require("fable-utils");
+const path = require('path');
 
 function resolve(filePath) {
   return path.resolve(__dirname, filePath)
 }
 
-// var babelOptions = {
+// const babelOptions = fableUtils.resolveBabelOptions({
 //   "presets": [
 //     // ["es2015", { "modules": false }],
 //     // ["minify", { "mangle": false }],
 //   ]
+// });
+
+// const fableOptions = {
+//   // babel: babelOptions,
+//   fableCore: resolve("../../../../build/fable-core"),
+//   //plugins: [],
+//   define: [
+//     "COMPILER_PUBLIC_API",
+//     "FX_NO_CORHOST_SIGNER",
+//     "FX_NO_LINKEDRESOURCES",
+//     "FX_NO_PDB_READER",
+//     "FX_NO_PDB_WRITER",
+//     "FX_NO_WEAKTABLE",
+//     "FX_REDUCED_EXCEPTIONS",
+//     "NO_COMPILER_BACKEND",
+//     "NO_INLINE_IL_PARSER"
+//   ]
 // };
 
-var fableOptions = {
-  // babel: babelOptions,
-  fableCore: resolve("../../../../build/fable-core"),
-  //plugins: [],
-  define: [
-    "COMPILER_PUBLIC_API",
-    "FX_NO_CORHOST_SIGNER",
-    "FX_NO_LINKEDRESOURCES",
-    "FX_NO_PDB_READER",
-    "FX_NO_PDB_WRITER",
-    "FX_NO_WEAKTABLE",
-    "FX_REDUCED_EXCEPTIONS",
-    "NO_COMPILER_BACKEND",
-    "NO_INLINE_IL_PARSER"
-  ]
-};
-
 export default {
-  input: resolve('../Fable.JS.fsproj'),
+  // input: resolve('../Fable.JS.fsproj'),
+  input: resolve('./out/Main.js'),
   output: {
     file: resolve('./repl/bundle.min.js'),
     format: 'iife', // 'amd', 'cjs', 'es', 'iife', 'umd'
@@ -38,6 +41,7 @@ export default {
   name: 'Fable',
   //sourceMap: 'inline',
   plugins: [
-    fable(fableOptions),
+    // fable(fableOptions),
+    // babel(babelOptions),
   ],
 };
