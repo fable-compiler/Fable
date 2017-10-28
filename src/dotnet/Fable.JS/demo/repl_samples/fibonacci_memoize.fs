3e93329e69d680280eefa6120ebba728bdc942d8
ncave <test2@hotmail.com>
2017-10-28 11:02:08 -0700

    Updated samples

--- src/dotnet/Fable.JS/demo/repl_samples/fibonacci_memoize.fs
+++ src/dotnet/Fable.JS/demo/repl_samples/fibonacci_memoize.fs
@@ -4,13 +4,13 @@ let fastFib n =
   let sqrt5 = sqrt 5.0
   let a = (1.0 + sqrt5) / 2.0
   let b = (1.0 - sqrt5) / 2.0
-  let power = float (n+1)
+  let power = float n
   let result = ((a ** power) - (b ** power)) / sqrt5
   int (round result)
 
 let rec slowFib n =
     match n with
-    | 0 -> 1
+    | 0 -> 0
     | 1 -> 1
     | _ -> slowFib (n - 1) + slowFib (n - 2)
 
@@ -23,6 +23,6 @@ let memoizedFibonnaci =
         dict.Add(n, result)
         result
 
-[1..30]
+[0..30]
 |> List.map memoizedFibonnaci
 |> List.iter (printfn "%d")
