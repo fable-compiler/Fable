44426ec004df860df54028ce74cda9541b6c6c5c
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2018-01-15 10:44:50 +0100

    Add map to Fable.Core.JS/Array

--- src/js/fable-core/Array2.fs
+++ src/js/fable-core/Array2.fs
@@ -7,3 +7,10 @@ type ArrayCons =
     abstract Create: capacity: int -> 'T array
     [<Emit("$0.from($1)")>]
     abstract From: 'T seq -> 'T array
+
+let map (f: 'T->'U) (source: 'T[]) (cons: ArrayCons) =
+    let len = source.Length
+    let target = cons.Create(len)
+    for i = 0 to (len - 1) do
+        target.[i] <- f source.[i]
+    target
