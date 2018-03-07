277cf796111ef87fc28754b356f89677609d34c7
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2018-03-07 11:03:09 +0100

    Add more arithmetic tests

--- tests/Main/ArithmeticTests.fs
+++ tests/Main/ArithmeticTests.fs
@@ -10,6 +10,13 @@ let [<Literal>] aLiteral = 5
 let notALiteral = 5
 let [<Literal>] literalNegativeValue = -345
 
+let checkTo3dp (expected: float) actual =
+    equal (floor(actual * 1000.), expected)
+
+// let positiveInfinity = System.Double.PositiveInfinity
+// let negativeInfinity = System.Double.NegativeInfinity
+// let isNaN = fun x -> System.Double.IsNaN(x)
+
 let tests =
   testList "Arithmetic" [
     testCase "Infix add can be generated" <| fun () ->
@@ -147,174 +154,163 @@ testCase "BigInt Bitwise shift right can be generated" <| fun () ->
 
 testCase "BigInt abs works" <| fun () ->
     equal (abs -4I, 4I)
+*)
+    testCase "abs works" <| fun () ->
+        equal (abs -4, 4)
 
-testCase "abs works" <| fun () ->
-    equal (abs -4, 4)
+    testCase "round works" <| fun () ->
+        equal (round -12.5, -12.)
+        equal (round 1.5, 2.)
+        equal (round 1.535, 2.)
+        equal (round 1.525, 2.)
+        equal (System.Math.Round(1.55, 1), 1.6)
 
-testCase "round works" <| fun () ->
-    equal (round -12.5, -12.)
-    equal (round 1.5, 2.)
-    equal (round 1.535, 2.)
-    equal (round 1.525, 2.)
-    equal (System.Math.Round(1.55, 1), 1.6)
+    testCase "ceil works" <| fun () ->
+        equal (ceil 11.25, 12.)
 
-testCase "ceil works" <| fun () ->
-    equal (ceil 11.25, 12.)
+    testCase "floor works" <| fun () ->
+        equal (floor 11.75, 11.)
 
-testCase "floor works" <| fun () ->
-    equal (floor 11.75, 11.)
+    testCase "pown works" <| fun () ->
+        pown 2.2 3 |> checkTo3dp 10648.
 
-let checkTo3dp (expected: float) actual =
-    equal (floor(actual * 1000.), expected)
+    testCase "sqrt works" <| fun () ->
+        sqrt 4.5 |> checkTo3dp 2121.
 
-testCase "pown works" <| fun () ->
-    pown 2.2 3 |> checkTo3dp 10648.
+(*
+    // As per https://github.com/dotnet/corefx/blob/master/src/System.Runtime.Extensions/tests/System/Math.cs#L217
+    testCase "sqrt matches .net core implementation" <| fun () ->
+        checkTo3dp 1732. (sqrt 3.0)
+        equal (sqrt 0.0 , 0.0)
+        equal (isNaN (sqrt -3.0), true)
+        equal (isNaN (sqrt System.Double.NaN), true)
+        equal (isNaN (sqrt negativeInfinity), true)
+        equal (sqrt positiveInfinity, positiveInfinity)
 
-testCase "sqrt works" <| fun () ->
-    sqrt 4.5 |> checkTo3dp 2121.
+    testCase "Double.Parse works with IFormatProvider" <| fun () ->
+        // culture compiles to { } for now and it is ignore on the call-site
+        let culture = Globalization.CultureInfo.InvariantCulture
+        let result = System.Double.Parse("10.5", culture)
+        equal(10.5, result)
 
-let positiveInfinity = System.Double.PositiveInfinity
-let negativeInfinity = System.Double.NegativeInfinity
-let NaN = System.Double.NaN
-let isNaN = fun x -> System.Double.IsNaN(x)
+    testCase "Single.Parse works with IFormatProvider" <| fun () ->
+        // culture compiles to { } for now and it is ignore on the call-site
+        let culture = Globalization.CultureInfo.InvariantCulture
+        let result = System.Single.Parse("10.5", culture)
+        equal(10.5, float result)
+*)
+    testCase "acos works" <| fun () ->
+        acos 0.25 |> checkTo3dp 1318.
 
-// As per
-// https://github.com/dotnet/corefx/blob/master/src/System.Runtime.Extensions/tests/System/Math.cs#L217
-testCase "sqrt matches .net core implementation" <| fun () ->
-    checkTo3dp 1732. (sqrt 3.0)
-    equal (sqrt 0.0 , 0.0)
-    equal (isNaN (sqrt -3.0), true)
-    equal (isNaN (sqrt NaN), true)
-    equal (isNaN (sqrt negativeInfinity), true)
-    equal (sqrt positiveInfinity, positiveInfinity)
+    testCase "asin works" <| fun () ->
+        asin 0.25 |> checkTo3dp 252.
 
+    testCase "atan works" <| fun () ->
+        atan 0.25 |> checkTo3dp 244.
 
-testCase "Double.Parse works with IFormatProvider" <| fun () ->
-    // culture compiles to { } for now and it is ignore on the call-site
-    let culture = Globalization.CultureInfo.InvariantCulture
-    let result = System.Double.Parse("10.5", culture)
-    equal(10.5, result)
+    testCase "atan2 works" <| fun () ->
+        atan2 90. 15. |> checkTo3dp 1405.
 
-testCase "Single.Parse works with IFormatProvider" <| fun () ->
-    // culture compiles to { } for now and it is ignore on the call-site
-    let culture = Globalization.CultureInfo.InvariantCulture
-    let result = System.Single.Parse("10.5", culture)
-    equal(10.5, float result)
+    testCase "cos works" <| fun () ->
+        cos 0.25 |> checkTo3dp 968.
 
+    testCase "sin works" <| fun () ->
+        sin 0.25 |> checkTo3dp 247.
 
-testCase "acos works" <| fun () ->
-    acos 0.25 |> checkTo3dp 1318.
+    testCase "tan works" <| fun () ->
+        tan 0.25 |> checkTo3dp 255.
 
-testCase "asin works" <| fun () ->
-    asin 0.25 |> checkTo3dp 252.
+    testCase "exp works" <| fun () ->
+        exp 8.0 |> checkTo3dp 2980957.
+(*
+    // https://github.com/dotnet/corefx/blob/master/src/System.Runtime.Extensions/tests/System/Math.cs#L228
+    testCase "log works" <| fun () ->
+        log 232.12 |> checkTo3dp 5447.
+        checkTo3dp 1098. (log 3.0)
+        equal (log 0.0, negativeInfinity)
+        equal (isNaN (log -2.0), true)
+        equal (isNaN (log System.Double.NaN), true)
+        equal (isNaN (log negativeInfinity), true)
+        equal (log positiveInfinity, positiveInfinity)
 
-testCase "atan works" <| fun () ->
-    atan 0.25 |> checkTo3dp 244.
+    // https://github.com/dotnet/corefx/blob/master/src/System.Runtime.Extensions/tests/System/Math.cs#L239
+    testCase "Math.Log(double, double) works" <| fun () ->
+        equal (3.0, Math.Log(8.0, 2.0))
+        equal (1.0, Math.Log(3.0, 3.0))
+        Math.Log(14., 3.0) |> checkTo3dp 2402.
+        equal (negativeInfinity, Math.Log(0.0, 3.0))
+        equal (positiveInfinity, Math.Log(positiveInfinity, 3.0))
+        equal (true, isNaN (Math.Log(-3.0, 3.0)))
+        equal (true, isNaN (Math.Log(System.Double.NaN, 3.0)))
+        equal (true, isNaN (Math.Log(negativeInfinity, 3.0)))
+*)
+    testCase "log10 works" <| fun () ->
+        log10 232.12 |> checkTo3dp 2365.
 
-testCase "atan2 works" <| fun () ->
-    atan2 90. 15. |> checkTo3dp 1405.
+    testCase "PI works" <| fun () ->
+        checkTo3dp 3141. Math.PI
 
-testCase "cos works" <| fun () ->
-    cos 0.25 |> checkTo3dp 968.
+    testCase "E works" <| fun () ->
+        checkTo3dp 2718. Math.E
 
-testCase "sin works" <| fun () ->
-    sin 0.25 |> checkTo3dp 247.
+    testCase "Math.abs works" <| fun () ->
+        equal(Math.Abs -4, 4)
 
-testCase "tan works" <| fun () ->
-    tan 0.25 |> checkTo3dp 255.
+    testCase "Math.pown works" <| fun () ->
+        Math.Pow(2.2, 3.0) |> checkTo3dp 10648.
 
-testCase "exp works" <| fun () ->
-    exp 8.0 |> checkTo3dp 2980957.
+    testCase "Math.sqrt works" <| fun () ->
+        Math.Sqrt 4.5 |> checkTo3dp 2121.
 
-// https://github.com/dotnet/corefx/blob/master/src/System.Runtime.Extensions/tests/System/Math.cs#L228
-testCase "log works" <| fun () ->
-    log 232.12 |> checkTo3dp 5447.
-    checkTo3dp 1098. (log 3.0)
-    equal (log 0.0, negativeInfinity)
-    equal (isNaN (log -2.0), true)
-    equal (isNaN (log NaN), true)
-    equal (isNaN (log negativeInfinity), true)
-    equal (log positiveInfinity, positiveInfinity)
+    testCase "Math.round works" <| fun () ->
+        equal(Math.Round -12.5, -12.)
 
-// https://github.com/dotnet/corefx/blob/master/src/System.Runtime.Extensions/tests/System/Math.cs#L239
-testCase "Math.Log(double, double) works" <| fun () ->
-    equal (3.0, Math.Log(8.0, 2.0))
-    equal (1.0, Math.Log(3.0, 3.0))
-    Math.Log(14., 3.0) |> checkTo3dp 2402.
-    equal (negativeInfinity, Math.Log(0.0, 3.0))
-    equal (positiveInfinity, Math.Log(positiveInfinity, 3.0))
-    equal (true, isNaN (Math.Log(-3.0, 3.0)))
-    equal (true, isNaN (Math.Log(NaN, 3.0)))
-    equal (true, isNaN (Math.Log(negativeInfinity, 3.0)))
+    testCase "Math.ceil works" <| fun () ->
+        equal(Math.Ceiling 11.25, 12.)
 
+    testCase "Math.floor works" <| fun () ->
+        equal(Math.Floor 11.75, 11.)
 
-testCase "log10 works" <| fun () ->
-    log10 232.12 |> checkTo3dp 2365.
+    testCase "Math.acos works" <| fun () ->
+        Math.Acos 0.25 |> checkTo3dp 1318.
 
-testCase "PI works" <| fun () ->
-    checkTo3dp 3141. Math.PI
+    testCase "Math.asin works" <| fun () ->
+        Math.Asin 0.25 |> checkTo3dp 252.
 
-testCase "E works" <| fun () ->
-    checkTo3dp 2718. Math.E
+    testCase "Math.atan works" <| fun () ->
+        Math.Atan 0.25 |> checkTo3dp 244.
 
-testCase "Math.abs works" <| fun () ->
-    equal(Math.Abs -4, 4)
+    testCase "Math.atan2 works" <| fun () ->
+        Math.Atan2(90., 15.) |> checkTo3dp 1405.
 
-testCase "Math.pown works" <| fun () ->
-    Math.Pow(2.2, 3.0) |> checkTo3dp 10648.
+    testCase "Math.cos works" <| fun () ->
+        Math.Cos(0.1 * Math.PI) |> checkTo3dp 951.
 
-testCase "Math.sqrt works" <| fun () ->
-    Math.Sqrt 4.5 |> checkTo3dp 2121.
+    testCase "Math.sin works" <| fun () ->
+        Math.Sin(0.25 * Math.PI) |> checkTo3dp 707.
 
-testCase "Math.round works" <| fun () ->
-    equal(Math.Round -12.5, -12.)
+    testCase "Math.tan works" <| fun () ->
+        Math.Tan(0.5) |> checkTo3dp 546.
 
-testCase "Math.ceil works" <| fun () ->
-    equal(Math.Ceiling 11.25, 12.)
+    testCase "Math.exp works" <| fun () ->
+        Math.Exp 8.0 |> checkTo3dp 2980957.
 
-testCase "Math.floor works" <| fun () ->
-    equal(Math.Floor 11.75, 11.)
+    testCase "Math.log works" <| fun () ->
+        Math.Log 232.12 |> checkTo3dp 5447.
 
-testCase "Math.acos works" <| fun () ->
-    Math.Acos 0.25 |> checkTo3dp 1318.
+    testCase "Math.log10 works" <| fun () ->
+        Math.Log10 232.12 |> checkTo3dp 2365.
 
-testCase "Math.asin works" <| fun () ->
-    Math.Asin 0.25 |> checkTo3dp 252.
+(*
+    testCase "incr works" <| fun () ->
+        let i = ref 5
+        incr i
+        equal(!i, 6)
 
-testCase "Math.atan works" <| fun () ->
-    Math.Atan 0.25 |> checkTo3dp 244.
-
-testCase "Math.atan2 works" <| fun () ->
-    Math.Atan2(90., 15.) |> checkTo3dp 1405.
-
-testCase "Math.cos works" <| fun () ->
-    Math.Cos(0.1 * Math.PI) |> checkTo3dp 951.
-
-testCase "Math.sin works" <| fun () ->
-    Math.Sin(0.25 * Math.PI) |> checkTo3dp 707.
-
-testCase "Math.tan works" <| fun () ->
-    Math.Tan(0.5) |> checkTo3dp 546.
-
-testCase "Math.exp works" <| fun () ->
-    Math.Exp 8.0 |> checkTo3dp 2980957.
-
-testCase "Math.log works" <| fun () ->
-    Math.Log 232.12 |> checkTo3dp 5447.
-
-testCase "Math.log10 works" <| fun () ->
-    Math.Log10 232.12 |> checkTo3dp 2365.
-
-testCase "incr works" <| fun () ->
-    let i = ref 5
-    incr i
-    equal(!i, 6)
-
-testCase "decr works" <| fun () ->
-    let i = ref 5
-    decr i
-    equal(!i, 4)
-
+    testCase "decr works" <| fun () ->
+        let i = ref 5
+        decr i
+        equal(!i, 4)
 testCase "System.Random works" <| fun () ->
     let rnd = System.Random()
     let x = rnd.Next(5)
