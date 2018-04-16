68f1864f22cec3194a14730c3bb0dfc5c5c7d4b3
Maxime Mangel <maxime.shadaen@protonmail.com>
2018-04-16 13:03:33 +0200

    Start re-activating the Sets tests (currently broken)

--- tests/Main/Main.fs
+++ tests/Main/Main.fs
@@ -6,6 +6,7 @@ let allTests =
      Arrays.tests
      Char.tests
      Lists.tests
+     Sets.tests
   |]
 
 #if FABLE_COMPILER
--- tests/Main/SetTests.fs
+++ tests/Main/SetTests.fs
@@ -1,336 +1,336 @@
-[<Util.Testing.TestFixture>]
 module Fable.Tests.Sets
 open Util.Testing
-open Fable.Tests.Util
 
-[<Test>]
-let ``set function works``() =
-    let xs = set [1]
-    xs |> Seq.isEmpty
-    |> equal false
+let tests =
+    testList "Lists" [
+        testCase "set function works" <| fun () ->
+            let xs = set [1]
+            xs |> Seq.isEmpty
+            |> equal false
+    ]
 
-[<Test>]
-let ``Set.isEmpty works``() =
-    let xs = set []
-    Set.isEmpty xs |> equal true
-    let ys = set [1]
-    Set.isEmpty ys |> equal false
+// [<Test>]
+// let ``Set.isEmpty works``() =
+//     let xs = set []
+//     Set.isEmpty xs |> equal true
+//     let ys = set [1]
+//     Set.isEmpty ys |> equal false
 
-[<Test>]
-let ``Set.IsEmpty works``() =
-    let xs = Set.empty<int>
-    xs.IsEmpty |> equal true
-    let ys = set [1; 1]
-    ys.IsEmpty |> equal false
+// [<Test>]
+// let ``Set.IsEmpty works``() =
+//     let xs = Set.empty<int>
+//     xs.IsEmpty |> equal true
+//     let ys = set [1; 1]
+//     ys.IsEmpty |> equal false
 
-[<Test>]
-let ``Set.Count works``() =
-    let xs = Set.empty |> Set.add 1
-    xs.Count
-    |> equal 1
+// [<Test>]
+// let ``Set.Count works``() =
+//     let xs = Set.empty |> Set.add 1
+//     xs.Count
+//     |> equal 1
 
-[<Test>]
-let ``Set.add works``() =
-    let xs = Set.empty |> Set.add 1
-    Set.count xs
-    |> equal 1
+// [<Test>]
+// let ``Set.add works``() =
+//     let xs = Set.empty |> Set.add 1
+//     Set.count xs
+//     |> equal 1
 
-[<Test>]
-let ``Set.Add works``() =
-    let xs = Set.empty.Add 1
-    Set.count xs
-    |> equal 1
+// [<Test>]
+// let ``Set.Add works``() =
+//     let xs = Set.empty.Add 1
+//     Set.count xs
+//     |> equal 1
 
-[<Test>]
-let ``Set.contains works``() =
-    let xs = Set.empty |> Set.add 1
-    xs |> Set.contains 1 |> equal true
-    xs |> Set.contains 2 |> equal false
+// [<Test>]
+// let ``Set.contains works``() =
+//     let xs = Set.empty |> Set.add 1
+//     xs |> Set.contains 1 |> equal true
+//     xs |> Set.contains 2 |> equal false
 
-[<Test>]
-let ``Set.Contains works``() =
-    let xs = Set.empty |> Set.add 1
-    xs.Contains 1 |> equal true
-    xs.Contains 2 |> equal false
+// [<Test>]
+// let ``Set.Contains works``() =
+//     let xs = Set.empty |> Set.add 1
+//     xs.Contains 1 |> equal true
+//     xs.Contains 2 |> equal false
 
-[<Test>]
-let ``Set.remove works``() =
-    let xs = Set.empty |> Set.add 1 |> Set.remove 1
-    xs.IsEmpty |> equal true
+// [<Test>]
+// let ``Set.remove works``() =
+//     let xs = Set.empty |> Set.add 1 |> Set.remove 1
+//     xs.IsEmpty |> equal true
 
-[<Test>]
-let ``Set.Remove works``() =
-    let xs = (Set.empty |> Set.add 1).Remove 1
-    xs.IsEmpty |> equal true
+// [<Test>]
+// let ``Set.Remove works``() =
+//     let xs = (Set.empty |> Set.add 1).Remove 1
+//     xs.IsEmpty |> equal true
 
-[<Test>]
-let ``Set.singleton works``() =
-    let xs = Set.singleton 1
-    xs.Count |> equal 1
+// [<Test>]
+// let ``Set.singleton works``() =
+//     let xs = Set.singleton 1
+//     xs.Count |> equal 1
 
-[<Test>]
-let ``Set.union works``() =
-    let xs = Set.singleton 1
-    let ys = Set.singleton 2
-    let zs = xs |> Set.union ys
-    (zs.Contains 1 && zs.Contains 2)
-    |> equal true
+// [<Test>]
+// let ``Set.union works``() =
+//     let xs = Set.singleton 1
+//     let ys = Set.singleton 2
+//     let zs = xs |> Set.union ys
+//     (zs.Contains 1 && zs.Contains 2)
+//     |> equal true
 
-[<Test>]
-let ``Set.union works II``() =
-    Set.union (set [1; 2]) (set [3; 4; 5])
-    |> equal (set [1; 2; 3; 4; 5])
+// [<Test>]
+// let ``Set.union works II``() =
+//     Set.union (set [1; 2]) (set [3; 4; 5])
+//     |> equal (set [1; 2; 3; 4; 5])
 
-[<Test>]
-let ``Set (+) works``() =
-    let xs = Set.singleton 1
-    let ys = Set.singleton 2
-    let zs = xs + ys
-    (zs.Contains 1 && zs.Contains 2)
-    |> equal true
+// [<Test>]
+// let ``Set (+) works``() =
+//     let xs = Set.singleton 1
+//     let ys = Set.singleton 2
+//     let zs = xs + ys
+//     (zs.Contains 1 && zs.Contains 2)
+//     |> equal true
 
-[<Test>]
-let ``Set.unionMany works``() =
-    let xs = Set.singleton 1
-    let ys = Set.singleton 2
-    let zs = Set.singleton 3
-    let ks = Set.unionMany [xs; ys; zs]
-    (ks.Contains 1 && ks.Contains 2 && ks.Contains 3)
-    |> equal true
+// [<Test>]
+// let ``Set.unionMany works``() =
+//     let xs = Set.singleton 1
+//     let ys = Set.singleton 2
+//     let zs = Set.singleton 3
+//     let ks = Set.unionMany [xs; ys; zs]
+//     (ks.Contains 1 && ks.Contains 2 && ks.Contains 3)
+//     |> equal true
 
-[<Test>]
-let ``Set.intersect works``() =
-    let xs = set [1; 2; 3; 4]
-    let ys = set [0; 2; 4; 5]
-    let zs = xs |> Set.intersect ys
-    (zs.Contains 2 && zs.Contains 4 && not(zs.Contains 1) && not(zs.Contains 5))
-    |> equal true
+// [<Test>]
+// let ``Set.intersect works``() =
+//     let xs = set [1; 2; 3; 4]
+//     let ys = set [0; 2; 4; 5]
+//     let zs = xs |> Set.intersect ys
+//     (zs.Contains 2 && zs.Contains 4 && not(zs.Contains 1) && not(zs.Contains 5))
+//     |> equal true
 
-[<Test>]
-let ``Set.intersectMany works``() =
-    let xs = set [1; 2]
-    let ys = Set.singleton 2
-    let zs = set [2; 3]
-    let ks = Set.intersectMany [xs; ys; zs]
-    (ks.Contains 2 && not(ks.Contains 1 || ks.Contains 3))
-    |> equal true
+// [<Test>]
+// let ``Set.intersectMany works``() =
+//     let xs = set [1; 2]
+//     let ys = Set.singleton 2
+//     let zs = set [2; 3]
+//     let ks = Set.intersectMany [xs; ys; zs]
+//     (ks.Contains 2 && not(ks.Contains 1 || ks.Contains 3))
+//     |> equal true
 
-[<Test>]
-let ``Set.iterate works``() =
-    let xs = set [1.; 2.; 3.; 4.]
-    let total = ref 0.
-    xs |> Set.iter (fun x -> total := !total + x)
-    !total |> equal 10.
+// [<Test>]
+// let ``Set.iterate works``() =
+//     let xs = set [1.; 2.; 3.; 4.]
+//     let total = ref 0.
+//     xs |> Set.iter (fun x -> total := !total + x)
+//     !total |> equal 10.
 
-[<Test>]
-let ``Set.forAll works``() =
-    let xs = set [1; 2; 3; 4]
-    xs |> Set.forall (fun x -> x < 5)
-    |> equal true
+// [<Test>]
+// let ``Set.forAll works``() =
+//     let xs = set [1; 2; 3; 4]
+//     xs |> Set.forall (fun x -> x < 5)
+//     |> equal true
 
-[<Test>]
-let ``Set.exists works``() =
-    let xs = set [1; 2; 3; 4]
-    xs |> Set.exists ((=) 2) |> equal true
-    xs |> Set.exists ((<) 5) |> equal false
+// [<Test>]
+// let ``Set.exists works``() =
+//     let xs = set [1; 2; 3; 4]
+//     xs |> Set.exists ((=) 2) |> equal true
+//     xs |> Set.exists ((<) 5) |> equal false
 
-[<Test>]
-let ``Set.filter works``() =
-    let xs = set [1; 2; 3; 4]
-    xs |> Set.filter (fun x -> x % 2 = 0)
-    |> Set.count |> equal 2
+// [<Test>]
+// let ``Set.filter works``() =
+//     let xs = set [1; 2; 3; 4]
+//     xs |> Set.filter (fun x -> x % 2 = 0)
+//     |> Set.count |> equal 2
 
-[<Test>]
-let ``Set.partition works``() =
-    let xs = set [1; 2; 3; 4]
-    let ys, zs = xs |> Set.partition (fun x -> x % 2 = 0)
-    (ys.Count + zs.Count)
-    |> equal 4
+// [<Test>]
+// let ``Set.partition works``() =
+//     let xs = set [1; 2; 3; 4]
+//     let ys, zs = xs |> Set.partition (fun x -> x % 2 = 0)
+//     (ys.Count + zs.Count)
+//     |> equal 4
 
-[<Test>]
-let ``Set.fold works``() =
-    let xs = set [1.; 2.; 3.; 4.]
-    xs |> Set.fold (-) 0.
-    |> equal -10.
+// [<Test>]
+// let ``Set.fold works``() =
+//     let xs = set [1.; 2.; 3.; 4.]
+//     xs |> Set.fold (-) 0.
+//     |> equal -10.
 
-[<Test>]
-let ``Set.foldBack works``() =
-    let xs = set [1.; 2.; 3.; 4.]
-    Set.foldBack (-) xs 0.
-    |> equal -2.
+// [<Test>]
+// let ``Set.foldBack works``() =
+//     let xs = set [1.; 2.; 3.; 4.]
+//     Set.foldBack (-) xs 0.
+//     |> equal -2.
 
-[<Test>]
-let ``Set.map works``() =
-    let xs = set [1.; 2.; 3.; 4.]
-    let ys = xs |> Set.map ((*) 2.)
-    ys.Contains 1.
-    |> equal false
+// [<Test>]
+// let ``Set.map works``() =
+//     let xs = set [1.; 2.; 3.; 4.]
+//     let ys = xs |> Set.map ((*) 2.)
+//     ys.Contains 1.
+//     |> equal false
 
-[<Test>]
-let ``Set.minElement works``() =
-    let xs = set [1.; 2.; 3.; 4.]
-    xs |> Set.minElement
-    |> equal 1.
+// [<Test>]
+// let ``Set.minElement works``() =
+//     let xs = set [1.; 2.; 3.; 4.]
+//     xs |> Set.minElement
+//     |> equal 1.
 
-[<Test>]
-let ``Set.MinimumElement works``() =
-    let xs = set [1.; 2.; 3.; 4.]
-    xs.MinimumElement
-    |> equal 1.
+// [<Test>]
+// let ``Set.MinimumElement works``() =
+//     let xs = set [1.; 2.; 3.; 4.]
+//     xs.MinimumElement
+//     |> equal 1.
 
-[<Test>]
-let ``Set.maxElement works``() =
-    let xs = set [1.; 2.; 3.; 4.]
-    xs |> Set.maxElement
-    |> equal 4.
+// [<Test>]
+// let ``Set.maxElement works``() =
+//     let xs = set [1.; 2.; 3.; 4.]
+//     xs |> Set.maxElement
+//     |> equal 4.
 
-[<Test>]
-let ``Set.MaximumElement works``() =
-    let xs = set [1.; 2.; 3.; 4.]
-    xs.MaximumElement
-    |> equal 4.
+// [<Test>]
+// let ``Set.MaximumElement works``() =
+//     let xs = set [1.; 2.; 3.; 4.]
+//     xs.MaximumElement
+//     |> equal 4.
 
-[<Test>]
-let ``Set.difference works``() =
-    let xs = set [1.; 2.; 3.; 4.]
-    let ys = set [1.; 2.]
-    let zs = Set.difference xs ys
-    zs.Count |> equal 2
+// [<Test>]
+// let ``Set.difference works``() =
+//     let xs = set [1.; 2.; 3.; 4.]
+//     let ys = set [1.; 2.]
+//     let zs = Set.difference xs ys
+//     zs.Count |> equal 2
 
-[<Test>]
-let ``Set (-) works``() =
-    let xs = set [1.; 2.; 3.; 4.]
-    let ys = set [1.; 2.]
-    let zs = xs - ys
-    zs.Count |> equal 2
+// [<Test>]
+// let ``Set (-) works``() =
+//     let xs = set [1.; 2.; 3.; 4.]
+//     let ys = set [1.; 2.]
+//     let zs = xs - ys
+//     zs.Count |> equal 2
 
-[<Test>]
-let ``Set.isSubset works``() =
-    let xs = set [1.; 2.; 3.; 4.]
-    let ys = set [1.; 2.]
-    Set.isSubset ys xs
-    |> equal true
+// [<Test>]
+// let ``Set.isSubset works``() =
+//     let xs = set [1.; 2.; 3.; 4.]
+//     let ys = set [1.; 2.]
+//     Set.isSubset ys xs
+//     |> equal true
 
-[<Test>]
-let ``Set.IsSubset works``() =
-    let xs = set [1.; 2.; 3.; 4.]
-    let ys = set [1.; 2.]
-    ys.IsSubsetOf xs
-    |> equal true
+// [<Test>]
+// let ``Set.IsSubset works``() =
+//     let xs = set [1.; 2.; 3.; 4.]
+//     let ys = set [1.; 2.]
+//     ys.IsSubsetOf xs
+//     |> equal true
 
-[<Test>]
-let ``Set.isSuperset works``() =
-    let xs = set [1.; 2.; 3.; 4.]
-    let ys = set [1.; 2.]
-    Set.isSuperset xs ys
-    |> equal true
+// [<Test>]
+// let ``Set.isSuperset works``() =
+//     let xs = set [1.; 2.; 3.; 4.]
+//     let ys = set [1.; 2.]
+//     Set.isSuperset xs ys
+//     |> equal true
 
-[<Test>]
-let ``Set.IsSuperset works``() =
-    let xs = set [1.; 2.; 3.; 4.]
-    let ys = set [1.; 2.]
-    xs.IsSupersetOf ys
-    |> equal true
+// [<Test>]
+// let ``Set.IsSuperset works``() =
+//     let xs = set [1.; 2.; 3.; 4.]
+//     let ys = set [1.; 2.]
+//     xs.IsSupersetOf ys
+//     |> equal true
 
-[<Test>]
-let ``Set.isProperSubset works``() =
-    let xs = set [1.; 2.; 3.; 4.]
-    let ys = set [1.; 2.]
-    Set.isProperSubset ys xs
-    |> equal true
+// [<Test>]
+// let ``Set.isProperSubset works``() =
+//     let xs = set [1.; 2.; 3.; 4.]
+//     let ys = set [1.; 2.]
+//     Set.isProperSubset ys xs
+//     |> equal true
 
-[<Test>]
-let ``Set.IsProperSubset works``() =
-    let xs = set [1.; 2.; 3.; 4.]
-    let ys = set [1.; 2.]
-    ys.IsProperSubsetOf xs
-    |> equal true
+// [<Test>]
+// let ``Set.IsProperSubset works``() =
+//     let xs = set [1.; 2.; 3.; 4.]
+//     let ys = set [1.; 2.]
+//     ys.IsProperSubsetOf xs
+//     |> equal true
 
-[<Test>]
-let ``Set.isProperSuperset works``() =
-    let xs = set [1.; 2.; 3.; 4.]
-    let ys = set [1.; 2.]
-    Set.isProperSuperset xs ys
-    |> equal true
+// [<Test>]
+// let ``Set.isProperSuperset works``() =
+//     let xs = set [1.; 2.; 3.; 4.]
+//     let ys = set [1.; 2.]
+//     Set.isProperSuperset xs ys
+//     |> equal true
 
-[<Test>]
-let ``Set.IsProperSuperset works``() =
-    let xs = set [1.; 2.; 3.; 4.]
-    let ys = set [1.; 2.]
-    xs.IsProperSupersetOf ys
-    |> equal true
+// [<Test>]
+// let ``Set.IsProperSuperset works``() =
+//     let xs = set [1.; 2.; 3.; 4.]
+//     let ys = set [1.; 2.]
+//     xs.IsProperSupersetOf ys
+//     |> equal true
 
-[<Test>]
-let ``Set.ofList works``() =
-    let xs = Set.ofList [1.; 2.; 3.; 4.; 4.]
-    xs.Count |> equal 4
+// [<Test>]
+// let ``Set.ofList works``() =
+//     let xs = Set.ofList [1.; 2.; 3.; 4.; 4.]
+//     xs.Count |> equal 4
 
-[<Test>]
-let ``Set.ofArray works``() =
-    let xs = Set.ofArray [|1.; 2.; 3.; 4.; 4.|]
-    xs.Count |> equal 4
+// [<Test>]
+// let ``Set.ofArray works``() =
+//     let xs = Set.ofArray [|1.; 2.; 3.; 4.; 4.|]
+//     xs.Count |> equal 4
 
-[<Test>]
-let ``Set.ofSeq works``() =
-    let xs = Set.ofSeq [1.; 2.; 3.; 4.; 4.]
-    xs.Count |> equal 4
+// [<Test>]
+// let ``Set.ofSeq works``() =
+//     let xs = Set.ofSeq [1.; 2.; 3.; 4.; 4.]
+//     xs.Count |> equal 4
 
-[<Test>]
-let ``Set.toList works``() =
-    let xs = [1.; 2.; 3.; 4.]
-    let ys = Set.ofList xs
-    let zs = Set.toList ys
-    xs.[2] = zs.[2]
-    |> equal true
+// [<Test>]
+// let ``Set.toList works``() =
+//     let xs = [1.; 2.; 3.; 4.]
+//     let ys = Set.ofList xs
+//     let zs = Set.toList ys
+//     xs.[2] = zs.[2]
+//     |> equal true
 
-[<Test>]
-let ``Set.toArray works``() =
-    let xs = [|1.; 2.; 3.; 4.|]
-    let ys = Set.ofArray xs
-    let zs = Set.toArray ys
-    xs.[2] = zs.[2]
-    |> equal true
+// [<Test>]
+// let ``Set.toArray works``() =
+//     let xs = [|1.; 2.; 3.; 4.|]
+//     let ys = Set.ofArray xs
+//     let zs = Set.toArray ys
+//     xs.[2] = zs.[2]
+//     |> equal true
 
-[<Test>]
-let ``Set.toSeq works``() =
-    let xs = seq [1.; 2.; 3.; 4.]
-    let ys = Set.ofSeq xs
-    let zs = Set.toSeq ys
-    (Seq.item 2 xs) = (Seq.item 2 zs)
-    |> equal true
+// [<Test>]
+// let ``Set.toSeq works``() =
+//     let xs = seq [1.; 2.; 3.; 4.]
+//     let ys = Set.ofSeq xs
+//     let zs = Set.toSeq ys
+//     (Seq.item 2 xs) = (Seq.item 2 zs)
+//     |> equal true
 
-type R = { i: int; s: string }
+// type R = { i: int; s: string }
 
-[<Test>]
-let ``Sets can be JSON serialized forth and back``() =
-    let x = [{ i=1; s="1" }; { i=2; s="2" } ] |> set
-    #if FABLE_COMPILER
-    let json = Fable.Core.JsInterop.toJson x
-    let x2 = Fable.Core.JsInterop.ofJson<Set<R>> json
-    x2.IsSubsetOf x |> equal true
-    (0, x2) ||> Set.fold (fun acc v -> acc + v.i) |> equal 3
-    let x2 = Fable.Core.JsInterop.ofJsonAsType json (x.GetType()) :?> Set<R>
-    x2.IsSubsetOf x |> equal true
-    (0, x2) ||> Set.fold (fun acc v -> acc + v.i) |> equal 3
-    let json = Fable.Core.JsInterop.toJsonWithTypeInfo x
-    let x2 = Fable.Core.JsInterop.ofJsonWithTypeInfo<Set<R>> json
-    #else
-    let json = Newtonsoft.Json.JsonConvert.SerializeObject x
-    let x2 = Newtonsoft.Json.JsonConvert.DeserializeObject<Set<R>> json
-    #endif
-    x2.IsSubsetOf x |> equal true
-    (0, x2) ||> Set.fold (fun acc v -> acc + v.i)
-    |> equal 3
+// [<Test>]
+// let ``Sets can be JSON serialized forth and back``() =
+//     let x = [{ i=1; s="1" }; { i=2; s="2" } ] |> set
+//     #if FABLE_COMPILER
+//     let json = Fable.Core.JsInterop.toJson x
+//     let x2 = Fable.Core.JsInterop.ofJson<Set<R>> json
+//     x2.IsSubsetOf x |> equal true
+//     (0, x2) ||> Set.fold (fun acc v -> acc + v.i) |> equal 3
+//     let x2 = Fable.Core.JsInterop.ofJsonAsType json (x.GetType()) :?> Set<R>
+//     x2.IsSubsetOf x |> equal true
+//     (0, x2) ||> Set.fold (fun acc v -> acc + v.i) |> equal 3
+//     let json = Fable.Core.JsInterop.toJsonWithTypeInfo x
+//     let x2 = Fable.Core.JsInterop.ofJsonWithTypeInfo<Set<R>> json
+//     #else
+//     let json = Newtonsoft.Json.JsonConvert.SerializeObject x
+//     let x2 = Newtonsoft.Json.JsonConvert.DeserializeObject<Set<R>> json
+//     #endif
+//     x2.IsSubsetOf x |> equal true
+//     (0, x2) ||> Set.fold (fun acc v -> acc + v.i)
+//     |> equal 3
 
-[<Test>]
-let ``Sets serialized with Json.NET can be deserialized``() =
-    // let x = ["a", { i=1; s="1" }; "b", { i=2; s="2" } ] |> Map
-    // let json = JsonConvert.SerializeObject(x, JsonSerializerSettings(TypeNameHandling=TypeNameHandling.All))
-    let json = """{"$type":"Microsoft.FSharp.Collections.FSharpSet`1[[Fable.Tests.Sets+R, Fable.Tests]], FSharp.Core","$values":[{"$type":"Fable.Tests.Sets+R, Fable.Tests","i":1,"s":"1"},{"$type":"Fable.Tests.Sets+R, Fable.Tests","i":2,"s":"2"}]}"""
-    #if FABLE_COMPILER
-    let x2 = Fable.Core.JsInterop.ofJsonWithTypeInfo<Set<R>> json
-    #else
-    let x2 = Newtonsoft.Json.JsonConvert.DeserializeObject<Set<R>> json
-    #endif
-    (0, x2) ||> Set.fold (fun acc v -> acc + v.i)
-    |> equal 3
+// [<Test>]
+// let ``Sets serialized with Json.NET can be deserialized``() =
+//     // let x = ["a", { i=1; s="1" }; "b", { i=2; s="2" } ] |> Map
+//     // let json = JsonConvert.SerializeObject(x, JsonSerializerSettings(TypeNameHandling=TypeNameHandling.All))
+//     let json = """{"$type":"Microsoft.FSharp.Collections.FSharpSet`1[[Fable.Tests.Sets+R, Fable.Tests]], FSharp.Core","$values":[{"$type":"Fable.Tests.Sets+R, Fable.Tests","i":1,"s":"1"},{"$type":"Fable.Tests.Sets+R, Fable.Tests","i":2,"s":"2"}]}"""
+//     #if FABLE_COMPILER
+//     let x2 = Fable.Core.JsInterop.ofJsonWithTypeInfo<Set<R>> json
+//     #else
+//     let x2 = Newtonsoft.Json.JsonConvert.DeserializeObject<Set<R>> json
+//     #endif
+//     (0, x2) ||> Set.fold (fun acc v -> acc + v.i)
+//     |> equal 3
