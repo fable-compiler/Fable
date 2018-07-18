df90b4cfcfcff0ca411535fd18f10cfafd4b3875
ncave <777696+ncave@users.noreply.github.com>
2018-07-18 11:31:12 -0700

    bench cleanup

--- src/dotnet/Fable.JS/bench/platform.fs
+++ src/dotnet/Fable.JS/bench/platform.fs
@@ -4,7 +4,6 @@ module Bench.Platform
 
 let fableCoreDir = System.IO.Path.Combine(__SOURCE_DIRECTORY__, "../../../build/fable-core")
 
-let initFable () = Fable.JS.Main.init ()
 let readAllBytes metadataPath (fileName:string) = System.IO.File.ReadAllBytes (metadataPath + fileName)
 let readAllText (filePath:string) = System.IO.File.ReadAllText (filePath, System.Text.Encoding.UTF8)
 let writeAllText (filePath:string) (text:string) = System.IO.File.WriteAllText (filePath, text)
@@ -15,27 +14,25 @@ let measureTime (f: 'a -> 'b) x =
     sw.Stop()
     sw.ElapsedMilliseconds, res
 
-let writeJs (filePath:string) (babelAst:obj) =
-    () // Does nothing in .NET for now
+// not really serializing, just a stub
+let toJson (value: obj) = sprintf "%A" value // Newtonsoft.Json.JsonConvert.SerializeObject(value)
 
 #else
 
 let fableCoreDir = "${entryDir}/../../../build/fable-core"
 
-type IFileSystem =
+type private IFileSystem =
     abstract readFileSync: string -> byte[]
     abstract readFileSync: string * string -> string
     abstract writeFileSync: string * string -> unit
 
-type IProcess =
+type private IProcess =
     abstract hrtime: unit -> float []
     abstract hrtime: float[] -> float[]
 
-let FileSystem: IFileSystem = Fable.Core.JsInterop.importAll "fs"
-let Process: IProcess = Fable.Core.JsInterop.importAll "process"
+let private FileSystem: IFileSystem = Fable.Core.JsInterop.importAll "fs"
+let private Process: IProcess = Fable.Core.JsInterop.importAll "process"
 
-// let initFable (): Fable.JS.IFableManager = Fable.Core.JsInterop.import "init" "${entryDir}/../out/Main"
-let initFable () = Fable.JS.Main.init ()
 let readAllBytes metadataPath (fileName:string) = FileSystem.readFileSync(metadataPath + fileName)
 let readAllText (filePath:string) = FileSystem.readFileSync (filePath, "utf8")
 let writeAllText (filePath:string) (text:string) = FileSystem.writeFileSync (filePath, text)
@@ -46,14 +43,6 @@ let measureTime (f: 'a -> 'b) x =
     let elapsed = Process.hrtime(startTime)
     int64 (elapsed.[0] * 1e3 + elapsed.[1] / 1e6), res
 
-type private IBabelResult =
-    abstract code: string
-
-let private transformFromAst (babelAst: obj): IBabelResult =
-    Fable.Core.JsInterop.importMember "babel-core"
-
-let writeJs (filePath:string) (babelAst:obj) =
-    let res = transformFromAst babelAst
-    writeAllText filePath res.code
+let toJson (value: obj) = value |> Fable.Core.JsInterop.toJson
 
 #endif
