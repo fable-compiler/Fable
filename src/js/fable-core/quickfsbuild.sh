1456bb0da443f439aa8282f9ab3b29e616d3b51c
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2018-09-04 17:00:57 +0200

    Clean up quicktest files

--- src/js/fable-core/quickfsbuild.sh
+++
@@ -1,5 +0,0 @@
-dotnet run -p ../../dotnet/Fable.Compiler \
-    node-run ../fable-splitter/dist/cli --fable-core force:\${outDir}
-
-# Check argument injections
-dotnet run -p ../../tools/InjectProcessor
