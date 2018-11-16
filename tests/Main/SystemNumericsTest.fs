d40283962ea32ab49198f5859ce896782cf62b7a
Lukas Rieger <rieger@progress-psd.com>
2018-11-16 16:00:40 +0100

    add tests for field read / write

--- tests/Main/SystemNumericsTest.fs
+++ tests/Main/SystemNumericsTest.fs
@@ -24,6 +24,15 @@ let tests =
             let v1 = System.Numerics.Vector3(1.f, 1.f, 1.f)
             v1 |> equal System.Numerics.Vector3.One
             [| v1.X; v1.Y; v1.Z |] |> equal [| 1.f; 1.f; 1.f |]
+            
+
+      testCase "Vector3 writing fields works" <| fun () ->
+            let mutable v1 = System.Numerics.Vector3(1.f, 1.f, 1.f)
+            v1.X <- 5.f
+            v1.X |> equal 5.f
+            v1.Y <- 6.f
+            v1.Z <- 7.f
+            v1 |> equal (System.Numerics.Vector3(5.f, 6.f, 7.f))
     
 
       // Quaternion
@@ -32,4 +41,14 @@ let tests =
             let q1 = System.Numerics.Quaternion(0.f, 0.f, 0.f, 1.f)
             q1 |> equal System.Numerics.Quaternion.Identity
             [| q1.X; q1.Y; q1.Z; q1.W |] |> equal [| 0.f; 0.f; 0.f; 1.f |]
+            
+
+      testCase "Quaternion writing fields works" <| fun () ->
+            let mutable q1 = System.Numerics.Quaternion(1.f, 1.f, 1.f, 1.f)
+            q1.X <- 5.f
+            q1.X |> equal 5.f
+            q1.Y <- 6.f
+            q1.Z <- 7.f
+            q1.W <- 8.f
+            q1 |> equal (System.Numerics.Quaternion(5.f, 6.f, 7.f, 8.f))
 ]
