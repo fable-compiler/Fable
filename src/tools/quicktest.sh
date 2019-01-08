2c4b76b9fcb19cc35fbefe0eacd3c8a8867d80b7
Alfonso Garcia-Caro <alfonso.garcia-caro@hotmail.com>
2019-01-08 10:26:35 +0100

    Fix #1689: List.pairwise

--- src/tools/quicktest.sh
+++ src/tools/quicktest.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # First time after cloning the repository, run:
-#   bash quicktest.sh --build-core
+#   bash quicktest.sh --build-library
 
-# Next time, if src/js/fable-core files haven't changed, just:
+# Next time, if src/js/fable-library files haven't changed, just:
 #   bash quicktest.sh
 
 ARGS="$@"
@@ -10,23 +10,23 @@ ARGS="$@"
 echo "dotnet SDK version"
 dotnet --version
 
-if [ "${ARGS/--build-js-core/}" != "$ARGS" -o "${ARGS/--build-core/}" != "$ARGS" ]; then
-    echo "Building fable-core (JS/TS files)..."
+if [ "${ARGS/--build-js-library/}" != "$ARGS" -o "${ARGS/--build-library/}" != "$ARGS" ]; then
+    echo "Building fable-library (JS/TS files)..."
 
     pushd ../..
     yarn
-    # yarn tslint --project src/js/fable-core
-    yarn tsc --project src/js/fable-core
+    # yarn tslint --project src/js/fable-library
+    yarn tsc --project src/js/fable-library
     popd
 fi
 
-if [ "${ARGS/--build-core/}" != "$ARGS" ]; then
-    echo "Building fable-core (F# files)..."
+if [ "${ARGS/--build-library/}" != "$ARGS" ]; then
+    echo "Building fable-library (F# files)..."
 
-    dotnet run -p ../dotnet/Fable.Compiler \
+    dotnet run -c Release -p ../dotnet/Fable.Compiler \
         fable-splitter --force-pkgs \
-        --fable-core force:\${outDir} \
-        --args "-c src/js/fable-core/splitter.config.js"
+        --fable-library force:\${outDir} \
+        --args "-c src/js/fable-library/splitter.config.js"
 fi
 
 dotnet run -p ../dotnet/Fable.Compiler \
