33fdcf41d5bd4e0242a05ab8057adf84acf41684
ncave <777696+ncave@users.noreply.github.com>
2019-01-12 19:15:58 -0800

    updated trampoline

--- src/dotnet/Fable.Compiler/Transforms/MonadicTrampoline.fs
+++ src/dotnet/Fable.Compiler/Transforms/MonadicTrampoline.fs
@@ -1,51 +1,23 @@
-﻿// Source: http://fssnip.net/dK/title/Monadic-Trampoline
-module MonadicTrampoline
+﻿module MonadicTrampoline
 
 #if NETSTANDARD2_0 || !FABLE_COMPILER
-type TrampolineValue<'T> =
-    | DelayValue of Delay<'T>
-    | ReturnValue of Return<'T>
-    | BindValue of IBind<'T>
 
-and ITrampoline<'T> =
-    abstract member Value : TrampolineValue<'T>
-    abstract member Run : unit -> 'T
+type Thunk<'T> =
+    | DelayValue of (unit -> Thunk<'T>)
+    | ReturnValue of 'T
 
-and Delay<'T>(f : unit -> ITrampoline<'T>) =
-    member self.Func = f
-    interface ITrampoline<'T> with
-        member self.Value = DelayValue self
-        member self.Run () = (f ()).Run()
-
-and Return<'T>(x :'T) =
-    member self.Value = x
-    interface ITrampoline<'T> with
-        member self.Value = ReturnValue self
-        member self.Run () = x
-
-and IBind<'T> =
-    abstract Bind<'R> : ('T -> ITrampoline<'R>) -> ITrampoline<'R>
-
-and Bind<'T, 'R>(trampoline : ITrampoline<'T>, f : ('T -> ITrampoline<'R>)) =
-    interface IBind<'R> with
-        member self.Bind<'K>(f' : 'R -> ITrampoline<'K>) : ITrampoline<'K> =
-            new Bind<'T, 'K>(trampoline, fun t -> new Bind<'R, 'K>(f t, (fun r -> f' r)) :> _) :> _
-    interface ITrampoline<'R> with
-        member self.Value = BindValue self
-        member self.Run () =
-            match trampoline.Value with
-            | BindValue b -> b.Bind(f).Run()
-            | ReturnValue r -> (f r.Value).Run()
-            | DelayValue d -> (new Bind<'T, 'R>(d.Func (), f) :> ITrampoline<'R>).Run()
+let rec run = function
+    | DelayValue f -> f () |> run
+    | ReturnValue x -> x
 
 type TrampolineBuilder() =
-    member self.Bind(tr, f) = new Bind<'T, 'R>(tr, f) :> ITrampoline<'R>
-    member self.Delay f = new Delay<_>(f) :> ITrampoline<_>
-    member self.Return a = new Return<_>(a) :> ITrampoline<_>
-    member self.ReturnFrom a = a
+    member __.Bind(thunk, f) = DelayValue (fun () -> run thunk |> f)
+    member __.Delay f = DelayValue f
+    member __.Return a = ReturnValue a
+    member __.ReturnFrom (a: Thunk<'T>) = a
 
-let inline run (tr : ITrampoline<'T>) = tr.Run()
 #else
+
 // Fable cannot tail-call optimize mutually recursive functions, rendering
 // the implementation above useless, so we use the following one instead.
 // However, take note this uses unsafe boxing/unboxing, which is ok
@@ -71,9 +43,10 @@ type TrampolineBuilder() =
     member __.Delay f = DelayValue f
     member __.Return a = ReturnValue a
     member __.ReturnFrom (a: Thunk) = a
+
 #endif
 
-let trampoline = new TrampolineBuilder()
+let trampoline = TrampolineBuilder()
 
 let rec trampolineListMapAcc acc f xs =
     trampoline {
