87166e8ec18ea530efa2e5358683dea41b37963d
nojaf <florian.verdonck@outlook.com>
2019-03-15 09:35:30 +0100

    Testing webpack-dev-server.

--- samples/react-todomvc-fsx/webpack.config.js
+++ samples/react-todomvc-fsx/webpack.config.js
@@ -1,27 +1,33 @@
+const path = require("path");
+
 module.exports = {
-  entry: './src/ReactTodoMVC.fsx',
-  mode: "development",
-  devServer: {
-      contentBase: "./dist",
-      port: 8080,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.fs(x|proj)?$/,
-          use: {
-              loader: "fable-loader",
-              options: {
-                  cli: {
-                      path: "../../src/Fable.Cli"
-                  }
-              }
-          }
-      }
-    ]
-  },
-  externals: {
-    "react": "React",
-    "react-dom": "ReactDOM",
-  },
+    entry: './src/ReactTodoMVC.fsx',
+    output: {
+        path: path.join(__dirname, "./public"),
+        filename: "bundle.js",
+    },
+    mode: "development",
+    devServer: {
+        contentBase: "./public",
+        port: 8080,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.fs(x|proj)?$/,
+                use: {
+                    loader: "fable-loader",
+                    options: {
+                        cli: {
+                            path: "../../src/Fable.Cli"
+                        }
+                    }
+                }
+            }
+        ]
+    },
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM",
+    },
 };
