b7a07a1eaade95d0a0d0e28e13dc89597632be1d
Lukas Rieger <rieger@progress-psd.com>
2019-08-05 20:15:13 +0200

    add support for nested "testList", fixes https://github.com/fable-compiler/Fable/issues/1873

--- tests/Main/Util/Util.fs
+++ tests/Main/Util/Util.fs
@@ -4,12 +4,16 @@ open System
 
 module Testing =
 #if FABLE_COMPILER
+    type TestKind =
+    | TestList of string * TestKind seq
+    | TestCase of (string*obj)
+
     open Fable.Core
     open Fable.Core.Testing
 
-    let testList (name: string) (tests: (string * obj) seq) = name, tests
-    let testCase (msg: string) (test: unit->unit) = msg, box test
-    let testCaseAsync (msg: string) (test: unit->Async<unit>) = msg, box(fun () -> test () |> Async.StartAsPromise)
+    let testList (name: string) (tests: TestKind seq) = TestList( name, tests )
+    let testCase (msg: string) (test: unit->unit) = TestCase( msg, box test )
+    let testCaseAsync (msg: string) (test: unit->Async<unit>) = TestCase( msg, box(fun () -> test () |> Async.StartAsPromise) )
 
     let equal expected actual: unit = Assert.AreEqual(actual, expected)
     let notEqual expected actual: unit = Assert.NotEqual(actual, expected)
