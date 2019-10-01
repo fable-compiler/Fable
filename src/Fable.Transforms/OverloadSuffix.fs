7425e79294bfd5d0fdcc3f98b288aad86d794f43
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2019-10-01 16:05:28 +0200

    Improve generic constrain overload hash

--- src/Fable.Transforms/OverloadSuffix.fs
+++ src/Fable.Transforms/OverloadSuffix.fs
@@ -42,34 +42,39 @@ let private hashToString (i: int) =
     then "Z" + (abs i).ToString("X")
     else i.ToString("X")
 
-// TODO: This is very simple right now and only checks the constrain kind
-let private getGenericParamConstrainsHash (p: FSharpGenericParameter) =
+// Not perfect but hopefully covers most of the cases
+// Using only common constrains from https://docs.microsoft.com/en-us/dotnet/fsharp/language-reference/generics/constraints
+let rec private getGenericParamConstrainsHash genParams (p: FSharpGenericParameter) =
     let getConstrainHash (c: FSharpGenericParameterConstraint) =
-        if c.IsCoercesToConstraint then 1
-        elif c.IsComparisonConstraint then 2
-        elif c.IsDefaultsToConstraint then 4
-        elif c.IsDelegateConstraint then 8
-        elif c.IsEnumConstraint then 16
-        elif c.IsEqualityConstraint then 32
-        elif c.IsMemberConstraint then 64
-        elif c.IsNonNullableValueTypeConstraint then 128
-        elif c.IsReferenceTypeConstraint then 256
-        elif c.IsRequiresDefaultConstructorConstraint then 512
-        elif c.IsSimpleChoiceConstraint then 1024
-        elif c.IsSupportsNullConstraint then 2048
-        elif c.IsUnmanagedConstraint then 4096
-        else 0
-    let v = p.Constraints |> Seq.sumBy getConstrainHash
-    if v = 0 then ""
-    else hashToString v
+        if c.IsCoercesToConstraint then
+            ":>" + getTypeFastFullName genParams c.CoercesToTarget
+        elif c.IsSupportsNullConstraint then
+            "null"
+        elif c.IsMemberConstraint then
+            let d = c.MemberConstraintData // TODO: Full member signature hash?            
+            (if d.MemberIsStatic then "static " else "") + "member " + d.MemberName
+        elif c.IsRequiresDefaultConstructorConstraint then
+            "new"
+        elif c.IsNonNullableValueTypeConstraint then
+            "struct"
+        elif c.IsReferenceTypeConstraint then
+            "not struct"
+        elif c.IsComparisonConstraint then
+            "comparison"
+        elif c.IsEqualityConstraint then
+            "equality"
+        elif c.IsUnmanagedConstraint then
+            "unmanaged"
+        else ""
+    p.Constraints |> Seq.map getConstrainHash |> String.concat ","
 
 // Attention: we need to keep this similar to FSharp2Fable.TypeHelpers.makeType
-let rec private getTypeFastFullName (genParams: IDictionary<_,_>) (t: FSharpType) =
+and private getTypeFastFullName (genParams: IDictionary<_,_>) (t: FSharpType) =
     let t = nonAbbreviatedType t
     if t.IsGenericParameter then
         match genParams.TryGetValue(t.GenericParameter.Name) with
         | true, i -> i
-        | false, _ -> getGenericParamConstrainsHash t.GenericParameter
+        | false, _ -> getGenericParamConstrainsHash genParams t.GenericParameter
     elif t.IsTupleType
     then t.GenericArguments |> Seq.map (getTypeFastFullName genParams) |> String.concat " * "
     elif t.IsFunctionType
