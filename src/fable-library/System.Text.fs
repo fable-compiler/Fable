e221918d5e9c08b16bf6fb56949aa54eb3d695d1
ThisFunctionalTom <tomas.leko@gmail.com>
2020-04-14 14:42:22 +0200

    Add Length and Substring with firstIndex and length in StringBuilder

--- src/fable-library/System.Text.fs
+++ src/fable-library/System.Text.fs
@@ -12,3 +12,7 @@ type StringBuilder(value: string, capacity: int) =
     member x.AppendLine() = buf.Add(System.Environment.NewLine); x
     member x.AppendLine(s: string) = buf.Add(s); buf.Add(System.Environment.NewLine); x
     override __.ToString() = System.String.Concat(buf)
+    member x.Length = buf |> Seq.sumBy String.length
+    member x.ToString(firstIndex: int, length: int) =
+        let str = x.ToString()
+        str.Substring(firstIndex, length)
