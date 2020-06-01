af59bb0e964344d366ab3bf2d8c707d8a6b1226d
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2020-06-01 14:33:05 +0900

    Revert temp changes

--- src/quicktest/QuickTest.fs
+++ src/quicktest/QuickTest.fs
@@ -12,57 +12,49 @@ open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Core.Testing
 
-//let log (o: obj) =
-//    printfn "%O" o
-//
-//let equal expected actual =
-//    let areEqual = expected = actual
-//    printfn "%A = %A > %b" expected actual areEqual
-//    if not areEqual then
-//        failwithf "[ASSERT ERROR] Expected %A but got %A" expected actual
-//
-//let throwsError (expected: string) (f: unit -> 'a): unit =
-//    let success =
-//        try
-//            f () |> ignore
-//            true
-//        with e ->
-//            if not <| String.IsNullOrEmpty(expected) then
-//                equal e.Message expected
-//            false
-//    // TODO better error messages
-//    equal false success
-//
-//let testCase (msg: string) f: unit =
-//    try
-//        printfn "%s" msg
-//        f ()
-//    with ex ->
-//        printfn "%s" ex.Message
-//        if ex.Message <> null && ex.Message.StartsWith("[ASSERT ERROR]") |> not then
-//            printfn "%s" ex.StackTrace
-//    printfn ""
-//
-//let testCaseAsync msg f =
-//    testCase msg (fun () ->
-//        async {
-//            try
-//                do! f ()
-//            with ex ->
-//                printfn "%s" ex.Message
-//                if ex.Message <> null && ex.Message.StartsWith("[ASSERT ERROR]") |> not then
-//                    printfn "%s" ex.StackTrace
-//        } |> Async.StartImmediate)
-//
-//// Write here your unit test, you can later move it
-//// to Fable.Tests project. For example:
-//// testCase "Addition works" <| fun () ->
-////     2 + 2 |> equal 4
+let log (o: obj) =
+    printfn "%O" o
 
+let equal expected actual =
+    let areEqual = expected = actual
+    printfn "%A = %A > %b" expected actual areEqual
+    if not areEqual then
+        failwithf "[ASSERT ERROR] Expected %A but got %A" expected actual
 
+let throwsError (expected: string) (f: unit -> 'a): unit =
+    let success =
+        try
+            f () |> ignore
+            true
+        with e ->
+            if not <| String.IsNullOrEmpty(expected) then
+                equal e.Message expected
+            false
+    // TODO better error messages
+    equal false success
 
-type MyPreciousAttribute(t: Type) =
-    inherit Attribute()
+let testCase (msg: string) f: unit =
+    try
+        printfn "%s" msg
+        f ()
+    with ex ->
+        printfn "%s" ex.Message
+        if ex.Message <> null && ex.Message.StartsWith("[ASSERT ERROR]") |> not then
+            printfn "%s" ex.StackTrace
+    printfn ""
 
-[<MyPrecious(typeof<Rintintin>)>]
-let testMyPrecious() = ()
+let testCaseAsync msg f =
+    testCase msg (fun () ->
+        async {
+            try
+                do! f ()
+            with ex ->
+                printfn "%s" ex.Message
+                if ex.Message <> null && ex.Message.StartsWith("[ASSERT ERROR]") |> not then
+                    printfn "%s" ex.StackTrace
+        } |> Async.StartImmediate)
+
+// Write here your unit test, you can later move it
+// to Fable.Tests project. For example:
+// testCase "Addition works" <| fun () ->
+//     2 + 2 |> equal 4
