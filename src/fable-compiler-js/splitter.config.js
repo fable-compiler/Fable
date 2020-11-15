12e94667e68aa58db1dea82d02906cae9500dccc
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2020-11-15 13:10:32 +0900

    Remove splitter.config.js files

--- src/fable-compiler-js/splitter.config.js
+++
@@ -1,7 +0,0 @@
-const resolve = (path) => require("path").join(__dirname, path);
-
-module.exports = {
-  cli: { path: resolve("../Fable.Cli") },
-  entry: resolve("./src/fable-compiler-js.fsproj"),
-  outDir: resolve("../../build/fable-compiler-js/out"),
-};
