79e69286bef788cf2fe7fa6cdae734778b2080ce
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2020-11-25 21:25:41 +0900

    Publish RC 9

--- src/Fable.Transforms/Global/Compiler.fs
+++ src/Fable.Transforms/Global/Compiler.fs
@@ -1,7 +1,7 @@
 namespace Fable
 
 module Literals =
-    let [<Literal>] VERSION = "3.0.0-nagareyama-rc-008"
+    let [<Literal>] VERSION = "3.0.0-nagareyama-rc-009"
 
 type CompilerOptionsHelper =
     static member DefaultExtension = ".fs.js"
