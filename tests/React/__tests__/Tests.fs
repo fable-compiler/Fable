8132a0e45e945c8e8a6f632ef3b3d29c8c6a28d5
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2020-12-11 12:35:33 +0900

    Runtime check for Option.Value

--- tests/React/__tests__/Tests.fs
+++ tests/React/__tests__/Tests.fs
@@ -5,17 +5,38 @@ open Fable.ReactTestingLibrary
 
 Jest.describe("React tests", (fun () ->
 
-    Jest.test("ReactComponent renders correctly", (fun () ->
-        let elem = RTL.render(App.Counter())
+    Jest.test("Counter renders correctly", (fun () ->
+        let elem = RTL.render(Counter.Counter())
         Jest.expect(elem.container).toMatchSnapshot()
     ))
 
-    Jest.test("ReactComponent state works", (fun () ->
-        let elem = RTL.render(App.Counter())
+    Jest.test("Counter state works", (fun () ->
+        let elem = RTL.render(Counter.Counter())
         let header = elem.getByTestId "header"
         let button = elem.getByTestId "button-increment"
         Jest.expect(header).toHaveTextContent("0")
         RTL.fireEvent.click(button)
         Jest.expect(header).toHaveTextContent("1")
+    ))
+
+    Jest.test("SpreadSheet renders correctly", (fun () ->
+        let elem = RTL.render(SpreadSheet.SpreadSheet())
+        Jest.expect(elem.container).toMatchSnapshot()
+    ))
+
+    Jest.test("SpreadSheet parser works", (fun () ->
+        let spreadsheet = RTL.render(SpreadSheet.SpreadSheet())
+        let cells = spreadsheet.getAllByRole("cell")
+        let cell = cells.[5]
+        RTL.fireEvent.click(cell)
+        let input = spreadsheet.getByAltText("cell editor")
+        RTL.fireEvent.input(input, [
+            event.target [
+                Feliz.prop.value "=5"
+            ]
+        ])
+        // Click another cell to remove the editor
+        RTL.fireEvent.click(cells.[8])
+        Jest.expect(cell).toHaveTextContent("5")
     ))
 ))
