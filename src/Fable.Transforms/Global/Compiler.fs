69d11d14448c7ce1391ac6319a69ffbfdafd7e32
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2021-02-18 15:27:34 +0900

    Release 3.1.5

--- src/Fable.Transforms/Global/Compiler.fs
+++ src/Fable.Transforms/Global/Compiler.fs
@@ -1,7 +1,7 @@
 namespace Fable
 
 module Literals =
-    let [<Literal>] VERSION = "3.1.4"
+    let [<Literal>] VERSION = "3.1.5"
 
 type CompilerOptionsHelper =
     static member DefaultExtension = ".fs.js"
