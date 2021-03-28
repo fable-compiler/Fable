405a938e3d36c09f791a09a40b489883436a05b5
Dag Brattli <dag@brattli.net>
2021-03-28 13:37:10 +0200

    Add more tests

--- tests/Python/TestSeq.fs
+++ tests/Python/TestSeq.fs
@@ -1,10 +1,24 @@
 module Fable.Tests.Seqs
 
 open Util.Testing
-open Fable.Tests.Util
+
+
+[<Fact>]
+let ``test Seq.empty works`` () =
+    let xs = Seq.empty<int>
+    Seq.length xs
+    |> equal 0
 
 [<Fact>]
 let ``test Seq.length works`` () =
     let xs = [1.; 2.; 3.; 4.]
     Seq.length xs
     |> equal 4
+
+[<Fact>]
+let ``test Seq.map works`` () =
+    let xs = [1; 2; 3; 4]
+    xs
+    |> Seq.map string
+    |> List.ofSeq
+    |> equal ["1"; "2"; "3"; "4"]
