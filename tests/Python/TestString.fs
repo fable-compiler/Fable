65181886a1e2057d671704ceae1069fb47655784
Dag Brattli <dag@brattli.net>
2021-03-30 07:49:33 +0200

    Better string testing

--- tests/Python/TestString.fs
+++ tests/Python/TestString.fs
@@ -2,10 +2,45 @@ module Fable.Tests.String
 
 open Util.Testing
 
+[<Fact>]
+let ``test sprintf works`` () =
+    // Immediately applied
+    sprintf "%.2f %g" 0.5468989 5.
+    |> equal "0.55 5"
+    // Curried
+    let printer = sprintf "Hi %s, good %s!"
+    let printer = printer "Alfonso"
+    printer "morning" |> equal "Hi Alfonso, good morning!"
+    printer "evening" |> equal "Hi Alfonso, good evening!"
+
+[<Fact>]
+let ``test sprintf works II`` () =
+      let printer2 = sprintf "Hi %s, good %s%s" "Maxime"
+      let printer2 = printer2 "afternoon"
+      printer2 "?" |> equal "Hi Maxime, good afternoon?"
+
+[<Fact>]
+let ``test sprintf with different decimal digits works`` () =
+      sprintf "Percent: %.0f%%" 5.0 |> equal "Percent: 5%"
+      sprintf "Percent: %.2f%%" 5. |> equal "Percent: 5.00%"
+      sprintf "Percent: %.1f%%" 5.24 |> equal "Percent: 5.2%"
+      sprintf "Percent: %.2f%%" 5.268 |> equal "Percent: 5.27%"
+      sprintf "Percent: %f%%" 5.67 |> equal "Percent: 5.670000%"
+
+[<Fact>]
+let ``sprintf displays sign correctly`` () =
+      sprintf "%i" 1 |> equal "1"
+      sprintf "%d" 1 |> equal "1"
+      sprintf "%d" 1L |> equal "1"
+      sprintf "%.2f" 1. |> equal "1.00"
+      sprintf "%i" -1 |> equal "-1"
+      sprintf "%d" -1 |> equal "-1"
+      sprintf "%d" -1L |> equal "-1"
+      sprintf "%.2f" -1. |> equal "-1.00"
 
 [<Fact>]
 let ``test interpolate works`` () =
-  let name = "Phillip"
-  let age = 29
-  $"Name: {name}, Age: %i{age}"
-  |> equal "Name: Phillip, Age: 29"
+    let name = "Phillip"
+    let age = 29
+    $"Name: {name}, Age: %i{age}"
+    |> equal "Name: Phillip, Age: 29"
