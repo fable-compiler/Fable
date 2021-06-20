f292065f7035d1e2a66f5b42bb6d9d98ab31f372
Gustavo Leon <1261319+gusty@users.noreply.github.com>
2021-06-20 07:57:15 +0200

    Enum overload doesn't work the same in Fable

--- tests/Main/ApplicativeTests.fs
+++ tests/Main/ApplicativeTests.fs
@@ -435,11 +435,7 @@ type Parse =
     static member inline Parse (_: ^R, _: obj  ) = fun (x:string) -> (^R: (static member Parse : _ -> ^R) x)
     static member inline Parse (_: ^R, _: Parse) = fun (x:string) -> (^R: (static member Parse : _ * _ -> ^R) (x, Globalization.CultureInfo.InvariantCulture))
 
-    static member inline Parse (_: 'T when 'T : enum<_>, _: Parse) = fun x ->
-        (match Enum.TryParse (x) with
-            | (true, v) -> v
-            | _         -> invalidArg "value" ("Requested value '" + x + "' was not found.")
-        ) : 'enum
+    static member inline Parse (_: 'T when ^T: null and ^T: struct, _: Parse) = id
 
     static member Parse (_: bool              , _: Parse) = fun (x:string) -> Boolean.Parse (x)
     static member Parse (_: char              , _: Parse) = fun  x -> Char.Parse (x)
