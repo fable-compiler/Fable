1527ca6922a7900b83284429a8c13656da208229
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2021-09-30 14:35:18 +0900

    Rename .fable to fable_modules

--- src/Fable.Cli/Util.fs
+++ src/Fable.Cli/Util.fs
@@ -270,7 +270,7 @@ module Imports =
         let importPath = Path.normalizePath importPath
         let outDir = Path.normalizePath outDir
         // It may happen the importPath is already in outDir,
-        // for example package sources in .fable folder
+        // for example package sources in fable_modules folder
         if importPath.StartsWith(outDir) then importPath
         else
             let importDir = Path.GetDirectoryName(importPath)
