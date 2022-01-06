05f5d0e76f9452d485335750cbd716a7e58cf96a
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2022-01-06 12:16:51 +0900

    Fix #2688, seed random

--- tests/Main/ArithmeticTests.fs
+++ tests/Main/ArithmeticTests.fs
@@ -2,6 +2,7 @@ module Fable.Tests.Arithmetic
 
 open System
 open Util.Testing
+open Fable.Tests.Util
 
 let [<Literal>] aLiteral = 5
 let notALiteral = 5
@@ -582,17 +583,49 @@ let tests =
 
     testCase "System.Random works" <| fun () ->
         let rnd = System.Random()
+        let x = rnd.Next()
+        x >= 0 |> equal true
+
         let x = rnd.Next(5)
         (x >= 0 && x < 5) |> equal true
 
-        let y = rnd.NextDouble()
-        (y >= 0.0 && y < 1.0) |> equal true
+        let x = rnd.Next(14, 20)
+        (x >= 14 && x < 20) |> equal true
+
+        let x = rnd.Next(-14, -10)
+        (x >= -14 && x < -10) |> equal true
+
+        let x = rnd.NextDouble()
+        (x >= 0.0 && x < 1.0) |> equal true
+
+        throwsAnyError <| fun () -> rnd.Next(-10)
+        throwsAnyError <| fun () -> rnd.Next(14, 10)
 
     // Note: Test could fail sometime during life of universe, if it picks all zeroes.
     testCase "System.Random.NextBytes works" <| fun () ->
         let buffer = Array.create 16 0uy // guid-sized buffer
         System.Random().NextBytes(buffer)
+        buffer.Length |> equal 16
         buffer = Array.create 16 0uy |> equal false
+
+        throwsAnyError <| fun () -> System.Random().NextBytes(null)
+
+    testCase "System.Random seeded works" <| fun () ->
+        let rnd = System.Random(1234)
+        rnd.Next() |> equal 857019877
+        rnd.Next(100) |> equal 89
+        rnd.Next(1000, 10000) |> equal 3872
+        rnd.NextDouble() |> equal 0.9467375338760845
+
+        throwsAnyError <| fun () -> rnd.Next(-10)
+        throwsAnyError <| fun () -> rnd.Next(14, 10)
+
+    testCase "System.Random.NextBytes seeded works" <| fun () ->
+        let buffer = Array.create 4 0uy // guid-sized buffer
+        System.Random(5432).NextBytes(buffer)
+        buffer |> equal [|152uy; 238uy; 227uy; 30uy|]
+
+        throwsAnyError <| fun () -> System.Random().NextBytes(null)
 
     testCase "Long integers equality works" <| fun () ->
         let x = 5L
