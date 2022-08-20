ef3de45f9ef1e11b5e7de49033c51a23f0677b6b
Ian Manske <IanManske@pm.me>
2022-08-20 15:47:10 -0400

    Cleanup imports.

--- src/Fable.Cli/ProjectCoreCracker.fs
+++ src/Fable.Cli/ProjectCoreCracker.fs
@@ -5,15 +5,16 @@ open System
 open System.IO
 
 open FSharp.Compiler.CodeAnalysis
+open Ionide.ProjInfo
 
 let private projInfo additionalMSBuildProps (file: string) =
     let projDir = Path.GetDirectoryName file
 
     // The following 3 calls may? throw, any need to reformat/reraise errors?
     // - Errors here are caught by retryGetCrackedProjects and the msg/error is lost.
-    let toolsPath = Ionide.ProjInfo.Init.init (DirectoryInfo projDir) None
-    let loader = Ionide.ProjInfo.WorkspaceLoader.Create(toolsPath, additionalMSBuildProps)
-    let proj = loader.LoadProjects ([file], ["IsCrossTargetingBuild"], Ionide.ProjInfo.BinaryLogGeneration.Off) |> Seq.head
+    let toolsPath = Init.init (DirectoryInfo projDir) None
+    let loader = WorkspaceLoader.Create(toolsPath, additionalMSBuildProps)
+    let proj = loader.LoadProjects ([file], ["IsCrossTargetingBuild"], BinaryLogGeneration.Off) |> Seq.head
 
     let isCrossTargeting =
         proj.CustomProperties |> List.tryPick (fun p ->
@@ -29,8 +30,8 @@ let private projInfo additionalMSBuildProps (file: string) =
             | (first :: _) ->
                 // Atm setting a preferenece is not supported in FSAC
                 // As workaround, lets choose the first of the target frameworks and use that
-                let loader = Ionide.ProjInfo.WorkspaceLoader.Create(toolsPath, ("TargetFramework", first) :: additionalMSBuildProps)
-                loader.LoadProjects ([file], [], Ionide.ProjInfo.BinaryLogGeneration.Off) |> Seq.head
+                let loader = WorkspaceLoader.Create(toolsPath, ("TargetFramework", first) :: additionalMSBuildProps)
+                loader.LoadProjects ([file], [], BinaryLogGeneration.Off) |> Seq.head
         else
             proj
 
