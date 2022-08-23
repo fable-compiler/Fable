ab6b721f985d9c7a2e65624ba1d4eaf0bde96a35
Ian Manske <IanManske@pm.me>
2022-08-23 16:35:59 -0400

    Add cross targeting test.

---
+++ tests/Integration/ProjectConfigs/CrossTargetingRelease/Main.fs
@@ -0,0 +1,32 @@
+﻿module Fable.Tests.CrossTargetingRelease
+
+open Fable.Core
+open Fable.Core.JsInterop
+open Fable.Core.Testing
+
+let [<Global>] describe (name: string) (f: unit->unit) : unit = jsNative
+let [<Global>] it (msg: string) (f: unit->unit) : unit = jsNative
+let equals expected actual = Assert.AreEqual(actual, expected)
+
+describe "CrossTargetingRelease" (fun () ->
+    let isDefined = "is defined"
+    let notDefined = "not defined"
+
+    it "should not define DEBUG" (fun () ->
+        #if DEBUG
+        isDefined
+        #else
+        notDefined
+        #endif
+        |> equals notDefined
+    )
+
+    it "should define first listed framework" (fun () ->
+        #if NET5_0
+        isDefined
+        #else
+        notDefined
+        #endif
+        |> equals isDefined
+    )
+)
