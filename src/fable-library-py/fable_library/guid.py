e084dedd8f886b690f38048389769e6df8d7d097
Dag Brattli <dag@brattli.net>
2022-09-05 08:05:07 +0200

    [Python] Guid fixes.
    - Add to/from array conversion

--- src/fable-library-py/fable_library/guid.py
+++ src/fable-library-py/fable_library/guid.py
@@ -4,7 +4,7 @@ from .types import FSharpRef
 
 
 def parse(string: str) -> uuid.UUID:
-    return uuid.UUID(string)
+    return uuid.UUID(string.strip("()"))
 
 
 def try_parse(string: str, def_value: FSharpRef[uuid.UUID]) -> bool:
@@ -21,3 +21,11 @@ def to_string(guid: uuid.UUID) -> str:
 
 def new_guid() -> uuid.UUID:
     return uuid.uuid4()
+
+
+def guid_to_array(guid: uuid.UUID) -> bytearray:
+    return bytearray(guid.bytes_le)
+
+
+def array_to_guid(guid: bytearray) -> uuid.UUID:
+    return uuid.UUID(bytes_le=bytes(guid))
