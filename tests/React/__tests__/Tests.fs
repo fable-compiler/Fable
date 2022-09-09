1bea675600cdd39a583435c33efa84220edb25d5
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2022-09-09 10:36:23 +0900

    Add JSX React test

--- tests/React/__tests__/Tests.fs
+++ tests/React/__tests__/Tests.fs
@@ -1,8 +1,12 @@
 module Fable.Tests.React
 
+open Fable.Core.JsInterop
 open Feliz
 open Fable.Jester
 open Fable.ReactTestingLibrary
+
+// Necessary for JSX compilation
+let React: obj = importAll "react"
 
 [<ReactComponent>]
 let ComponentAcceptingCurriedFunction fn _oneMoreParam =
@@ -15,26 +19,40 @@ let ComponentAcceptingCurriedFunction fn _oneMoreParam =
 Jest.describe("React tests", (fun () ->
 
     Jest.test("Counter renders correctly", (fun () ->
-        let elem = RTL.render(Counter.Counter())
+        let elem = RTL.render(Counter.Counter(5))
         Jest.expect(elem.container).toMatchSnapshot()
     ))
 
     Jest.test("Counter state works", (fun () ->
-        let elem = RTL.render(Counter.Counter())
+        let elem = RTL.render(Counter.Counter(5))
         let header = elem.getByTestId "header"
         let button = elem.getByTestId "button-increment"
-        Jest.expect(header).toHaveTextContent("0")
+        Jest.expect(header).toHaveTextContent("5")
         RTL.fireEvent.click(button)
-        Jest.expect(header).toHaveTextContent("1")
+        Jest.expect(header).toHaveTextContent("6")
+    ))
+
+    Jest.test("Counter JSX renders correctly", (fun () ->
+        let elem = RTL.render(Counter.CounterJSX(5) |> unbox)
+        Jest.expect(elem.container).toMatchSnapshot()
+    ))
+
+    Jest.test("Counter JSX state works", (fun () ->
+        let elem = RTL.render(Counter.CounterJSX(5) |> unbox)
+        let header = elem.getByTestId "header-jsx"
+        let button = elem.getByTestId "button-increment-jsx"
+        Jest.expect(header).toHaveTextContent("5")
+        RTL.fireEvent.click(button)
+        Jest.expect(header).toHaveTextContent("6")
     ))
 
     Jest.test("SpreadSheet renders correctly", (fun () ->
-        let elem = RTL.render(SpreadSheet.SpreadSheet())
+        let elem = RTL.render(SpreadSheet.SpreadSheet() |> unbox)
         Jest.expect(elem.container).toMatchSnapshot()
     ))
 
     Jest.test("SpreadSheet parser works", (fun () ->
-        let spreadsheet = RTL.render(SpreadSheet.SpreadSheet())
+        let spreadsheet = RTL.render(SpreadSheet.SpreadSheet() |> unbox)
         let cells = spreadsheet.getAllByRole("cell")
         let cell = cells.[5]
         RTL.fireEvent.click(cell)
