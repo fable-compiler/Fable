4bc65dd60b4aac99c7115349d82d9d2ceadb9ac7
Alfonso Garcia-Caro <alfonso.garcia-caro@outlook.com>
2022-10-31 20:16:07 +0900

    Release 4.0.0-theta-014

--- src/Fable.Transforms/Global/Compiler.fs
+++ src/Fable.Transforms/Global/Compiler.fs
@@ -1,7 +1,7 @@
 namespace Fable
 
 module Literals =
-    let [<Literal>] VERSION = "4.0.0-theta-011"
+    let [<Literal>] VERSION = "4.0.0-theta-014"
 
 type CompilerOptionsHelper =
     static member Make(?language,
