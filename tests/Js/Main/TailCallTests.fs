ed1823636642d1afb7d8704c6b8848a58201ae35
ncave <777696+ncave@users.noreply.github.com>
2023-01-05 14:24:49 -0800

    Removed comment

--- tests/Js/Main/TailCallTests.fs
+++ tests/Js/Main/TailCallTests.fs
@@ -131,7 +131,6 @@ let tests =
     testCase "Tailcall works in tail position" <| fun () ->
         Issue3301.simple 100000 1 |> equal 100001
 
-    // TODO: temporary diabled until Fable-FCS issue is solved, see #3311
     testCase "Tailcall works with bindings" <| fun () ->
         Issue3301.binding 100000 1 |> equal 100001
 
