d1e44c877dd11558cc7af4978db072b3adecf88b
Gauthier Segay <gauthier.segay@gmail.com>
2023-10-21 13:55:44 +0200

    revert QuickTest.fs

--- src/quicktest/QuickTest.fs
+++ src/quicktest/QuickTest.fs
@@ -5,13 +5,76 @@ module QuickTest
 // When everything works, move the tests to the appropriate file in tests/Main.
 // Please don't add this file to your commits.
 
-type U = Z of value: int | Y of name: string
+open System
+open System.Collections.Generic
+open Fable.Core
+open Fable.Core.JsInterop
+open Fable.Core.Testing
 
-let u1 = Z 1
-let u2 = Y "abc"
+let log (o: obj) =
+    JS.console.log(o)
+    // printfn "%A" o
 
-let a : U = Fable.Core.PyInterop.emitPyExpr 1 "U.Z($0)"
-let b : U = Fable.Core.PyInterop.emitPyExpr "abc" "U.Y($0)"
-Fable.Core.Testing.Assert.AreEqual(a, u1)
-Fable.Core.Testing.Assert.AreEqual(b, u2)
+let equal expected actual =
+   let areEqual = expected = actual
+   printfn "%A = %A > %b" expected actual areEqual
+   if not areEqual then
+       failwithf "[ASSERT ERROR] Expected %A but got %A" expected actual
 
+let throwsError (expected: string) (f: unit -> 'a): unit =
+   let success =
+       try
+           f () |> ignore
+           true
+       with e ->
+           if not <| String.IsNullOrEmpty(expected) then
+               equal e.Message expected
+           false
+   // TODO better error messages
+   equal false success
+
+let testCase (msg: string) f: unit =
+   try
+       printfn "%s" msg
+       f ()
+   with ex ->
+       printfn "%s" ex.Message
+       if ex.Message <> null && ex.Message.StartsWith("[ASSERT ERROR]") |> not then
+           printfn "%s" (ex.StackTrace ??= "")
+   printfn ""
+
+let testCaseAsync msg f =
+   testCase msg (fun () ->
+       async {
+           try
+               do! f ()
+           with ex ->
+               printfn "%s" ex.Message
+               if ex.Message <> null && ex.Message.StartsWith("[ASSERT ERROR]") |> not then
+                   printfn "%s" (ex.StackTrace ??= "")
+       } |> Async.StartImmediate)
+
+let throwsAnyError (f: unit -> 'a): unit =
+    let success =
+        try
+            f() |> ignore
+            true
+        with e ->
+            printfn "Got expected error: %s" e.Message
+            false
+    if success then
+        printfn "[ERROR EXPECTED]"
+
+let measureTime (f: unit -> unit): unit = emitJsStatement () """
+   //js
+   const startTime = process.hrtime();
+   f();
+   const elapsed = process.hrtime(startTime);
+   console.log("Ms:", elapsed[0] * 1e3 + elapsed[1] / 1e6);
+   //!js
+"""
+
+// Write here your unit test, you can later move it
+// to Fable.Tests project. For example:
+// testCase "Addition works" <| fun () ->
+//     2 + 2 |> equal 4
