e02898ba714c62f1e460f341d3a774eec7cdc458
Maxime Mangel <mangel.maxime@protonmail.com>
2023-10-31 15:58:33 +0100

    Port standalone from old build system
    #3536

--- src/Fable.Build/Standalone.fs
+++ src/Fable.Build/Standalone.fs
@@ -5,6 +5,7 @@ open Build.Utils
 open BlackFox.CommandLine
 open System.IO
 open Build.FableLibrary
+open Fake.IO
 
 let private projectDir = Path.Resolve("src", "fable-standalone")
 
@@ -12,7 +13,52 @@ let private buildDir = Path.Combine(projectDir, "temp")
 
 let private distDir = Path.Combine(projectDir, "dist")
 
-let build () =
+let private buildWorker (minify: bool) =
+
+    let fableArgs =
+        CmdLine.empty
+        |> CmdLine.appendRaw "src/Worker"
+        |> CmdLine.appendPrefix "--outDir" (buildDir </> "worker")
+        |> CmdLine.appendPrefix "--lang" "javascript"
+
+    Command.Fable(fableArgs, workingDirectory = projectDir)
+
+    Command.Run(
+        "npx",
+        CmdLine.empty
+        |> CmdLine.appendRaw "rollup"
+        |> CmdLine.appendRaw (buildDir </> "worker" </> "Worker.js")
+        |> CmdLine.appendPrefix "-o" (buildDir </> "worker" </> "bundle.js")
+        |> CmdLine.appendPrefix "--format" "iife"
+        |> CmdLine.toString,
+        workingDirectory = projectDir
+    )
+
+    Command.Run(
+        "npx",
+        CmdLine.empty
+        |> CmdLine.appendRaw "esbuild"
+        |> CmdLine.appendRaw (buildDir </> "worker" </> "bundle.js")
+        |> CmdLine.appendRaw "--bundle"
+        |> CmdLine.appendRaw $"""--outfile={(distDir </> "worker.min.js")}"""
+        |> CmdLine.appendRaw "--format=iife"
+        |> CmdLine.appendIf minify "--minify"
+        |> CmdLine.toString,
+        workingDirectory = projectDir
+    )
+
+    let fableLibraryDist = (distDir </> "fable-library")
+
+    Directory.ensure fableLibraryDist
+
+    // Copy Fable library to dist folder
+    Shell.copyRecursive
+        (Path.Resolve("temp", "fable-library"))
+        fableLibraryDist
+        true
+    |> ignore
+
+let build (minify: bool) =
 
     let fableArgs =
         CmdLine.concat [
@@ -42,8 +88,8 @@ let build () =
         CmdLine.empty
         |> CmdLine.appendRaw "rollup"
         |> CmdLine.appendRaw (buildDir </> "Main.js")
-        |> CmdLine.appendPrefix "-o" (distDir </> "bundle.js")
-        |> CmdLine.appendPrefix "--format" "umd"
+        |> CmdLine.appendPrefix "-o" (buildDir </> "bundle.js")
+        |> CmdLine.appendPrefix "--format" "esm"
         |> CmdLine.appendPrefix "--name" "__FABLE_STANDALONE__"
         |> CmdLine.toString,
         workingDirectory = projectDir
@@ -53,19 +99,22 @@ let build () =
         "npx",
         CmdLine.empty
         |> CmdLine.appendRaw "esbuild"
-        |> CmdLine.appendRaw (distDir </> "bundle.js")
+        |> CmdLine.appendRaw (buildDir </> "bundle.js")
         |> CmdLine.appendRaw "--bundle"
-        |> CmdLine.appendRaw $"""--outfile={(distDir </> "bundle.js")}"""
+        |> CmdLine.appendRaw $"""--outfile={(distDir </> "bundle.min.js")}"""
         |> CmdLine.appendRaw "--format=esm"
-        |> CmdLine.appendRaw "--platform=node"
-        |> CmdLine.appendRaw "--minify"
-        |> CmdLine.appendRaw "--overwrite"
+        |> CmdLine.appendIf minify "--minify"
         |> CmdLine.toString,
-            workingDirectory = projectDir
+        workingDirectory = projectDir
     )
+
+    buildWorker minify
 
 let handle (args: string list) =
     let minify = args |> List.contains "--no-minify" |> not
     let isWatch = args |> List.contains "--watch"
+    let skipFableLibrary = args |> List.contains "--skip-fable-library"
 
-    build ()
+    BuildFableLibraryJavaScript().Run(skipFableLibrary)
+
+    build minify
