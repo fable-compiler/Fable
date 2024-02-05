9321aea158a77b0434950479707e77c106514d98
Zach Robinson <zachary.robinson@interstates.com>
2024-02-04 21:28:13 -0600

    Add rest of Result module for Rust.

--- src/fable-library-rust/src/Result.fs
+++ src/fable-library-rust/src/Result.fs
@@ -24,3 +24,63 @@ let isError result : bool =
     match result with
     | Error _ -> true
     | Ok _ -> false
+
+let contains value result =
+    match result with
+    | Error _ -> false
+    | Ok x -> x = value
+
+let count result =
+    match result with
+    | Error _ -> 0
+    | Ok _ -> 1
+
+let defaultValue defaultValue result =
+    match result with
+    | Error _ -> defaultValue
+    | Ok x -> x
+
+let defaultWith defThunk result =
+    match result with
+    | Error e -> defThunk e
+    | Ok x -> x
+
+let exists predicate result =
+    match result with
+    | Error _ -> false
+    | Ok x -> predicate x
+
+let fold folder state result =
+    match result with
+    | Error _ -> state
+    | Ok x -> folder state x
+
+let foldBack folder result state =
+    match result with
+    | Error _ -> state
+    | Ok x -> folder x state
+
+let forall predicate result =
+    match result with
+    | Error _ -> true
+    | Ok x -> predicate x
+
+let iterate action result =
+    match result with
+    | Error _ -> ()
+    | Ok x -> action x
+
+let toArray result =
+    match result with
+    | Error _ -> [||]
+    | Ok x -> [| x |]
+
+let toList result =
+    match result with
+    | Error _ -> []
+    | Ok x -> [ x ]
+
+let toOption result =
+    match result with
+    | Error _ -> None
+    | Ok x -> Some x
--- tests/Rust/tests/src/ResultTests.fs
+++ tests/Rust/tests/src/ResultTests.fs
@@ -68,6 +68,111 @@ let ``isError function can be generated`` () =
     err |> Result.isError |> equal true
 
 [<Fact>]
+let ``test contains function can be generated`` () =
+    let ok: Result<int, string> = Ok 42
+    let err: Result<int, string> = Error "error"
+
+    ok |> Result.contains 42 |> equal true
+    ok |> Result.contains 43 |> equal false
+    err |> Result.contains 42 |> equal false
+
+[<Fact>]
+let ``count function can be generated`` () =
+    let ok: Result<int, string> = Ok 42
+    let err: Result<int, string> = Error "error"
+
+    ok |> Result.count |> equal 1
+    err |> Result.count |> equal 0
+
+[<Fact>]
+let ``defaultValue function can be generated`` () =
+    let ok: Result<int, string> = Ok 42
+    let err: Result<int, string> = Error "error"
+
+    ok |> Result.defaultValue 0 |> equal 42
+    err |> Result.defaultValue 0 |> equal 0
+
+[<Fact>]
+let ``defaultWith function can be generated`` () =
+    let ok: Result<int, string> = Ok 42
+    let err: Result<int, string> = Error "error"
+
+    ok |> Result.defaultWith (fun e -> e.Length) |> equal 42
+    err |> Result.defaultWith (fun e -> e.Length) |> equal 5
+
+[<Fact>]
+let ``exists function can be generated`` () =
+    let ok: Result<int, string> = Ok 42
+    let err: Result<int, string> = Error "error"
+
+    ok |> Result.exists ((=) 42) |> equal true
+    ok |> Result.exists ((=) 43) |> equal false
+    err |> Result.exists ((=) 42) |> equal false
+
+[<Fact>]
+let ``fold function can be generated`` () =
+    let ok: Result<int, string> = Ok 42
+    let err: Result<int, string> = Error "error"
+
+    ok |> Result.fold (fun s x -> s || x % 2 = 0) false |> equal true
+    err |> Result.fold (fun s x -> s || x % 2 = 0) false |> equal false
+
+[<Fact>]
+let ``foldBack function can be generated`` () =
+    let ok: Result<int, string> = Ok 42
+    let err: Result<int, string> = Error "error"
+
+    (ok, false) ||> Result.foldBack (fun x s -> s || x % 2 = 0) |> equal true
+    (err, false) ||> Result.foldBack (fun x s -> s || x % 2 = 0) |> equal false
+
+[<Fact>]
+let ``forAll function can be generated`` () =
+    let ok: Result<int, string> = Ok 42
+    let err: Result<int, string> = Error "error"
+
+    ok |> Result.forall ((=) 42) |> equal true
+    ok |> Result.forall ((=) 43) |> equal false
+    err |> Result.forall ((=) 42) |> equal true
+
+[<Fact>]
+let ``iter function can be generated`` () =
+    let ok: Result<int, string> = Ok 42
+    let err: Result<int, string> = Error "error"
+
+    let mutable count = 0
+    ok |> Result.iter (fun x -> count <- count + x)
+    equal 42 count
+
+    count <- 0
+    err |> Result.iter (fun x -> count <- count + x)
+    equal 0 count
+
+[<Fact>]
+let ``toArray function can be generated`` () =
+    let ok: Result<int, string> = Ok 42
+    let err: Result<int, string> = Error "error"
+
+    ok |> Result.toArray |> equal [| 42 |]
+    err |> Result.toArray |> equal [||]
+
+[<Fact>]
+let ``toList function can be generated`` () =
+    let ok: Result<int, string> = Ok 42
+    let err: Result<int, string> = Error "error"
+
+    ok |> Result.toList |> equal [ 42 ]
+    err |> Result.toList |> equal []
+
+[<Fact>]
+let ``toOption function can be generated`` () =
+    let ok: Result<int, string> = Ok 42
+    let err: Result<int, string> = Error "error"
+
+    ok |> Result.toOption |> Option.get |> equal 42
+    err |> Result.toOption |> Option.isNone |> equal true
+
+
+[<Fact>]
 let ``Choice matching works`` () =
     let ok: Choice<string, int> = Choice1Of2 "foo"
     match ok with
