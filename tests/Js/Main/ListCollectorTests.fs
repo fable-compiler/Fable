f1aaedbfd1a16db898cf0dd14fc48d39ae34f6d3
Maxime Mangel <mangel.maxime@protonmail.com>
2024-02-09 11:54:51 +0100

    Cover more API in the tests for ListCollector

--- tests/Js/Main/ListCollectorTests.fs
+++ tests/Js/Main/ListCollectorTests.fs
@@ -1,7 +1,6 @@
 module Fable.Tests.ListCollector
 
 open Util.Testing
-open Fable.Tests.Util
 open Microsoft.FSharp.Core.CompilerServices
 
 let cutOffLast list =
@@ -23,4 +22,32 @@ let tests =
         testCase "ListCollector.Add and .Close" <| fun () ->
             let result = cutOffLast [ 1; 2; 3 ]
             result |> equal [ 1; 2 ]
+
+        testCase "ListCollector.Close works for empty list" <| fun () ->
+            let mutable l = ListCollector<_>()
+            let result = l.Close()
+
+            result |> equal []
+
+        testCase "ListCollector.AddMany works" <| fun () ->
+            let mutable l = ListCollector<_>()
+            l.AddMany([ 1; 2; 3 ])
+
+            let result = l.Close()
+
+            result |> equal [ 1; 2; 3 ]
+
+        testCase "ListCollector.AddMany works for empty list" <| fun () ->
+            let mutable l = ListCollector<_>()
+            l.AddMany([])
+
+            let result = l.Close()
+
+            result |> equal []
+
+        testCase "ListCollector.AddManyAndClose works" <| fun () ->
+            let mutable l = ListCollector<_>()
+            let result = l.AddManyAndClose([ 1; 2; 3 ])
+
+            result |> equal [ 1; 2; 3 ]
     ]
