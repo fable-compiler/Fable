a3ca9b3685f3b1739e884d2b87f776a5484055ae
nojaf <florian.verdonck@outlook.com>
2024-02-09 10:44:05 +0100

    Revert QuickTest.fs

--- src/quicktest/QuickTest.fs
+++ src/quicktest/QuickTest.fs
@@ -98,14 +98,3 @@ printfn "Running quick tests..."
 // to Fable.Tests project. For example:
 // testCase "Addition works" <| fun () ->
 //     2 + 2 |> equal 4
-
-open Microsoft.FSharp.Core.CompilerServices
-
-testCase
-    "meh"
-    (fun () ->
-        let c = ListCollector<int>()
-        c.Add(1)
-        let r = c.Close()
-        ignore r
-    )
