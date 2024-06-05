704c92864d382cba1abb3b65746dbcd01a0ff40b
Alan Ball <vash.arclan@gmail.com>
2024-06-04 21:51:19 -0400

    added reminder to fix quicktest

--- src/Fable.Build/Quicktest/Lua.fs
+++ src/Fable.Build/Quicktest/Lua.fs
@@ -2,7 +2,7 @@ module Build.Quicktest.Lua
 
 open Build.FableLibrary
 open Build.Quicktest.Core
-
+//TODO: Quicktest still needs to be actually written.
 let handle (args: string list) =
     genericQuicktest
         {
