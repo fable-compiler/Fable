72fe81973c3632ad6dedfb14db45a15c66fb5d89
Goswin Rothenthal <goswin@rothenthal.com>
2024-09-18 20:11:56 +0200

    use Transformer Replacements

--- build.sh
+++ build.sh
@@ -2,3 +2,5 @@
 
 dotnet tool restore
 dotnet run --project src/Fable.Build/Fable.Build.fsproj -- $@
+# dotnet run --project src/Fable.Build/Fable.Build.fsproj -- test javascript
+# dotnet run --project src/Fable.Build/Fable.Build.fsproj -- test typescript
