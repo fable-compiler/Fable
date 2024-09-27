5c8cc1efc5ff7707fe6c1a6c361a98f54c8e5bd3
Слава Україні! Героям слава! <777696+ncave@users.noreply.github.com>
2024-09-27 18:17:43 +0000

    [TS] Fixed #3864 (#3904)

--- src/Fable.Transforms/Fable2Babel.fs
+++ src/Fable.Transforms/Fable2Babel.fs
@@ -757,6 +757,7 @@ module Annotation =
     // Move this to Replacements.tryEntity?
     let tryNativeOrFableLibraryInterface com ctx genArgs (ent: Fable.Entity) =
         match ent.FullName with
+        | Types.fsharpAsyncGeneric -> makeFableLibImportTypeAnnotation com ctx genArgs "AsyncBuilder" "IAsync" |> Some
         | _ when not ent.IsInterface -> None
         | Types.icollection -> makeNativeTypeAnnotation com ctx genArgs "Iterable" |> Some
         // -> makeFableLibImportTypeAnnotation com ctx [Fable.Any] "Util" "ICollection"
