d80f99bb419d8d72a2ddd39c20686898934a5fa3
ncave <777696+ncave@users.noreply.github.com>
2024-11-25 11:53:22 -0800

    Renamed tests

--- tests/Integration/Compiler/CompilerHelpersTests.fs
+++ tests/Integration/Compiler/CompilerHelpersTests.fs
@@ -12,12 +12,12 @@ let tests =
             Fable.CompilerExt.expectedVersionMatchesActual "5.0.1" "5.0.0" |> equal true
             Fable.CompilerExt.expectedVersionMatchesActual "5.1.0" "5.0.0" |> equal true
 
-        testCase "expectedVersionMatchesActual works if actual version is highter than expected version" <| fun _ ->
+        testCase "expectedVersionMatchesActual works if actual version is higher than expected version" <| fun _ ->
             Fable.CompilerExt.expectedVersionMatchesActual "4.0.0" "5.0.0" |> equal true
             Fable.CompilerExt.expectedVersionMatchesActual "4.0.1" "5.0.0" |> equal true
             Fable.CompilerExt.expectedVersionMatchesActual "4.1.0" "5.0.0" |> equal true
 
-        testCase "expectedVersionMatchesActual reject if actual version is highter than expected version" <| fun _ ->
+        testCase "expectedVersionMatchesActual reject if actual version is lower than expected version" <| fun _ ->
             Fable.CompilerExt.expectedVersionMatchesActual "4.0.0" "3.0.0" |> equal false
             Fable.CompilerExt.expectedVersionMatchesActual "4.0.1" "3.0.0" |> equal false
             Fable.CompilerExt.expectedVersionMatchesActual "4.1.0" "3.0.0" |> equal false
