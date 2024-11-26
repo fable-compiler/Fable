cb0b36b651145c43132a884b0ee1ba098c95dcd8
Maxime Mangel <mangel.maxime@protonmail.com>
2024-11-26 20:45:08 +0100

    fix: tests I had the argument order inverted

--- tests/Integration/Compiler/CompilerHelpersTests.fs
+++ tests/Integration/Compiler/CompilerHelpersTests.fs
@@ -9,13 +9,13 @@ let tests =
     testList "Compiler Helpers" [
         testCase "expectedVersionMatchesActual works for same major version" <| fun _ ->
             Fable.CompilerExt.expectedVersionMatchesActual "5.0.0" "5.0.0" |> equal true
-            Fable.CompilerExt.expectedVersionMatchesActual "5.0.1" "5.0.0" |> equal true
-            Fable.CompilerExt.expectedVersionMatchesActual "5.1.0" "5.0.0" |> equal true
+            Fable.CompilerExt.expectedVersionMatchesActual "5.0.0" "5.0.1" |> equal true
+            Fable.CompilerExt.expectedVersionMatchesActual "5.1.0" "5.1.0" |> equal true
 
         testCase "expectedVersionMatchesActual works if actual version is higher than expected version" <| fun _ ->
             Fable.CompilerExt.expectedVersionMatchesActual "4.0.0" "5.0.0" |> equal true
-            Fable.CompilerExt.expectedVersionMatchesActual "4.0.1" "5.0.0" |> equal true
-            Fable.CompilerExt.expectedVersionMatchesActual "4.1.0" "5.0.0" |> equal true
+            Fable.CompilerExt.expectedVersionMatchesActual "4.0.0" "5.0.1" |> equal true
+            Fable.CompilerExt.expectedVersionMatchesActual "4.0.0" "5.1.0" |> equal true
 
         testCase "expectedVersionMatchesActual reject if actual version is lower than expected version" <| fun _ ->
             Fable.CompilerExt.expectedVersionMatchesActual "4.0.0" "3.0.0" |> equal false
