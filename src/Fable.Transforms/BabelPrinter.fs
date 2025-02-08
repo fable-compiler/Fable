c07c18bc080dc54d4537f0386448b6aa67cfdcc8
cabboose <shayanftw@gmail.com>
2025-02-08 13:08:34 +0800

    Enhancement - special property name for plugins to utilise and spread objects

--- src/Fable.Transforms/BabelPrinter.fs
+++ src/Fable.Transforms/BabelPrinter.fs
@@ -443,6 +443,12 @@ module PrinterExtensions =
                 |> List.iter (
                     function
                     | _, NullOrUndefinedOrVoid -> ()
+                    | "_SPREAD_PROPERTY_", value ->
+                        printProp (fun () ->
+                            printer.Print("{...")
+                            printer.Print(value)
+                            printer.Print("}")
+                        )
                     | key, StringConstant value -> printProp (fun () -> printer.Print($"{key}=\"{value}\""))
                     | key, value ->
                         printProp (fun () ->
