a7c5841bcc6047b3ae5ce14ad7b4a7a7b36c91ed
Maxime Mangel <me@mangelmaxime.fr>
2025-02-23 17:20:30 +0100

    test: disable timezone dependant test

--- tests/Python/TestDateTime.fs
+++ tests/Python/TestDateTime.fs
@@ -440,8 +440,10 @@ let ``test DateTime.ToString with custom format works`` () =
     |> equal "r PM PM"
     DateTime(2019,1,1).ToString("r yyyyyy yyyyyyyyyy", CultureInfo.InvariantCulture)
     |> equal "r 002019 0000002019"
-    DateTime(2014, 7, 1, 16, 37, 0).ToString("r zzzz zzzzzz", CultureInfo.InvariantCulture)
-    |> equal "r +02:00 +02:00"
+
+    // Timezone dependent (test is configured for Europe/Paris timezone)
+    // DateTime(2014, 7, 1, 16, 37, 0).ToString("r zzzz zzzzzz", CultureInfo.InvariantCulture)
+    // |> equal "r +02:00 +02:00"
 
 
 [<Fact>]
