4a90f15523df8e06960bdad7101bc3fbb5985430
Maxime Mangel <me@mangelmaxime.fr>
2025-03-10 22:23:26 +0100

    [TS] Sanitize DUs case names when generating constructor function (#4072)
    Fix #4071

--- src/Fable.Transforms/Fable2Babel.fs
+++ src/Fable.Transforms/Fable2Babel.fs
@@ -1572,7 +1572,7 @@ module Util =
             if com.IsTypeScript then
                 match List.tryItem tag ent.UnionCases with
                 | Some case ->
-                    match tryJsConstructorWithSuffix com ctx ent ("_" + case.Name) with
+                    match tryJsConstructorWithSuffix com ctx ent ("_" + sanitizeName case.Name) with
                     | Some helperRef ->
                         let typeParams = makeTypeParamInstantiation com ctx genArgs
 
@@ -3718,7 +3718,7 @@ module Util =
                                 )
                             )
 
-                        let fnId = entName + "_" + case.Name |> Identifier.identifier
+                        let fnId = entName + "_" + sanitizeName case.Name |> Identifier.identifier
                         // Don't use return type, TypeScript will infer it and sometimes we want to use
                         // the actual constructor type in case it implements an interface
                         // let returnType = AliasTypeAnnotation(Identifier.identifier(entName + UnionHelpers.UNION_SUFFIX), entParamsInst)
