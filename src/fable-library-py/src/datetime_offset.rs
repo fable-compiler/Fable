fd52462f306bc29fa6be0e564bc214bc72a29420
Dag Brattli <dag.brattli@cognite.com>
2025-04-26 08:58:52 +0200

    Add missing datetime offset

---
+++ src/fable-library-py/src/datetime_offset.rs
@@ -0,0 +1,105 @@
+use pyo3::prelude::*;
+use chrono::Utc;
+
+/// A module for datetime offset operations
+pub fn register_datetime_offset_module(parent_module: &Bound<'_, PyModule>) -> PyResult<()> {
+    let m = PyModule::new(parent_module.py(), "datetime_offset")?;
+
+    // Add functions to the module
+    m.add_function(wrap_pyfunction!(now, &m)?)?;
+
+    // Commented out functions for now
+    // m.add_function(wrap_pyfunction!(add, m)?)?;
+    // m.add_function(wrap_pyfunction!(parse, m)?)?;
+    // m.add_function(wrap_pyfunction!(try_parse, m)?)?;
+    // m.add_function(wrap_pyfunction!(create, m)?)?;
+    // m.add_function(wrap_pyfunction!(utc_now, m)?)?;
+    // m.add_function(wrap_pyfunction!(op_addition, m)?)?;
+    // m.add_function(wrap_pyfunction!(op_subtraction, m)?)?;
+    // m.add_function(wrap_pyfunction!(min_value, m)?)?;
+
+    parent_module.add_submodule(&m)
+}
+
+/// Get current datetime
+///
+/// Returns the current local date and time as a Python datetime object.
+#[pyfunction]
+fn now() -> PyResult<PyObject> {
+    // Get the current local time using chrono
+    // Convert to Python datetime using PyO3's chrono feature
+    pyo3::prepare_freethreaded_python();
+    Python::with_gil(|py| {
+        let now = Utc::now();
+        let py_now = now.into_pyobject(py)?;
+        Ok(py_now.into())
+    })
+}
+
+// /// Add a timedelta to a datetime
+// #[pyfunction]
+// fn add(py: Python<'_>, d: DateTime<FixedOffset>, ts: Duration) -> DateTime<FixedOffset> {
+//     // Add the duration to the datetime
+//     d + ts
+// }
+//
+// /// Parse a string into a datetime
+// #[pyfunction]
+// #[pyo3(signature = (string, detect_utc=false))]
+// fn parse(py: Python<'_>, string: &str, detect_utc: bool) -> PyResult<DateTime<FixedOffset>> {
+//     // TODO: Implement using chrono
+//     unimplemented!()
+// }
+//
+// /// Try to parse a string into a datetime
+// #[pyfunction]
+// fn try_parse(py: Python<'_>, string: &str, style: i32, unsigned: bool, bitsize: i32, def_value: &PyAny) -> PyResult<bool> {
+//     // TODO: Implement using chrono
+//     unimplemented!()
+// }
+//
+// /// Create a datetime with timezone
+// #[pyfunction]
+// #[pyo3(signature = (year, month, day, h, m, s, ms, offset=None))]
+// fn create(
+//     py: Python<'_>,
+//     year: i32,
+//     month: i32,
+//     day: i32,
+//     h: i32,
+//     m: i32,
+//     s: i32,
+//     ms: i64,
+//     offset: Option<Duration>,
+// ) -> PyResult<DateTime<FixedOffset>> {
+//     // TODO: Implement using chrono
+//     unimplemented!()
+// }
+//
+// /// Get current UTC datetime
+// #[pyfunction]
+// fn utc_now(py: Python<'_>) -> DateTime<Utc> {
+//     // Get the current UTC time using chrono
+//     Utc::now()
+// }
+//
+// /// Add a timedelta to a datetime
+// #[pyfunction]
+// fn op_addition(py: Python<'_>, x: DateTime<FixedOffset>, y: Duration) -> DateTime<FixedOffset> {
+//     // Add the duration to the datetime
+//     x + y
+// }
+//
+// /// Subtract a datetime or timedelta from a datetime
+// #[pyfunction]
+// fn op_subtraction(py: Python<'_>, x: DateTime<FixedOffset>, y: DateTime<FixedOffset>) -> Duration {
+//     // Subtract the datetimes to get a duration
+//     x - y
+// }
+//
+// /// Get minimum datetime value
+// #[pyfunction]
+// fn min_value(py: Python<'_>) -> DateTime<Utc> {
+//     // Create a datetime for year 1, month 1, day 1
+//     Utc.with_ymd_and_hms(1, 1, 1, 0, 0, 0).unwrap()
+// }
