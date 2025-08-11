7737257549b4e2cd8e9efb0a1cefea4feba68e13
ncave <777696+ncave@users.noreply.github.com>
2025-08-11 09:17:14 -0700

    [JS/TS] Fix comparers for non-nullable types

--- src/Fable.Cli/CHANGELOG.md
+++ src/Fable.Cli/CHANGELOG.md
@@ -15,6 +15,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Fixed
 
 * [Python] Improve Python (e.g. Pydantic) interop (by @dbrattli)
+* [JS/TS] Fix comparers for non-nullable types (by @ncave)
 * [Rust] Fix compiler-generated generic args (by @ncave)
 * [All] Fixed #4041 missing unit argument (by @ncave)
 * [JS/TS/Python] Fixed eq comparer mangling (by @ncave)
--- src/fable-library-ts/FSharp.Collections.fs
+++ src/fable-library-ts/FSharp.Collections.fs
@@ -3,10 +3,16 @@ namespace FSharp.Collections
 open System.Collections.Generic
 
 module HashIdentity =
-    let FromFunctions<'T> hasher equals : IEqualityComparer<'T> =
+    let FromFunctions<'T when 'T: not struct> (hasher: 'T -> int) (equals: 'T -> 'T -> bool) : IEqualityComparer<'T> =
         { new IEqualityComparer<'T> with
             member _.GetHashCode(x) = hasher x
-            member _.Equals(x, y) = equals x y
+
+            member _.Equals(x, y) =
+                match x, y with
+                | null, null -> true
+                | null, y -> false
+                | x, null -> false
+                | x, y -> equals x y
         }
 
     let Structural<'T when 'T: equality> : IEqualityComparer<'T> =
@@ -22,9 +28,14 @@ module HashIdentity =
         }
 
 module ComparisonIdentity =
-    let FromFunction<'T> comparer : IComparer<'T> =
+    let FromFunction<'T when 'T: not struct> (comparer: 'T -> 'T -> int) : IComparer<'T> =
         { new IComparer<'T> with
-            member _.Compare(x, y) = comparer x y
+            member _.Compare(x, y) =
+                match x, y with
+                | null, null -> 0
+                | null, y -> -1
+                | x, null -> 1
+                | x, y -> comparer x y
         }
 
     let Structural<'T when 'T: comparison> : IComparer<'T> =
