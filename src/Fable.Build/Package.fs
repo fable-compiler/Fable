ee8de850627bb284a41fe5a0156704ff9224b8ca
Mangel Maxime <me@mangelmaxime.fr>
2026-03-21 21:09:39 +0100

    Include Fable.AST as one of the packaged project for local testing

--- src/Fable.Build/Package.fs
+++ src/Fable.Build/Package.fs
@@ -37,47 +37,28 @@ let handle (args: string list) =
             TypeScript = PackageJson.tempFableLibraryTs |> PackageJson.getVersion
         |}
 
-    Command.Run(
-        "dotnet",
-        CmdLine.empty
-        |> CmdLine.appendRaw "pack"
-        |> CmdLine.appendRaw Fsproj.fableCli
-        |> CmdLine.appendPrefix "-c" "Release"
-        // By pass the PackageVersion in the fsproj, without having to modify it on the disk
-        |> CmdLine.appendRaw "-p:EasyBuildPackageReleaseNotes_DisableSetVersion=true"
-        |> CmdLine.appendRaw $"-p:PackageVersion={tempVersion}"
-        |> CmdLine.appendPrefix "-o" packageDestination
-        |> CmdLine.toString
-    )
+    let packPackage fsproj =
+        Command.Run(
+            "dotnet",
+            CmdLine.empty
+            |> CmdLine.appendRaw "pack"
+            |> CmdLine.appendRaw fsproj
+            |> CmdLine.appendPrefix "-c" "Release"
+            // By pass the PackageVersion in the fsproj, without having to modify it on the disk
+            |> CmdLine.appendRaw "-p:EasyBuildPackageReleaseNotes_DisableSetVersion=true"
+            |> CmdLine.appendRaw $"-p:PackageVersion={tempVersion}"
+            |> CmdLine.appendPrefix "-o" packageDestination
+            |> CmdLine.toString
+        )
 
-    Command.Run(
-        "dotnet",
-        CmdLine.empty
-        |> CmdLine.appendRaw "pack"
-        |> CmdLine.appendRaw Fsproj.fableCompiler
-        |> CmdLine.appendPrefix "-c" "Release"
-        // By pass the PackageVersion in the fsproj, without having to modify it on the disk
-        |> CmdLine.appendRaw "-p:EasyBuildPackageReleaseNotes_DisableSetVersion=true"
-        |> CmdLine.appendRaw $"-p:PackageVersion={tempVersion}"
-        |> CmdLine.appendPrefix "-o" packageDestination
-        |> CmdLine.toString
-    )
+    packPackage Fsproj.fableCli
+    packPackage Fsproj.fableCompiler
 
     // This avoid common error of comitting the modified file
     File.WriteAllText(compilerFsPath, compilerFsOriginalContent)
 
-    Command.Run(
-        "dotnet",
-        CmdLine.empty
-        |> CmdLine.appendRaw "pack"
-        |> CmdLine.appendRaw Fsproj.fableCore
-        |> CmdLine.appendPrefix "-c" "Release"
-        // By pass the PackageVersion in the fsproj, without having to modify it on the disk
-        |> CmdLine.appendRaw "-p:EasyBuildPackageReleaseNotes_DisableSetVersion=true"
-        |> CmdLine.appendRaw $"-p:PackageVersion={tempVersion}"
-        |> CmdLine.appendPrefix "-o" packageDestination
-        |> CmdLine.toString
-    )
+    packPackage Fsproj.fableCore
+    packPackage Fsproj.fableAst
 
     printfn
         $"""Local packages created.
@@ -87,4 +68,5 @@ Use the following commands to install them:
 - Fable.Cli: dotnet tool update fable --version {tempVersion} --add-source {packageDestination}
 - Fable.Compiler: dotnet add package Fable.Compiler --version {tempVersion} --source {packageDestination}
 - Fable.Core: dotnet add package Fable.Core --version {tempVersion} --source {packageDestination}
+- Fable.AST: dotnet add package Fable.AST --version {tempVersion} --source {packageDestination}
     """
