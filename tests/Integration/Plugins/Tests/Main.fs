e1cb743009cfa417c7e2d876cf54e714a2b1c6e0
Mangel Maxime <me@mangelmaxime.fr>
2026-07-11 23:56:32 +0200

    test: add test project for Fable plugins

---
+++ tests/Integration/Plugins/Tests/Main.fs
@@ -0,0 +1,33 @@
+module Fable.Tests.Main
+
+let allTests = [| PluginsTests.tests |]
+
+#if FABLE_COMPILER
+
+open Fable.Core
+open Fable.Core.JsInterop
+
+let inline describe (name: string) (f: unit -> unit) : unit = import "describe" "node:test"
+let inline it (msg: string) (f: unit -> unit) : unit = import "it" "node:test"
+
+let rec flattenTest (test: Util.Testing.TestKind) : unit =
+    match test with
+    | Util.Testing.TestKind.TestList(name, tests) ->
+        describe name (fun () ->
+            for t in tests do
+                flattenTest t)
+    | Util.Testing.TestKind.TestCase(name, test) -> it name (unbox test)
+
+let run () =
+    for t in allTests do
+        flattenTest t
+
+run ()
+
+#else
+
+// This project only ever runs through Fable; this branch just keeps it a valid .NET project.
+[<EntryPoint>]
+let main _ = 0
+
+#endif
