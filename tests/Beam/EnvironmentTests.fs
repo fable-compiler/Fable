5b391352e9a23a97dd861b3b99ea7349a80b90ec
Mangel Maxime <me@mangelmaxime.fr>
2026-07-16 08:52:59 +0200

    chore: remove temp test

--- tests/Beam/EnvironmentTests.fs
+++ tests/Beam/EnvironmentTests.fs
@@ -12,9 +12,3 @@ let ``test Environment.GetEnvironmentVariable returns null for a missing variabl
 let ``test Environment.CurrentDirectory works`` () =
     Environment.CurrentDirectory.Length > 0
     |> equal true
-
-[<Fact>]
-let ``test Environment.GetEnvironmentVariable returns a value for an existing variable`` () =
-    // PATH should be set in any environment (Linux/macOS/Windows CI runners included)
-    Environment.GetEnvironmentVariable("PATH").Length > 0
-    |> equal true
