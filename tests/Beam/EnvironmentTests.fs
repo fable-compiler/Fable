50118cbe6b7cd2f708b25e6d4e0d32896666cc75
Mangel Maxime <me@mangelmaxime.fr>
2026-07-16 18:26:24 +0200

    chore: fix `set_CurrentDirectory`

--- tests/Beam/EnvironmentTests.fs
+++ tests/Beam/EnvironmentTests.fs
@@ -17,3 +17,15 @@ let ``test Environment.GetEnvironmentVariable returns null for a missing variabl
 let ``test Environment.CurrentDirectory works`` () =
     Environment.CurrentDirectory.Length > 0
     |> equal true
+
+[<Fact>]
+let ``test Environment.CurrentDirectory can be set and read back`` () =
+    let original = Environment.CurrentDirectory
+
+    try
+        Environment.CurrentDirectory <- ".."
+        let updated = Environment.CurrentDirectory
+        updated <> original |> equal true
+        original.StartsWith(updated) |> equal true
+    finally
+        Environment.CurrentDirectory <- original
